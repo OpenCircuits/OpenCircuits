97357f175af4ba256bc84e53918b62e9d1ee1155
Leon Montealegre <leonm99@gmail.com>
2018-01-10 18:57:27 -0800

    Babel-ified output for non-ES6 browsers.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ var uglify = require('gulp-uglify-es').default;
 var concat = require('gulp-concat');
 var gap = require('gulp-append-prepend');
 var mocha = require('gulp-mocha');
+var babel = require('gulp-babel');
 
 var dest = "site/app/public/js/combined.js";
 var dest_min = "site/app/public/js/combined-min.js";
@@ -19,10 +20,12 @@ var test_paths = files.map(function(file) { return "tests/app/public/js/" + file
 function build() {
     gulp.src(paths)
       .pipe(concat(dest))
-      .pipe(gap.prependText("/* Built at: " + (new Date()).toString() + " */\n"))
       .pipe(gap.prependText("var __TESTING__ = false;\n\n"))
       .pipe(gap.prependText("\"use strict\";"))
+      .pipe(babel({presets: ['es2015']}))
+      .pipe(gap.prependText("/* Built at: " + (new Date()).toString() + " */\n"))
       .pipe(gulp.dest("."))
+      .pipe(babel({presets: ['es2015']}))
       .pipe(uglify())
       .pipe(concat(dest_min))
       .pipe(gulp.dest("."));
