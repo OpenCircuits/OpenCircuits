19b51a5685470aee00e2b5ab0ff405a61279d5d5
Leon Montealegre <leonm99@gmail.com>
2019-01-25 16:44:26 -0500

    Commented out php code.

--- site/app/models/Config.php
+++ site/app/models/Config.php
@@ -26,29 +26,29 @@ class Config {
     public function __construct() {
         $properties = array();
 
-        $file = fopen('./data/config.txt', 'r');
-
-        if (!$file) {
-            die("Could not find config file!");
-        }
-
-        // Read properties line by line
-        while (!feof($file)) {
-            $line = fgets($file);
-
-            $pos = strpos($line,'=');
-
-            $prop = substr($line, 0, $pos);
-            $value = substr($line, $pos+1);
-
-            $properties[$prop] = $value;
-        }
-
-        fclose($file);
-
-        $this->debugMode = ($properties['DebugMode'] === "true");
-        $this->scripts = explode("|", $properties['Scripts']);
-        $this->items = explode("|", $properties['Items']);
+        // $file = fopen('./data/config.txt', 'r');
+        //
+        // if (!$file) {
+        //     die("Could not find config file!");
+        // }
+        //
+        // // Read properties line by line
+        // while (!feof($file)) {
+        //     $line = fgets($file);
+        //
+        //     $pos = strpos($line,'=');
+        //
+        //     $prop = substr($line, 0, $pos);
+        //     $value = substr($line, $pos+1);
+        //
+        //     $properties[$prop] = $value;
+        // }
+        //
+        // fclose($file);
+        // 
+        // $this->debugMode = ($properties['DebugMode'] === "true");
+        // $this->scripts = explode("|", $properties['Scripts']);
+        // $this->items = explode("|", $properties['Items']);
     }
 
     public function isDebugMode() {
