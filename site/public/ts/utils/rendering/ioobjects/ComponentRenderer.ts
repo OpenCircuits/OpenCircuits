62e9445d4a751ffa3704c10282718ea9bc9d707f
Leon Montealegre <leonm99@gmail.com>
2019-02-27 13:13:43 -0500

    Added rendering for ICs

--- site/public/ts/utils/rendering/ioobjects/ComponentRenderer.ts
+++ site/public/ts/utils/rendering/ioobjects/ComponentRenderer.ts
@@ -8,13 +8,16 @@ import {DEBUG_SHOW_CULLBOXES,
 import {Renderer} from "../Renderer";
 import {IOPortRenderer} from "./IOPortRenderer";
 import {GateRenderer} from "./gates/GateRenderer";
+import {ICRenderer} from "./other/ICRenderer";
 
+import {Transform} from "../../math/Transform";
 import {Camera} from "../../Camera";
 
 import {Component} from "../../../models/ioobjects/Component";
 import {PressableComponent} from "../../../models/ioobjects/PressableComponent";
 import {Gate} from "../../../models/ioobjects/gates/Gate";
 import {SevenSegmentDisplay} from "../../../models/ioobjects/outputs/SevenSegmentDisplay";
+import {IC} from "../../../models/ioobjects/other/IC";
 
 import {Images} from "../../Images";
 
@@ -23,6 +26,13 @@ import {Images} from "../../Images";
 // import {LED} from "../../../models/ioobjects/outputs/LED";
 
 export var ComponentRenderer = (function() {
+
+    let drawBox = function(renderer: Renderer, transform: Transform, selected: boolean) {
+        let borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
+        let fillCol   = (selected ? SELECTED_FILL_COLOR   : DEFAULT_FILL_COLOR);
+        renderer.rect(0, 0, transform.getSize().x, transform.getSize().y, fillCol, borderCol, DEFAULT_BORDER_WIDTH);
+    }
+
     return {
         render(renderer: Renderer, camera: Camera, object: Component, selected: boolean) {
             // Check if object is on the screen
@@ -50,24 +60,20 @@ export var ComponentRenderer = (function() {
                     imgName = object.getOnImageName();
 
                 let box = object.getSelectionBox();
-                let borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
-                let fillCol   = (selected ? SELECTED_FILL_COLOR   : DEFAULT_FILL_COLOR);
-                renderer.rect(box.getPos().x, box.getPos().y, box.getSize().x, box.getSize().y, fillCol, borderCol, DEFAULT_BORDER_WIDTH);
-            }
-            if (object instanceof Gate) {
-                GateRenderer.render(renderer, camera, object, selected);
+                drawBox(renderer, box, selected);
             }
 
-            //Seven Segment renderer
-            if (object instanceof SevenSegmentDisplay) {
-                let borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
-                let fillCol   = (selected ? SELECTED_FILL_COLOR   : DEFAULT_FILL_COLOR);
-                renderer.rect(0, 0, transform.getSize().x, transform.getSize().y, fillCol, borderCol, DEFAULT_BORDER_WIDTH);
-            }
+            // Specific renderers
+            if (object instanceof Gate)
+                GateRenderer.render(renderer, camera, object, selected);
+            if (object instanceof IC)
+                ICRenderer.render(renderer, camera, object, selected);
+            if (object instanceof SevenSegmentDisplay)
+                drawBox(renderer, transform, selected);
 
             // Draw tinted image
             let tint = (selected ? SELECTED_FILL_COLOR : undefined);
-            if (Images.GetImage(imgName)) 
+            if (Images.GetImage(imgName))
                 renderer.image(Images.GetImage(imgName), 0, 0, transform.getSize().x, transform.getSize().y, tint);
 
             renderer.restore();
