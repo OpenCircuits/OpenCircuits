26f5fa57d3c67eb70cc6fa15a4233b9e77e2c717
Leon Montealegre <leonm99@gmail.com>
2019-03-19 21:07:02 -0400

    Made PNGs save correctly

--- site/public/ts/utils/io/Exporter.ts
+++ site/public/ts/utils/io/Exporter.ts
@@ -5,39 +5,17 @@ import {CircuitDesigner} from "../../models/CircuitDesigner";
 
 export const Exporter = (function() {
 
-    let saved = false;
+    let saved = true; // TODO, set saved to true when saving file
+                      //  but somehow set it to false anytime the circuit changes
 
     const write = function(designer: CircuitDesigner): string {
-        let writer = new XMLWriter(designer.getXMLName());
+        const writer = new XMLWriter(designer.getXMLName());
 
         writer.setVersion(1);
 
         designer.save(writer.getRoot());
 
         return writer.serialize();
-    }
-
-    const saveData = function(data: string, projectName: string, extension: string): void {
-        // Get name
-        if (projectName.replace(/\s+/g, '') === "")
-            projectName = "Untitled Circuit";
-        let filename = projectName + extension;
-
-        let file = new Blob([data], {type: "text/plain"});
-        if (window.navigator.msSaveOrOpenBlob) { // IE10+
-            window.navigator.msSaveOrOpenBlob(file, filename);
-        } else { // Others
-            let a = document.createElement("a");
-            let url = URL.createObjectURL(file);
-            a.href = url;
-            a.download = filename;
-            document.body.appendChild(a);
-            a.click();
-            setTimeout(function() {
-                document.body.removeChild(a);
-                window.URL.revokeObjectURL(url);
-            }, 0);
-        }
     }
 
     // Prompt for exit
@@ -51,12 +29,52 @@ export const Exporter = (function() {
 
     return {
         saveFile: function(designer: CircuitDesigner, projectName: string) {
-            let data = write(designer);
-            saveData(data, projectName, ".circuit");
+            const data = write(designer);
+
+            // Get name
+            if (projectName.replace(/\s+/g, '') === "")
+                projectName = "Untitled Circuit";
+            const filename = projectName + ".circuit";
+
+            const file = new Blob([data], {type: "text/plain"});
+            if (window.navigator.msSaveOrOpenBlob) { // IE10+
+                window.navigator.msSaveOrOpenBlob(file, filename);
+            } else { // Others
+                const a = document.createElement("a");
+                const url = URL.createObjectURL(file);
+                a.href = url;
+                a.download = filename;
+                document.body.appendChild(a);
+                a.click();
+                setTimeout(function() {
+                    document.body.removeChild(a);
+                    window.URL.revokeObjectURL(url);
+                }, 0);
+            }
         },
         savePNG: function(canvas: HTMLCanvasElement, projectName: string) {
             const data = canvas.toDataURL("image/png", 1.0);
-            saveData(data, projectName, ".png");
+
+            // Get name
+            if (projectName.replace(/\s+/g, '') === "")
+                projectName = "Untitled Circuit";
+            const filename = projectName + ".png";
+
+            if (window.navigator.msSaveOrOpenBlob) { // IE10+
+                const file = new Blob([data], {type: "image/png"});
+                window.navigator.msSaveOrOpenBlob(file, filename);
+            } else { // Others
+                const a = document.createElement("a");
+                const url = data;
+                a.href = url;
+                a.download = filename;
+                document.body.appendChild(a);
+                a.click();
+                setTimeout(function() {
+                    document.body.removeChild(a);
+                    window.URL.revokeObjectURL(url);
+                }, 0);
+            }
         },
         savePDF: function(canvas: HTMLCanvasElement, projectName: string) {
             const width  = canvas.width;
