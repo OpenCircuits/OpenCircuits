ae7137500d8014e7ff42a4ce9f81da021d3870b4
Leon Montealegre <leonm99@gmail.com>
2019-04-04 02:03:32 -0400

    Clean up Vector

--- site/public/ts/utils/math/Vector.ts
+++ site/public/ts/utils/math/Vector.ts
@@ -1,11 +1,11 @@
 export class Vector {
-    x: number;
-    y: number;
+    public x: number;
+    public y: number;
 
-    constructor(x: Vector | number = 0, y: number = 0) {
+    public constructor(x: Vector | number = 0, y: number = 0) {
         this.set(x, y);
     }
-    set(x: Vector | number, y: number = 0): void {
+    public set(x: Vector | number, y: number = 0): void {
         if (x instanceof Vector) {
             this.x = (x.x ? x.x : 0);
             this.y = (x.y ? x.y : 0);
@@ -14,33 +14,33 @@ export class Vector {
             this.y = (y ? y : 0);
         }
     }
-    translate(dx: Vector | number, dy: number = 0): void {
+    public translate(dx: Vector | number, dy: number = 0): void {
         if (dx instanceof Vector)
             this.set(this.add(dx));
         else
             this.set(this.x + dx, this.y + dy);
     }
-    add(x: Vector | number, y: number = 0): Vector {
+    public add(x: Vector | number, y: number = 0): Vector {
         if (x instanceof Vector)
             return new Vector(this.x + x.x, this.y + x.y);
         else
             return new Vector(this.x + x, this.y + y);
     }
-    sub(x: Vector | number, y: number = 0): Vector {
+    public sub(x: Vector | number, y: number = 0): Vector {
         if (x instanceof Vector)
             return new Vector(this.x - x.x, this.y - x.y);
         else (y != null)
             return new Vector(this.x - x, this.y - y);
     }
-    scale(a: Vector | number): Vector {
+    public scale(a: Vector | number): Vector {
         if (a instanceof Vector)
             return new Vector(a.x * this.x, a.y * this.y);
         return new Vector(a * this.x, a * this.y);
     }
-    abs(): Vector {
+    public abs(): Vector {
         return new Vector(Math.abs(this.x), Math.abs(this.y));
     }
-    normalize(): Vector {
+    public normalize(): Vector {
         const len = this.len();
         if (len === 0) {
             return new Vector(0, 0);
@@ -49,40 +49,37 @@ export class Vector {
             return new Vector(this.x * invLen, this.y * invLen);
         }
     }
-    len(): number {
+    public len(): number {
         return Math.sqrt(this.x*this.x + this.y*this.y);
     }
-    len2(): number {
+    public len2(): number {
         return this.x*this.x + this.y*this.y;
     }
-    angle(): number {
+    public angle(): number {
         return Math.atan2(this.y, this.x);
     }
-    distanceTo(v: Vector): number {
+    public distanceTo(v: Vector): number {
         return this.sub(v).len();
     }
-    dot(v: Vector): number {
+    public dot(v: Vector): number {
         return this.x * v.x + this.y * v.y;
     }
-    project(v: Vector): Vector {
+    public project(v: Vector): Vector {
         return this.scale(v.dot(this) / this.len2());
     }
-    copy(): Vector {
+    public copy(): Vector {
         return new Vector(this.x, this.y);
     }
 
-    static min(v1: Vector, v2: Vector): Vector {
+    public static min(v1: Vector, v2: Vector): Vector {
         return new Vector(Math.min(v1.x, v2.x), Math.min(v1.y, v2.y));
     }
-    static max(v1: Vector, v2: Vector): Vector {
+    public static max(v1: Vector, v2: Vector): Vector {
         return new Vector(Math.max(v1.x, v2.x), Math.max(v1.y, v2.y));
     }
-    static clamp(x: Vector, lo: Vector, hi: Vector): Vector {
+    public static clamp(x: Vector, lo: Vector, hi: Vector): Vector {
         return Vector.min(Vector.max(x, lo), hi);
     }
-    // static V(x: Vector | number, y: number = 0): Vector {
-    //     return new Vector(x, y);
-    // }
 }
 
 export function V(x: Vector | number = 0, y: number = 0): Vector {
