63fd27be78c33680652fa0f39df14408746d6099
Hannah Lim <lim.hannah0228@gmail.com>
2019-04-09 16:54:21 -0400

    Code revised to get rid of identical code, port rendering condensed

--- site/public/ts/utils/rendering/ioobjects/IOPortRenderer.ts
+++ site/public/ts/utils/rendering/ioobjects/IOPortRenderer.ts
@@ -7,24 +7,13 @@ import {DEFAULT_FILL_COLOR,
         IO_PORT_BORDER_WIDTH} from "../../Constants";
 
 import {Renderer} from "../Renderer";
-import {InputPort} from "../../../models/ioobjects/InputPort";
-import {OutputPort} from "../../../models/ioobjects/OutputPort";
+import {Port} from "../../../models/ioobjects/Port";
 
 export const IOPortRenderer = (function() {
     return {
-        renderIPort(renderer: Renderer, iport: InputPort, selected: boolean) {
-            const o = iport.getOriginPos();
-            const v = iport.getTargetPos();
-
-            const borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
-            renderer.line(o.x, o.y, v.x, v.y, borderCol, IO_PORT_LINE_WIDTH);
-
-            const circleFillCol = (selected ? SELECTED_FILL_COLOR : DEFAULT_FILL_COLOR);
-            renderer.circle(v.x, v.y, IO_PORT_RADIUS, circleFillCol, borderCol, IO_PORT_BORDER_WIDTH);
-        },
-        renderOPort(renderer: Renderer, oport: OutputPort, selected: boolean) {
-            const o = oport.getOriginPos();
-            const v = oport.getTargetPos();
+        renderPort(renderer: Renderer, port: Port, selected: boolean) {
+            const o = port.getOriginPos();
+            const v = port.getTargetPos();
 
             const borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
             renderer.line(o.x, o.y, v.x, v.y, borderCol, IO_PORT_LINE_WIDTH);
