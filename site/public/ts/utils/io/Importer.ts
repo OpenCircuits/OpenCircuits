2a1281a0d4272424f10cbd64b64ac80d2da39c6e
Leon Montealegre <leonm99@gmail.com>
2019-04-09 23:45:56 -0400

    Made name of the circuit save and load

--- site/public/ts/utils/io/Importer.ts
+++ site/public/ts/utils/io/Importer.ts
@@ -6,7 +6,7 @@ import {CircuitDesigner} from "../../models/CircuitDesigner";
 export const Importer = (function() {
     let saved = false;
 
-    const read = function(designer: CircuitDesigner, file: string): string {
+    const read = function(designer: CircuitDesigner, file: string, setName: (n: string) => void): void {
         let root = <XMLDocument>new DOMParser().parseFromString(file, "text/xml");
         if (root.documentElement.nodeName == "parsererror")
             return;
@@ -17,11 +17,13 @@ export const Importer = (function() {
         if (reader.getVersion() == -1)
             ResolveVersionConflict(reader);
 
+        setName(reader.getName());
+
         designer.load(reader.getRoot());
     }
 
     return {
-        loadFile: function(designer: CircuitDesigner, file: File) {
+        loadFile: function(designer: CircuitDesigner, file: File, setName: (n: string) => void): void {
             // TOOD: only ask for confirmation if nothing was done to the scene
             //        ex. no objects, or wires, or history of actions
             let open = confirm("Are you sure you want to overwrite your current scene?");
@@ -31,7 +33,7 @@ export const Importer = (function() {
 
                 let reader = new FileReader();
                 reader.onload = function(e) {
-                    read(designer, reader.result.toString());
+                    read(designer, reader.result.toString(), setName);
                 }
 
                 reader.readAsText(file);
