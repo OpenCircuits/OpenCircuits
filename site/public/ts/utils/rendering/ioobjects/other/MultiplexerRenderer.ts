ad4e31de1d434a5b79259d1462e038cd6cfc63f2
LeonMontealegre <leonm99@gmail.com>
2019-06-12 15:02:47 -0400

    Migrated mux renderer to new system

--- site/public/ts/utils/rendering/ioobjects/other/MultiplexerRenderer.ts
+++ site/public/ts/utils/rendering/ioobjects/other/MultiplexerRenderer.ts
@@ -2,14 +2,16 @@ import {DEFAULT_BORDER_WIDTH,
         DEFAULT_BORDER_COLOR,
         DEFAULT_FILL_COLOR,
         SELECTED_BORDER_COLOR,
-        SELECTED_FILL_COLOR,
-        GATE_NOT_CIRCLE_RADIUS} from "../../../Constants";
-import {Vector,V} from "../../../math/Vector";
+        SELECTED_FILL_COLOR} from "../../../Constants";
+import {V} from "../../../math/Vector";
 
 import {Renderer} from "../../Renderer";
 import {Camera} from "../../../Camera";
 import {Multiplexer} from "../../../../models/ioobjects/other/Multiplexer";
 import {Demultiplexer} from "../../../../models/ioobjects/other/Demultiplexer";
+
+import {Polygon} from "../../shapes/Polygon";
+import {Style} from "../../Style";
 
 export const MultiplexerRenderer = (function() {
 
@@ -18,6 +20,7 @@ export const MultiplexerRenderer = (function() {
             const transform = mul.getTransform();
             const borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
             const fillCol   = (selected ? SELECTED_FILL_COLOR   : DEFAULT_FILL_COLOR);
+            const style = new Style(fillCol, borderCol, DEFAULT_BORDER_WIDTH);
 
             //
             // Creates the Multiplexer and Demultiplexer the correct size
@@ -28,7 +31,7 @@ export const MultiplexerRenderer = (function() {
                 const p3 = V(transform.getSize().x/2 , -transform.getSize().y/2 + 18);
                 const p4 = V(transform.getSize().x/2 , transform.getSize().y/2 - 18);
 
-                renderer.shape([p1, p2, p3, p4], fillCol, borderCol, DEFAULT_BORDER_WIDTH);
+                renderer.draw(new Polygon([p1, p2, p3, p4]), style);
             }
             else {
                 const p1 = V(transform.getSize().x/2 , transform.getSize().y/2 + 7);
@@ -36,7 +39,7 @@ export const MultiplexerRenderer = (function() {
                 const p3 = V(-transform.getSize().x/2, -transform.getSize().y/2 + 18);
                 const p4 = V(-transform.getSize().x/2, transform.getSize().y/2 - 18);
 
-                renderer.shape([p1, p2, p3, p4], fillCol, borderCol, DEFAULT_BORDER_WIDTH);
+                renderer.draw(new Polygon([p1, p2, p3, p4]), style);
             }
         }
     }
