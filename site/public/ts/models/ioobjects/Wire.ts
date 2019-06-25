c0fd2b97e47a5a10dd321cf2651c4aa959ed73bc
LeonMontealegre <leonm99@gmail.com>
2019-06-25 14:10:29 -0400

    Fixed bug with wire

--- site/public/ts/models/ioobjects/Wire.ts
+++ site/public/ts/models/ioobjects/Wire.ts
@@ -2,7 +2,6 @@ import {DEFAULT_SIZE,
         WIRE_THICKNESS} from "../../utils/Constants";
 
 import {V,Vector} from "../../utils/math/Vector";
-import {Matrix2x3} from "../../utils/math/Matrix";
 import {BezierCurve} from "../../utils/math/BezierCurve";
 import {XMLNode} from "../../utils/io/xml/XMLNode";
 import {OutputPort} from "../ports/OutputPort";
@@ -19,9 +18,6 @@ export class Wire extends CullableObject {
     private shape: BezierCurve;
     private straight: boolean;
 
-    private prevInputTransform: Matrix2x3;
-    private prevOutputTransform: Matrix2x3;
-
     public constructor(input: OutputPort, output: InputPort) {
         super();
 
@@ -36,26 +32,16 @@ export class Wire extends CullableObject {
 
     private updateCurve(): void {
         if (this.input != null) {
-            // If transform matrix differs then update curve
-            const mat = this.input.getParent().getTransform().getMatrix();
-            if (!mat.equals(this.prevInputTransform)) {
-                this.prevInputTransform = mat;
-                const pos = this.input.getWorldTargetPos();
-                const dir = this.input.getWorldDir();
-                this.shape.setP1(pos);
-                this.shape.setC1(dir.scale(DEFAULT_SIZE).add(pos));
-            }
+            const pos = this.input.getWorldTargetPos();
+            const dir = this.input.getWorldDir();
+            this.shape.setP1(pos);
+            this.shape.setC1(dir.scale(DEFAULT_SIZE).add(pos));
         }
         if (this.output != null) {
-            // If transform matrix differs then update curve
-            const mat = this.output.getParent().getTransform().getMatrix();
-            if (!mat.equals(this.prevOutputTransform)) {
-                this.prevOutputTransform = mat;
-                const pos = this.output.getWorldTargetPos();
-                const dir = this.output.getWorldDir();
-                this.shape.setP2(pos);
-                this.shape.setC2(dir.scale(DEFAULT_SIZE).add(pos));
-            }
+            const pos = this.output.getWorldTargetPos();
+            const dir = this.output.getWorldDir();
+            this.shape.setP2(pos);
+            this.shape.setC2(dir.scale(DEFAULT_SIZE).add(pos));
         }
     }
 
