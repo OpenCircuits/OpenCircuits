5d1bd8371c99a0bf6734c9f6b4749c391226b35d
LeonMontealegre <leonm99@gmail.com>
2019-06-25 14:11:39 -0400

    Added DeletePathAction

---
+++ site/public/ts/utils/actions/DeletePathAction.ts
@@ -0,0 +1,38 @@
+import {Action} from "./Action";
+import {GroupAction} from "./GroupAction";
+import {DeleteAction} from "./DeleteAction";
+import {DeleteWireAction} from "./DeleteWireAction";
+
+import {Wire} from "../../models/ioobjects/Wire";
+import {WirePort} from "../../models/ioobjects/other/WirePort";
+
+export class DeletePathAction implements Action {
+    private action: GroupAction;
+
+    public constructor(path: Array<Wire | WirePort>) {
+        this.action = new GroupAction();
+
+        // Remove wires first
+        path.filter((p) => p instanceof Wire)
+            .map((p) => p as Wire)
+            .forEach((w) => this.action.add(new DeleteWireAction(w)));
+
+        // Then remove WirePorts
+        path.filter((p) => p instanceof WirePort)
+            .map((p) => p as WirePort)
+            .forEach((wp) => this.action.add(new DeleteAction(wp)));
+    }
+
+    public execute(): Action {
+        this.action.execute();
+
+        return this;
+    }
+
+    public undo(): Action {
+        this.action.undo();
+
+        return this;
+    }
+
+}
