334d5dea1297d02c0eaf154ba49a5f92079ffddd
Leon Montealegre <leonm99@gmail.com>
2019-07-08 01:43:27 -0400

    Removed clipboard from Config

--- site/public/ts/utils/Config.ts
+++ site/public/ts/utils/Config.ts
@@ -3,7 +3,3 @@ export function setSAVED(b:boolean): void {SAVED = b;}
 
 export let LABEL = true;
 export function setLABEL(b:boolean): void {LABEL = b;}
-
-// used in CopyController.ts
-export let CLIPBOARD = "";
-export function setCLIPBOARD(s:string): void {CLIPBOARD = s;}
