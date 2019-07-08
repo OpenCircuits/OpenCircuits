602f7793936923f32268f266b53ca4a8946fab4a
Leon Montealegre <leonm99@gmail.com>
2019-07-08 02:42:01 -0400

    Fixed copy controller

--- site/public/ts/controllers/CopyController.ts
+++ site/public/ts/controllers/CopyController.ts
@@ -1,13 +1,19 @@
+import {V} from "../utils/math/Vector";
+
 import {Exporter} from "../utils/io/Exporter";
 import {Importer} from "../utils/io/Importer";
 import {CopyGroup} from "../utils/ComponentUtils";
 
+import {GroupAction} from "../utils/actions/GroupAction";
+import {TranslateAction} from "../utils/actions/transform/TranslateAction";
+import {CreateGroupSelectAction,
+        CreateDeselectAllAction} from "../utils/actions/selection/SelectActionsFactory";
 import {CreateDeleteGroupAction} from "../utils/actions/deletion/DeleteGroupActionFactory";
 import {CreateAddGroupAction} from "../utils/actions/addition/AddGroupActionFactory";
-import {TranslateAction} from "../utils/actions/transform/TranslateAction";
+import {TransferICDataAction} from "../utils/actions/TransferICDataAction";
 
 import {CircuitDesigner} from "../models/CircuitDesigner";
-
+import {IOObject} from "../models/ioobjects/IOObject";
 import {IC} from "../models/ioobjects/other/IC";
 
 import {MainDesignerController} from "./MainDesignerController";
@@ -15,10 +21,11 @@ import {MainDesignerController} from "./MainDesignerController";
 export const CopyController = (() => {
     const copy = function(e: ClipboardEvent): void {
         const selections = MainDesignerController.GetSelections();
+        const objs = selections.filter((o) => o instanceof IOObject) as Array<IOObject>;
 
         // Create sub-circuit with just selections to save
         const designer = new CircuitDesigner(-1);
-        designer.addGroup(CopyGroup(selections));
+        designer.addGroup(CopyGroup(objs));
 
 
         // Add all necessary IC data
@@ -38,12 +45,13 @@ export const CopyController = (() => {
 
     const cut = function(e: ClipboardEvent): void {
         const selections = MainDesignerController.GetSelections();
+        const objs = selections.filter((o) => o instanceof IOObject) as Array<IOObject>;
 
         copy(e);
 
         // Delete the selections
         MainDesignerController.AddAction(
-            CreateDeleteGroupAction(selections).execute()
+            CreateDeleteGroupAction(objs).execute()
         );
     }
 
@@ -55,11 +63,29 @@ export const CopyController = (() => {
         Importer.read(designer, contents);
 
         const group = CopyGroup(designer.getGroup());
+        const objs = group.getAllComponents();
 
-        const action = CreateAddGroupAction(mainDesigner, group);
-        // action.add()
+        const action = new GroupAction();
+
+        // Transfer the IC data over
+        action.add(new TransferICDataAction(designer, mainDesigner));
+
+        // Add each wire and object
+        action.add(CreateAddGroupAction(mainDesigner, group));
+
+        // Deselect current selections
+        action.add(CreateDeselectAllAction(MainDesignerController.GetSelectionTool()));
+
+        // Select everything that was added
+        action.add(CreateGroupSelectAction(
+                        MainDesignerController.GetSelectionTool(), objs));
+
+        // Translate the copies over a bit
+        action.add(new TranslateAction(objs, objs.map((o) => o.getPos()),
+                                        objs.map((o) => o.getPos().add(V(5, 5)))));
 
         MainDesignerController.AddAction(action.execute());
+        MainDesignerController.Render();
     }
 
     return {
