a191513678155cfef3c4898b12948869f567dfb2
LeonMontealegre <leonm99@gmail.com>
2019-07-12 18:17:09 -0400

    Added more tests for FakeInput

--- tests/public/ts/utils/FakeInput.test.ts
+++ tests/public/ts/utils/FakeInput.test.ts
@@ -1,18 +1,25 @@
 import "jest";
 
-import {V} from "../../../../site/public/ts/utils/math/Vector";
+import {Vector, V} from "../../../../site/public/ts/utils/math/Vector";
 
 import {FakeInput} from "./FakeInput";
 
 describe("Fake Input", () => {
     const input = new FakeInput();
 
+    let positions: Array<Vector> = [];
     let steps = 0;
-    input.addListener("mousemove", () => {
+    function step(): void {
+        positions.push(input.getMousePos());
         steps++;
-    });
+    }
+
+    input.addListener("mousedown", () => step());
+    input.addListener("mousemove", () => step());
+    input.addListener("mouseup",   () => step());
 
     beforeEach(() => {
+        positions = [];
         steps = 0;
     });
 
@@ -21,6 +28,15 @@ describe("Fake Input", () => {
 
         expect(steps).toBe(5);
         expect(input.getMousePos()).toEqual(V(20, 20));
+    });
+
+    test("Drag", () => {
+        input.drag(V(-20, 20),
+                   V(20, -20), 5);
+
+        expect(steps).toBe(7);
+        expect(positions[0]).toEqual(V(-20, 20));
+        expect(input.getMousePos()).toEqual(V(20, -20));
     });
 
 });
