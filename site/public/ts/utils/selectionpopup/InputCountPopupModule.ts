3644bc757b681f61596f76dec91701e3ba541256
LeonMontealegre <leonm99@gmail.com>
2019-07-19 17:48:35 -0400

    Fixed issue with clamping min/max values on Input Count

--- site/public/ts/utils/selectionpopup/InputCountPopupModule.ts
+++ site/public/ts/utils/selectionpopup/InputCountPopupModule.ts
@@ -1,12 +1,17 @@
+import {ClampedValue} from "../ClampedValue";
+
+import {GroupAction} from "../actions/GroupAction";
+import {InputPortChangeAction} from "../actions/ports/InputPortChangeAction";
+import {SelectPortChangeAction} from "../actions/ports/SelectPortChangeAction";
+
 import {MainDesignerController} from "../../controllers/MainDesignerController";
-import {SelectionPopupModule} from "./SelectionPopupModule";
+
 import {Gate} from "../../models/ioobjects/gates/Gate";
 import {BUFGate} from "../../models/ioobjects/gates/BUFGate";
 import {Decoder} from "../../models/ioobjects/other/Decoder";
 import {Mux} from "../../models/ioobjects/other/Mux";
-import {GroupAction} from "../actions/GroupAction";
-import {InputPortChangeAction} from "../actions/ports/InputPortChangeAction";
-import {SelectPortChangeAction} from "../actions/ports/SelectPortChangeAction";
+
+import {SelectionPopupModule} from "./SelectionPopupModule";
 
 export class InputCountPopupModule extends SelectionPopupModule {
     private count: HTMLInputElement;
@@ -30,18 +35,27 @@ export class InputCountPopupModule extends SelectionPopupModule {
                 .filter(o => o instanceof Decoder)
                 .map(o => o as Decoder);
 
-        const enable = selections.length == gates.length + muxes.length + decos.length && selections.length > 0;
+        // Only enable if there's exactly 1 type, so just Gates or just Muxes or just Decoders
+        const enable = selections.length > 0 && (selections.length == gates.length ||
+                                                 selections.length == muxes.length ||
+                                                 selections.length == decos.length);
 
         if (enable) {
             // Calculate input counts for each component
-            const counts: Array<number> = [];
+            const counts: Array<ClampedValue> = [];
             gates.forEach(g => counts.push(g.getInputPortCount()));
             muxes.forEach(m => counts.push(m.getSelectPortCount()));
             decos.forEach(d => counts.push(d.getInputPortCount()));
 
-            const same = counts.every((count) => count === counts[0]);
+            const same = counts.every((count) => count.getValue() === counts[0].getValue());
+            const min = counts.reduce((min, v) => Math.max(v.getMinValue(), min), -Infinity); // Find max minimum
+            const max = counts.reduce((max, v) => Math.min(v.getMaxValue(), max), +Infinity); // Find min maximum
 
-            this.count.value = same ? counts[0].toString() : "-";
+            this.count.value = same ? counts[0].getValue().toString() : "";
+            this.count.placeholder = same ? "" : "-";
+
+            this.count.min = min.toString();
+            this.count.max = max.toString();
         }
 
         this.setEnabled(enable);
