3762fb2a69450bf44f0b9d3c46935d9a0201081d
Leon Montealegre <leonm99@gmail.com>
2019-07-23 01:43:41 -0400

    Added sign out ability and promise resolution once GAPI has loaded

--- site/public/ts/controllers/LoginController.ts
+++ site/public/ts/controllers/LoginController.ts
@@ -2,7 +2,9 @@ export const LoginController = (() => {
     const loginPopup = document.getElementById("login-popup");
     const overlay = document.getElementById("overlay");
 
+    const loginHeaderContainer = document.getElementById("header-login-container");
     const loginHeaderButton = document.getElementById("header-signin-button");
+    const logoutHeaderButton = document.getElementById("header-signout-button");
 
     let isOpen = false;
     let disabled = false;
@@ -12,21 +14,35 @@ export const LoginController = (() => {
         overlay.classList.toggle("invisible");
     }
 
-    const onGapiSuccess = function(u: gapi.auth2.GoogleUser): void {
+    const onLogin = function(u: gapi.auth2.GoogleUser): void {
         console.log(u.getBasicProfile().getName());
+        loginHeaderContainer.classList.add("hide");
+        logoutHeaderButton.classList.remove("hide");
+        if (LoginController.IsOpen())
+            LoginController.Toggle();
     }
 
-    const onGapiError = function(e: {error: string}): void {
+    const onLogout = function(): void {
+        loginHeaderContainer.classList.remove("hide");
+        logoutHeaderButton.classList.add("hide");
+    }
+
+    const onLoginError = function(e: {error: string}): void {
         console.log(e);
     }
 
     return {
-        Init: function(): void {
+        Init: function(resolve: (value?: unknown) => void): void {
             isOpen = false;
 
             loginHeaderButton.onclick = () => {
                 LoginController.Toggle();
             };
+
+            logoutHeaderButton.onclick = () => {
+                const auth2 = gapi.auth2.getAuthInstance();
+                auth2.signOut().then(onLogout);
+            }
 
             overlay.addEventListener("click", () => {
                 if (LoginController.IsOpen())
@@ -39,8 +55,18 @@ export const LoginController = (() => {
                     'width': 240,
                     'height': 50,
                     'longtitle': true,
-                    'onsuccess': onGapiSuccess,
-                    'onfailure': onGapiError
+                    'onsuccess': onLogin,
+                    'onfailure': onLoginError
+                });
+
+                gapi.load('auth2', () => {
+                    const clientId = document.getElementsByName('google-signin-client_id')[0].getAttribute('content');
+                    gapi.auth2.init({
+                        client_id: clientId
+                    }).then((auth2) => {
+                        console.log(auth2.isSignedIn.get());
+                        resolve(1);
+                    });
                 });
             }
         },
