d0471ba3038a6cac453469d9414773d86db0a3a5
tyler99b <bejelt@rpi.edu>
2019-10-04 16:16:04 -0400

    Overloaded getMinPos() and getMaxPos() for OR and XOR gates to fix the cullboxes

--- app/digital/ts/models/ioobjects/gates/ORGate.ts
+++ app/digital/ts/models/ioobjects/gates/ORGate.ts
@@ -1,4 +1,7 @@
-import {V} from "Vector";
+import {DEFAULT_BORDER_WIDTH,
+        IO_PORT_RADIUS,
+        IO_PORT_BORDER_WIDTH} from "../../../utils/Constants";
+import {Vector, V} from "Vector";
 import {ClampedValue} from "math/ClampedValue";
 import {Gate} from "./Gate";
 
@@ -22,6 +25,52 @@ export class ORGate extends Gate {
 
     public getImageName(): string {
         return "or.svg";
+    }
+
+    public getMinPos(): Vector {
+        const min = V(Infinity);
+
+        // Find minimum pos from corners of transform
+        var BOX_WIDTH = DEFAULT_BORDER_WIDTH;
+        if(this.numInputs() >3 && this.numInputs() < 7)
+            BOX_WIDTH += 50;
+        if(this.numInputs() == 8)
+            BOX_WIDTH += 100;
+
+        const corners = this.transform.getCorners().map(
+            v => v.sub(DEFAULT_BORDER_WIDTH,BOX_WIDTH)
+        );
+
+        // corners[0][0] -= BOX_WIDTH;
+
+        // Find minimum pos from ports
+        const ports = this.getPorts().map(
+            p => p.getWorldTargetPos().sub(IO_PORT_RADIUS+IO_PORT_BORDER_WIDTH)
+        );
+
+        return Vector.min(min, ...corners, ...ports);
+    }
+
+    public getMaxPos(): Vector {
+        const max = V(-Infinity);
+
+        var BOX_WIDTH = DEFAULT_BORDER_WIDTH;
+        if(this.numInputs() >3 && this.numInputs() < 7)
+            BOX_WIDTH += 50;
+        if(this.numInputs() == 8)
+            BOX_WIDTH += 100;
+
+        // Find maximum pos from corners of transform
+        const corners = this.transform.getCorners().map(
+            v => v.add(DEFAULT_BORDER_WIDTH,BOX_WIDTH)
+        );
+
+        // Find maximum pos from ports
+        const ports = this.getPorts().map(
+            p => p.getWorldTargetPos().add(IO_PORT_RADIUS+IO_PORT_BORDER_WIDTH)
+        );
+
+        return Vector.max(max, ...corners, ...ports);
     }
 
     public getXMLName(): string {
