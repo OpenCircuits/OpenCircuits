b25b8301369142123e133e8a820d1a4b1baa51cb
tyler99b <bejelt@rpi.edu>
2019-10-20 17:15:50 -0400

    modified getMaxPos for XOR gate to call super and removed repeated code

--- app/digital/ts/models/ioobjects/gates/XORGate.ts
+++ app/digital/ts/models/ioobjects/gates/XORGate.ts
@@ -48,25 +48,20 @@ export class XORGate extends Gate {
     }
 
     public getMaxPos(): Vector {
-        const max = V(-Infinity);
+        const max = super.getMaxPos();
 
         var BOX_WIDTH = DEFAULT_BORDER_WIDTH;
         if(this.numInputs() >3 && this.numInputs() < 7)
             BOX_WIDTH += 50;
         if(this.numInputs() == 8)
-            BOX_WIDTH += 100;
+            BOX_WIDTH += 50*2;
 
         // Find maximum pos from corners of transform
         const corners = this.transform.getCorners().map(
             v => v.add(DEFAULT_BORDER_WIDTH,BOX_WIDTH)
         );
 
-        // Find maximum pos from ports
-        const ports = this.getPorts().map(
-            p => p.getWorldTargetPos().add(IO_PORT_RADIUS+IO_PORT_BORDER_WIDTH)
-        );
-
-        return Vector.max(max, ...corners, ...ports);
+        return Vector.max(max, ...corners);
     }
 
     public getXMLName(): string {
