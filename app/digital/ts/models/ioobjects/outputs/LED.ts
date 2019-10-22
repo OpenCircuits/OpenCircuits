f513f199ee0f1b5dd77fda7a8fc2b95d842c3af6
rsdexter <rsdexter00@gmail.com>
2019-10-22 17:09:15 -0400

    Added constants and applied them in getMinPos and getMaxPos

--- app/digital/ts/models/ioobjects/outputs/LED.ts
+++ app/digital/ts/models/ioobjects/outputs/LED.ts
@@ -1,7 +1,9 @@
 import {DEFAULT_SIZE,
         DEFAULT_BORDER_WIDTH,
         IO_PORT_RADIUS,
-        IO_PORT_BORDER_WIDTH} from "digital/utils/Constants";
+        IO_PORT_BORDER_WIDTH,
+        LED_LIGHT_RADIUS,
+        LED_BULB_WIDTH} from "digital/utils/Constants";
 
 import {Vector, V} from "Vector";
 import {ClampedValue} from "math/ClampedValue";
@@ -25,10 +27,10 @@ export class LED extends Component {
     public getMinPos(): Vector {
         const min = V(Infinity);
 
-        // if the LED is on, create a new border width
+        // if the LED is on, create a new border width to account for the light
         let newBorderWidth = DEFAULT_BORDER_WIDTH;
         if (this.inputs.first.getIsOn())
-            newBorderWidth += 50;
+            newBorderWidth += (LED_LIGHT_RADIUS - LED_BULB_WIDTH/2);
 
         // find the corners of the object using the new border width
         const corners = this.transform.getCorners().map(
@@ -48,10 +50,10 @@ export class LED extends Component {
     public getMaxPos(): Vector {
         const max = V(-Infinity);
 
-        // if the LED is on, create a new border width
+        // if the LED is on, create a new border width to account for the light
         let newBorderWidth = DEFAULT_BORDER_WIDTH;
         if (this.inputs.first.getIsOn())
-            newBorderWidth += 50;
+            newBorderWidth += (LED_LIGHT_RADIUS - LED_BULB_WIDTH/2);
 
         // Find maximum pos from corners of transform
         const corners = this.transform.getCorners().map(
