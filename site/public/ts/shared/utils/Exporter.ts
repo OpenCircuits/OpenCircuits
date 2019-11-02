69430ee311d8028b177cf03d5bfe1866f17b8c0d
Audrey Baxter <baxtea@pm.me>
2019-11-01 20:56:02 -0400

    background and grid correct, also improved fit in thumbnail

--- site/public/ts/shared/utils/Exporter.ts
+++ site/public/ts/shared/utils/Exporter.ts
@@ -2,6 +2,7 @@ import {XMLWriter} from "../../../../../app/core/ts/utils/io/xml/XMLWriter";
 import {DigitalCircuitDesigner} from "digital/models/DigitalCircuitDesigner";
 import {DigitalCircuitView} from "site/digital/views/DigitalCircuitView";
 import {V} from "Vector";
+import {Browser} from "core/utils/Browser";
 
 // Returns a 256x256 canvas on which the given circuit is centered
 //! No idea what happens if the designer is empty, divide-by-zero seems plausible
@@ -17,8 +18,8 @@ function RenderCircuit(designer: DigitalCircuitDesigner): HTMLCanvasElement {
     let ymax = -Infinity;
     for (let c of designer.getObjects()) {
         const cullbox = c.getCullBox();
-        const min = cullbox.getTopLeft();
-        const max = cullbox.getBottomRight();
+        const max = cullbox.getTopRight();
+        const min = cullbox.getBottomLeft();
         xmin = Math.min(min.x, xmin);
         ymin = Math.min(min.y, ymin);
         xmax = Math.max(max.x, xmax);
@@ -26,6 +27,7 @@ function RenderCircuit(designer: DigitalCircuitDesigner): HTMLCanvasElement {
     }
 
     //! Warning: several pieces of CircuitView classes rely on there being a screen and a window
+    // I think using this vw and vh cancels it out, but I'm not confident in that
     const canvas = document.createElement("canvas");
     const vw = THUMBNAIL_SIZE/window.innerWidth;
     const vh = THUMBNAIL_SIZE/window.innerHeight;
@@ -35,12 +37,23 @@ function RenderCircuit(designer: DigitalCircuitDesigner): HTMLCanvasElement {
     const center = V((xmin + xmax)/2, (ymin + ymax)/2);
     const camera = view.getCamera();
     camera.setPos(center);
-    const zoom = Math.max((xmax - xmin)/THUMBNAIL_SIZE, (ymax - ymin)/THUMBNAIL_SIZE) * 1.1; // zoom out 10% more than required
-    camera.zoomBy(zoom); // zoomTo would be ideal but does calculations in screen space, whereas zoomBy is entirely world space
+    // Zoom out 2.5% more than we need, just for a little padding
+    const zoom = Math.max((xmax - xmin)/THUMBNAIL_SIZE, (ymax - ymin)/THUMBNAIL_SIZE) * 1.025;
+    camera.zoomBy(zoom);
 
-    // Do the actual render
+    // Create background the same color as the site to composite the image on top of
+    // NOTE: can't just fill the first canvas before the render since the first thing the renderer does it clear it
+    const composite = document.createElement("canvas");
+    composite.width = THUMBNAIL_SIZE;
+    composite.height = THUMBNAIL_SIZE;
+    const context = composite.getContext("2d");
+    context.fillStyle = window.getComputedStyle(document.body, null).getPropertyValue("background-color");
+    context.fillRect(0, 0, THUMBNAIL_SIZE, THUMBNAIL_SIZE);
+
+    // Do the render, and composite it on top of the background
     view.render(designer, [], undefined);
-    return canvas;
+    context.drawImage(canvas, 0, 0, THUMBNAIL_SIZE, THUMBNAIL_SIZE);
+    return composite;
 }
 
 export function WriteCircuit(designer: DigitalCircuitDesigner, name: string): string {
