86f7b57aabcec642b0ecfc53609a5c6282893f23
unknown <rosan2@rpi.edu>
2019-11-05 16:27:53 -0500

    Added more diagonal types

--- app/digital/ts/models/ioobjects/outputs/SegmentDisplay.ts
+++ app/digital/ts/models/ioobjects/outputs/SegmentDisplay.ts
@@ -11,21 +11,22 @@ import {InputPort} from "../../ports/InputPort";
 
 import {DigitalComponent} from "digital/models/DigitalComponent";
 
-export type SegmentType = "vertical" | "horizontal" | "diagonal" | "horizontal0.5";
+export type SegmentType = "vertical" | "horizontal" | "diagonaltr" | "diagonaltl" | "diagonalbr" | "diagonalbl" | "horizontal0.5";
 
 export class SegmentDisplay extends DigitalComponent {
     public constructor() {
-        super(new ClampedValue(7),
+        super(new ClampedValue(9),
               new ClampedValue(0),
               V(70, 100),
               new ConstantSpacePositioner<InputPort>(2*IO_PORT_RADIUS+1));
 
-        this.setInputPortCount(7);
+        this.setInputPortCount(9);
     }
 
     public getSegments(): Array<[Vector, SegmentType]> {
-        const segments = Segments["7"];
+        const segments = Segments["9"];
 
+        // Turns the array into an array of Vectors and SegmentTypes
         return segments.map((value: [number[], SegmentType]) =>
             [V(value[0][0], value[0][1]), value[1]]
         );
