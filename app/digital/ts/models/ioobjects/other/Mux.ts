85cb7bf38c19a971d80fc51b8c29dd24567bcebc
Leon Montealegre <leonm99@gmail.com>
2019-11-06 16:38:17 -0500

    Update Mux.ts

--- app/digital/ts/models/ioobjects/other/Mux.ts
+++ app/digital/ts/models/ioobjects/other/Mux.ts
@@ -21,7 +21,7 @@ export abstract class Mux extends DigitalComponent {
         super(inputPortCount, outputPortCount, V(DEFAULT_SIZE+10, 2*DEFAULT_SIZE), inputPositioner, outputPositioner);
 
         this.selects = new PortSet<InputPort>(this, new ClampedValue(2, 1, 8), new MuxSelectPositioner(), InputPort);
-        
+
         this.setSelectPortCount(2);
     }
 
@@ -54,22 +54,20 @@ export abstract class Mux extends DigitalComponent {
     // @Override
     public copy(): Mux {
         const copy = <Mux>super.copy();
-
         copy.selects = this.selects.copy(copy);
-
         return copy;
     }
 
     // @Override
     public save(node: XMLNode): void {
         super.save(node);
-        node.addAttribute("selects",this.numSelects());
+        node.addAttribute("selects", this.numSelects());
     }
 
     // @Override
     public load(node: XMLNode): void {
         super.load(node);
-        this.setSelectPortCount(node.getIntAttribute("selects"))
+        this.setSelectPortCount(node.getIntAttribute("selects"));
     }
 
 }
