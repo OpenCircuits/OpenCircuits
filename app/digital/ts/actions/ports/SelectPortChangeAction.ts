dd75d6ab2a1372952f85fc312f3e7199f1ca30df
Leon Montealegre <leonm99@gmail.com>
2019-11-07 13:55:01 -0500

    More changes to select port change action

--- app/digital/ts/actions/ports/SelectPortChangeAction.ts
+++ app/digital/ts/actions/ports/SelectPortChangeAction.ts
@@ -12,17 +12,21 @@ export class SelectPortChangeAction extends DigitalPortChangeAction {
     public constructor(obj: Mux, target: number) {
         super(obj, target, obj.getSelectPorts().length);
 
+        this.inputAction = new InputPortChangeAction(obj, Math.pow(2, target));
+
         this.action = super.createAction(this.obj.getSelectPorts(),
                                          this.targetCount);
     }
 
     public execute(): Action {
         super.execute();
+        this.inputAction.execute();
         this.obj.setSelectPortCount(this.targetCount);
         return this;
     }
 
     public undo(): Action {
+        this.inputAction.undo();
         this.obj.setSelectPortCount(this.initialCount);
         super.undo();
         return this;
