38075db9e0761d03bc4d60783425f599ce6b0353
rsdexter <rsdexter00@gmail.com>
2019-11-08 16:55:41 -0500

    created positioner for MUX output ports

--- app/digital/ts/models/ports/positioners/MuxPositioners.ts
+++ app/digital/ts/models/ports/positioners/MuxPositioners.ts
@@ -5,8 +5,10 @@ import {V} from "Vector";
 
 import {Port} from "core/models/ports/Port";
 import {InputPort} from "../InputPort";
+import {OutputPort} from "../OutputPort";
 
 import {Positioner} from "core/models/ports/positioners/Positioner";
+
 
 export class MuxPositioner<T extends Port> extends Positioner<T> {
 
@@ -48,4 +50,33 @@ export class MuxSelectPositioner extends Positioner<InputPort> {
         });
     }
 
+}
+
+export class MuxOutputPositioner extends Positioner<OutputPort> {
+    /**
+     * Port positioning for Multiplexer output ports
+     * 
+     * @paramm arr The array of output ports
+     */
+    public updatePortPositions(ports: Array<OutputPort>): void {
+        ports.forEach((port, i) => {
+            const height = port.getParent().getSize().y;
+
+            // Calculate x position of port
+
+            // Calculate y position of the port
+            //let y = -height/2*(i - ports.length/2 + 0.5)
+
+            //if(port.getParent().getInputPortCount().getValue() <= Math.pow(2,2)){
+            let l = port.getParent().getSize().x + 10*port.getParent().getInputPortCount().getValue();
+            
+
+
+            /*
+                TODO: get the normal x value then add, not current x value
+                      change the origin position based on IO_PORT_LENGTH so its not too long
+            */
+            port.setTargetPos(V(l, port.getTargetPos().y));
+        });
+    }
 }
