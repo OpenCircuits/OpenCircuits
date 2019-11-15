ebd237e09b010339b412f48d5efb847fdd055e39
Leon Montealegre <leonm99@gmail.com>
2019-11-14 21:23:23 -0500

    Added new test

--- tests/app/digital/ts/utils/actions/SelectPortChangeAction.test.ts
+++ tests/app/digital/ts/utils/actions/SelectPortChangeAction.test.ts
@@ -2,13 +2,13 @@ import "jest";
 
 import {DigitalCircuitDesigner} from "digital/models/DigitalCircuitDesigner";
 import {Switch}          from "digital/models/ioobjects/inputs/Switch";
-import {LED}             from "digital/models/ioobjects/outputs/LED";
+import {Multiplexer} from "digital/models/ioobjects/other/Multiplexer";
+import {DigitalNode} from "digital/models/ioobjects/other/DigitalNode";
 
 import {ConnectionAction} from "core/actions/addition/ConnectionAction";
-
-import {Place} from "test/helpers/Helpers";
-import {Multiplexer} from "digital/models/ioobjects/other/Multiplexer";
 import {SelectPortChangeAction} from "digital/actions/ports/SelectPortChangeAction";
+
+import {Place, Connect} from "test/helpers/Helpers";
 
 describe("Select Port Change Action", () => {
     test("Undo/Redo 1", () => {
@@ -41,5 +41,38 @@ describe("Select Port Change Action", () => {
         expect(mux.getSelectPortCount().getValue()).toBe(4);
         expect(mux.getInputPortCount().getValue()).toBe(16);
         expect(mux.getOutputPortCount().getValue()).toBe(1);
+    });
+    test("Undo/Redo 1", () => {
+        const designer = new DigitalCircuitDesigner(0);
+        const sw = new Switch();
+        const n = new DigitalNode();
+        const mux = new Multiplexer();
+
+        Place(designer, [mux, sw, n]);
+
+        // Connect switch to node and then then to input and select ports of Mux
+        Connect(sw, 0, n, 0);
+        Connect(n, 0, mux, 3);
+        new ConnectionAction(n.getOutputPorts()[0], mux.getSelectPorts()[1]).execute();
+
+        expect(designer.getObjects()).toHaveLength(3);
+        expect(designer.getWires()).toHaveLength(3);
+
+        // change select port count
+        const a1 = new SelectPortChangeAction(mux, 1).execute();
+
+        // initial
+        expect(designer.getObjects()).toHaveLength(2);
+        expect(designer.getWires()).toHaveLength(0);
+
+        // reverted
+        a1.undo();
+        expect(designer.getObjects()).toHaveLength(3);
+        expect(designer.getWires()).toHaveLength(3);
+
+        // back to initial
+        a1.execute();
+        expect(designer.getObjects()).toHaveLength(2);
+        expect(designer.getWires()).toHaveLength(0);
     });
 });
