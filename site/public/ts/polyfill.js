29954bbc6bfe1fca10f3e9c514d46c56ee12f562
Leon Montealegre <leonm99@gmail.com>
2019-12-08 15:31:47 -0500

    Added flat and flatMap polyfill for browser compatability

--- site/public/ts/polyfill.js
+++ site/public/ts/polyfill.js
@@ -49,3 +49,33 @@ if (!Array.prototype.includes) {
     }
   });
 }
+
+if (!Array.prototype.flat) {
+	Object.defineProperty(Array.prototype, 'flat', {
+		configurable: true,
+		value: function flat () {
+			var depth = isNaN(arguments[0]) ? 1 : Number(arguments[0]);
+
+			return depth ? Array.prototype.reduce.call(this, function (acc, cur) {
+				if (Array.isArray(cur)) {
+					acc.push.apply(acc, flat.call(cur, depth - 1));
+				} else {
+					acc.push(cur);
+				}
+
+				return acc;
+			}, []) : Array.prototype.slice.call(this);
+		},
+		writable: true
+	});
+}
+
+if (!Array.prototype.flatMap) {
+	Object.defineProperty(Array.prototype, 'flatMap', {
+		configurable: true,
+		value: function flatMap (callback) {
+			return Array.prototype.map.apply(this, arguments).flat();
+		},
+		writable: true
+	});
+}
