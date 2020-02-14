5dcfd47a7eeac757b6c235b8ee7b3dd9e770f5bc
Robert Hammond <hamrobe502@gmail.com>
2020-02-14 17:18:08 -0500

    Added routing for the landing page

--- site/go/web/handlers.go
+++ site/go/web/handlers.go
@@ -115,3 +115,9 @@ func analogHandler(manager auth.AuthenticationManager, examplesCsif interfaces.C
 		})
 	}
 }
+
+func landingPageHandler(manager auth.AuthenticationManager, examplesCsif interfaces.CircuitStorageInterfaceFactory, cache StaticCache) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		c.HTML(http.StatusOK, "landingpage.gohtml", gin.H{})
+	}
+}
