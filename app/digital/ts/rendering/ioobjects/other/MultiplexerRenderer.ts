21f2327095b76f056320bd2a61e2b1b144c10745
Robert Hammond <hamrobe502@gmail.com>
2020-03-27 17:54:31 -0500

    Added binary string labels to both Mux and Demux

--- app/digital/ts/rendering/ioobjects/other/MultiplexerRenderer.ts
+++ app/digital/ts/rendering/ioobjects/other/MultiplexerRenderer.ts
@@ -22,6 +22,7 @@ export const MultiplexerRenderer = (() => {
             const borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
             const fillCol   = (selected ? SELECTED_FILL_COLOR   : DEFAULT_FILL_COLOR);
             const style = new Style(fillCol, borderCol, DEFAULT_BORDER_WIDTH);
+            const selectCount = mul.getSelectPortCount().getValue();
 
             //
             // Creates the Multiplexer and Demultiplexer the correct size
@@ -33,6 +34,17 @@ export const MultiplexerRenderer = (() => {
                 const p4 = V(transform.getSize().x/2 , transform.getSize().y/2 - MULTIPLEXER_HEIGHT_OFFSET);
 
                 renderer.draw(new Polygon([p1, p2, p3, p4]), style);
+
+                const align: CanvasTextAlign = "left";
+                const inputCount = mul.getInputPortCount().getValue();
+                let p = V(-transform.getSize().x/2 + 4, transform.getSize().y/2 - 11);
+                let numStr = "0".repeat(selectCount);
+                for (let i = 0; i < inputCount; i++) {
+                    renderer.text(numStr, p, align);
+                    // increment the binary string
+                    numStr = numStr.substr(0, numStr.lastIndexOf("0")) + "1" + "0".repeat(numStr.length - numStr.lastIndexOf("0") - 1);
+                    p.y -= 25;
+                }
             }
             else {
                 const p1 = V(transform.getSize().x/2 , transform.getSize().y/2 + 7);
@@ -41,6 +53,16 @@ export const MultiplexerRenderer = (() => {
                 const p4 = V(-transform.getSize().x/2, transform.getSize().y/2 - MULTIPLEXER_HEIGHT_OFFSET);
 
                 renderer.draw(new Polygon([p1, p2, p3, p4]), style);
+
+                const align: CanvasTextAlign = "right";
+                const outputCount = mul.getOutputPortCount().getValue();
+                let p = V(transform.getSize().x/2 - 4, transform.getSize().y/2 - 11);
+                let numStr = "0".repeat(selectCount);
+                for (let i = 0; i < outputCount; i++) {
+                    renderer.text(numStr, p, align);
+                    numStr = numStr.substr(0, numStr.lastIndexOf("0")) + "1" + "0".repeat(numStr.length - numStr.lastIndexOf("0") - 1);
+                    p.y -= 25;
+                }
             }
         }
     }
