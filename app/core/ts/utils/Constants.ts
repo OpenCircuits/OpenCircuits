eb11b1023a361486e4d18a0a5476fa12a4fcfd5b
Leon Montealegre <leonm99@gmail.com>
2020-07-05 17:11:02 -0400

    Added DEBUG_NO_FILL option and changed how loading images is done

--- app/core/ts/utils/Constants.ts
+++ app/core/ts/utils/Constants.ts
@@ -5,6 +5,7 @@ export const SAVE_VERSION = "2.1";
 export const DEBUG_CULLBOXES = false;
 export const DEBUG_PRESSABLE_BOUNDS = false;
 export const DEBUG_SELECTION_BOUNDS = false;
+export const DEBUG_NO_FILL = false;
 
 export const WIRE_DIST_THRESHOLD  = 5;
 export const WIRE_DIST_THRESHOLD2 = Math.pow(WIRE_DIST_THRESHOLD, 2);
@@ -13,10 +14,10 @@ export const WIRE_NEWTON_ITERATIONS = 5;
 
 export const DEFAULT_SIZE = 50;
 export const GRID_SIZE    = 50;
-export const DEFAULT_FILL_COLOR    = "#ffffff";
+export const DEFAULT_FILL_COLOR    = (DEBUG_NO_FILL ? undefined : "#ffffff");
 export const DEFAULT_BORDER_COLOR  = "#000000";
 export const DEFAULT_ON_COLOR      = "#3cacf2";
-export const SELECTED_FILL_COLOR   = "#1cff3e";
+export const SELECTED_FILL_COLOR   = (DEBUG_NO_FILL ? undefined : "#1cff3e");
 export const SELECTED_BORDER_COLOR = "#0d7f1f";
 
 export const DEFAULT_BORDER_WIDTH = 2;
