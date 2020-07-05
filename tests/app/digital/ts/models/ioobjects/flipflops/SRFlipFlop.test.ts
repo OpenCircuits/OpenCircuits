33dc1754afb10862882fa75ac2e05e98aa8c706c
Leon Montealegre <leonm99@gmail.com>
2020-07-05 13:30:11 -0400

    Fixed flip flop tests

--- tests/app/digital/ts/models/ioobjects/flipflops/SRFlipFlop.test.ts
+++ tests/app/digital/ts/models/ioobjects/flipflops/SRFlipFlop.test.ts
@@ -7,70 +7,77 @@ import {LED}             from "digital/models/ioobjects/outputs/LED";
 
 import {Place, Connect} from "test/helpers/Helpers";
 
-describe("SRFlipFLop", () => {
-    const designer = new DigitalCircuitDesigner(0);
-    const clk = new Switch(), s = new Switch(), r = new Switch(),
-    	f = new SRFlipFlop(), l0 = new LED(), l1 = new LED();
+describe("SRFlipFlop", () => {
+    const ON = true, OFF = false;
 
-    Place(designer, [clk, s, r, f, l1, l0]);
-    Connect(clk, 0,  f, 1);
-    Connect(s, 0,  f, 0);
-    Connect(r, 0,  f, 2);
-    Connect(f, 0,  l0, 0);
-    Connect(f, 1,  l1, 0);
+    const designer = new DigitalCircuitDesigner(0);
+    const C = new Switch(), S = new Switch(), R = new Switch(), PRE = new Switch();
+    const f = new SRFlipFlop(), CLR = new Switch(), Q = new LED(), Q2 = new LED();
+
+    Place(designer, [C, S, R, PRE, CLR, f, Q, Q2]);
+    Connect(C,  0, f, SRFlipFlop.CLK_PORT);
+    Connect(S, 0, f, SRFlipFlop.SET_PORT);
+    Connect(R, 0, f, SRFlipFlop.RST_PORT);
+    Connect(PRE, 0, f, SRFlipFlop.PRE_PORT);
+    Connect(CLR, 0, f, SRFlipFlop.CLR_PORT);
+    Connect(f, SRFlipFlop.Q_PORT,  Q, 0);
+    Connect(f, SRFlipFlop.Q2_PORT, Q2, 0);
+
+    function expectState(state: boolean): void {
+        expect(Q.isOn()).toBe(state);
+        expect(Q2.isOn()).toBe(!state);
+    }
 
     test("Initial State", () => {
-        expect(l1.isOn()).toBe(false);
-        expect(l0.isOn()).toBe(false);
+        expectState(OFF);
     });
     test("Turn On an Input", () => {
-        clk.activate(false);
-        s.activate(true);
-        s.activate(false);
+        C.activate(OFF);
+        S.activate(ON);
+        S.activate(OFF);
 
-        expect(l1.isOn()).toBe(true);
-        expect(l0.isOn()).toBe(false);
+        expectState(OFF);
     });
     test("Set", () => {
-		s.activate(true);
-		clk.activate(true);
-        s.activate(false);
-        clk.activate(false);
+        S.activate(ON);
+        C.activate(ON);
+        S.activate(OFF);
+        C.activate(OFF);
 
-        expect(l1.isOn()).toBe(false);
-        expect(l0.isOn()).toBe(true);
+        expectState(ON);
     });
     test("Set while On, Reset falling edge", () => {
-    	s.activate(true);
-		clk.activate(true);
-        s.activate(false);
-        r.activate(true);
-        clk.activate(false);
+        S.activate(ON);
+        C.activate(ON);
+        S.activate(OFF);
+        R.activate(ON);
+        C.activate(OFF);
 
-        expect(l1.isOn()).toBe(false);
-        expect(l0.isOn()).toBe(true);
+        expectState(ON);
     });
     test("Reset", () => {
-    	clk.activate(true);
-        r.activate(false);
-        clk.activate(false);
+        C.activate(ON);
+        R.activate(OFF);
+        C.activate(OFF);
 
-        expect(l1.isOn()).toBe(true);
-        expect(l0.isOn()).toBe(false);
+        expectState(OFF);
     });
     test("Reset while Off, Set falling edge", () => {
-		r.activate(true);
-		clk.activate(true);
-        r.activate(false);
-        s.activate(true);
-        clk.activate(false);
+        R.activate(ON);
+        C.activate(ON);
+        R.activate(OFF);
+        S.activate(ON);
+        C.activate(OFF);
 
-        expect(l1.isOn()).toBe(true);
-        expect(l0.isOn()).toBe(false);
+        expectState(OFF);
     });
     test("Set and Reset, undefined behavior", () => {
-        r.activate(true);
-        clk.activate(true);
-        clk.activate(false);
+        R.activate(ON);
+        S.activate(ON)
+
+        C.activate(ON);
+        C.activate(OFF);
+
+        expect.anything();
     });
 });
