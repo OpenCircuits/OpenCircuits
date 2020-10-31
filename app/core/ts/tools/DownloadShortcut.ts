bb35f83b101f863e35b7646f2d41fb3876bd32e7
ml619 <70662865+ml619@users.noreply.github.com>
2020-10-31 19:01:36 -0400

    Update DownloadShortcut.ts

--- app/core/ts/tools/DownloadShortcut.ts
+++ app/core/ts/tools/DownloadShortcut.ts
@@ -1,10 +1,7 @@
 import {Input} from "core/utils/Input";
 import {S_KEY} from "core/utils/Constants";
 import {MainDesignerController} from "site/shared/controllers/MainDesignerController";
-import {LoginController} from "site/shared/controllers/LoginController";
 import {RemoteController} from "site/shared/controllers/RemoteController";
-import {addSetSavedCallback, setSAVED} from "core/utils/Config";
-import {HeaderController} from "site/shared/controllers/HeaderController";
 import {SaveFile} from "site/shared//utils/Exporter";
 export function DownloadShortcut(main: MainDesignerController, project_name: string, input: Input, key?: number): boolean
 {
