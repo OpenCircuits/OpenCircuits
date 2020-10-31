f45edec786ba0966c40c08730ef9ed6ad7e964bf
brandon-red <brandonred627@gmail.com>
2020-10-31 14:59:35 -0400

    Created renderer file for BCDDisplay

---
+++ app/digital/ts/rendering/ioobjects/outputs/BCDDisplayRenderer.ts
@@ -0,0 +1,19 @@
+import {DEFAULT_BORDER_WIDTH,
+    DEFAULT_BORDER_COLOR,
+    DEFAULT_FILL_COLOR,
+    SELECTED_BORDER_COLOR,
+    SELECTED_FILL_COLOR,
+    DEFAULT_ON_COLOR,
+    SEGMENT_DISPLAY_WIDTH} from "core/utils/Constants";
+import {V} from "Vector";
+
+import {Camera} from "math/Camera";
+
+import {Renderer} from "core/rendering/Renderer";
+import {Rectangle} from "core/rendering/shapes/Rectangle";
+import {Style} from "core/rendering/Style";
+
+import {BCDDisplay} from "digital/models/ioobjects/outputs/BCDDisplay";
+
+import {Images} from "digital/utils/Images";
+import {Line} from "core/rendering/shapes/Line";
