716b96f779da46aab582da6c7a12e7ffdc9877c1
ml619 <70662865+ml619@users.noreply.github.com>
2020-10-31 19:04:16 -0400

    Update Saveshortcut.ts

--- app/core/ts/tools/Saveshortcut.ts
+++ app/core/ts/tools/Saveshortcut.ts
@@ -1,12 +1,9 @@
 import {Input} from "core/utils/Input";
 import {S_KEY} from "core/utils/Constants";
 import {MainDesignerController} from "site/shared/controllers/MainDesignerController";
-import {LoginController} from "site/shared/controllers/LoginController";
 import {RemoteController} from "site/shared/controllers/RemoteController";
 import {addSetSavedCallback, setSAVED} from "core/utils/Config";
-import {HeaderController} from "site/shared/controllers/HeaderController";
 import {SideNavController} from "site/shared/controllers/SideNavController";
-import {SaveFile} from "site/shared//utils/Exporter";
 export function SaveShortcut(main: MainDesignerController, sidenav: SideNavController, input: Input, key?: number): boolean
 {
     const promise = new Promise((resolve,reject) =>{
