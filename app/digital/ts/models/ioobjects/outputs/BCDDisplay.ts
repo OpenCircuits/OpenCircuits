3ecbff7503a5660741f6745bb101143faf929be3
brandon-red <brandonred627@gmail.com>
2020-11-22 20:18:00 -0500

    Added constructor, getDisplayName, and setInputPortCount functions for the BCD Display main file

--- app/digital/ts/models/ioobjects/outputs/BCDDisplay.ts
+++ app/digital/ts/models/ioobjects/outputs/BCDDisplay.ts
@@ -8,11 +8,35 @@ import {ClampedValue} from "math/ClampedValue";
 import {Name} from "core/utils/Name";
 
 import {BCDDisplayPositioner} from "digital/models/ports/positioners/BCDDisplayPositioner";
-import { SegmentDisplay } from "./SegmentDisplay";
+import { DigitalComponent } from "digital/models/DigitalComponent";
 
 export type SegmentType = "vertical" | "horizontal" | "diagonaltr" | "diagonaltl" | "diagonalbr" | "diagonalbl" | "horizontal0.5";
 
 @serializable("BCDDisplay")
-export class BCDDisplay extends SegmentDisplay {
+export class BCDDisplay extends DigitalComponent{
 
+    public constructor(){
+        super(new ClampedValue(7, 7, 16),
+              new ClampedValue(0),
+              V(70, 100),
+              new BCDDisplayPositioner());
+
+        this.setInputPortCount(7);
+    }
+
+    public setInputPortCount(val: number): void {
+        super.setInputPortCount(val);
+        // We do not want to reset the user typed name so we check
+        //  if it was set in the first place
+        if (!this.name.isSet())
+            this.name = new Name(this.getDisplayName());
+    }
+
+    //public getBinaryNum(): number {}
+    //Insert code for getting the binary number based on the inputs.
+
+    public getDisplayName(): string {
+        if (this.inputs == undefined)
+            return "BCD Display"
+        return this.getInputPorts().length + " BCD Display";
 }
