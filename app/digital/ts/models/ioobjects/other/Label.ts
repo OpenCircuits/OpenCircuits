529fce6b534bebcd4487b3570446c2c216ebd3d6
ml619 <70662865+ml619@users.noreply.github.com>
2020-12-11 18:48:28 -0500

    Add files via upload

--- app/digital/ts/models/ioobjects/other/Label.ts
+++ app/digital/ts/models/ioobjects/other/Label.ts
@@ -6,12 +6,24 @@ import {serializable} from "serialeazy";
 
 @serializable("Label")
 export class Label extends DigitalComponent {
+    
+    private color: string;
 
     public constructor() {
         super(new ClampedValue(0), new ClampedValue(0), V(60, 30));
+        this.color = "#ffffff"; 
     }
 
     public getDisplayName(): string {
         return "LABEL";
     }
+
+    public getColor(): string {
+        return this.color;
+    }
+
+    public setColor(color: string): void {
+        this.color = color;
+    }
+
 }
