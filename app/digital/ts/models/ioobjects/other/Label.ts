e611a27b99422fab7874879f54581ee6f560614b
Leon Montealegre <leonm99@gmail.com>
2020-12-16 02:31:37 -0500

    Rearranged methods in Label class

--- app/digital/ts/models/ioobjects/other/Label.ts
+++ app/digital/ts/models/ioobjects/other/Label.ts
@@ -1,37 +1,40 @@
+import {serializable} from "serialeazy";
+
 import {V} from "Vector";
 import {ClampedValue} from "math/ClampedValue";
 
 import {DigitalComponent} from "digital/models/DigitalComponent";
-import {serializable} from "serialeazy";
 
 @serializable("Label")
 export class Label extends DigitalComponent {
-    
     private color: string;
     private textColor: string;
+
     public constructor() {
         super(new ClampedValue(0), new ClampedValue(0), V(60, 30));
-        this.color = "#ffffff"; 
-    }
 
-    public getDisplayName(): string {
-        return "LABEL";
-    }
-
-    public getColor(): string {
-        return this.color;
+        this.color = "#ffffff";
+        this.textColor = "#000000";
     }
 
     public setColor(color: string): void {
         this.color = color;
     }
 
+    public setTextColor(textColor: string): void {
+        this.textColor = textColor;
+    }
+
+    public getColor(): string {
+        return this.color;
+    }
+
     public getTextColor(): string {
         return this.textColor;
     }
 
-    public setTextColor(textColor: string): void {
-        this.textColor = textColor;
+    public getDisplayName(): string {
+        return "LABEL";
     }
 
 }
