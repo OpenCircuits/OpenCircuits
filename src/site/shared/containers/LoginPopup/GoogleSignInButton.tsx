e1caae536cb1d08b9e0f96bf468fdafbe875a48c
Leon Montealegre <leonm99@gmail.com>
2021-03-06 18:12:16 -0500

    Fixed issue where google sign in doesn't close popup

--- src/site/shared/containers/LoginPopup/GoogleSignInButton.tsx
+++ src/site/shared/containers/LoginPopup/GoogleSignInButton.tsx
@@ -1,20 +1,12 @@
 import {useEffect} from "react";
-import {connect} from "react-redux";
 
-import {GoogleAuthState} from "shared/api/auth/GoogleAuthState";
-
-import {SharedAppState} from "shared/state";
 import {Login} from "shared/state/UserInfo/actions";
 
 
-type OwnProps = {}
-type StateProps = {}
-type DispatchProps = {
-    Login: typeof Login;
+type Props = {
+    onLogin: (success: boolean) => void;
 }
-
-type Props = StateProps & DispatchProps & OwnProps;
-const _GoogleAuthButton = ({ Login }: Props) => {
+export const GoogleAuthButton = ({ onLogin }: Props) => {
     useEffect(() => {
         // Render sign in button
         gapi.signin2.render("login-popup-google-signin", {
@@ -22,16 +14,10 @@ const _GoogleAuthButton = ({ Login }: Props) => {
             "width": 180,
             "height": 60,
             "longtitle": false,
-            "onsuccess": (_) => Login(new GoogleAuthState()),
-            "onfailure": (e) => { if (e.error !== "popup_closed_by_user") throw new Error(e.error); }
+            "onsuccess": (_) => onLogin(true),
+            "onfailure": (e) => { if (e.error !== "popup_closed_by_user") throw new Error(e.error); onLogin(false); }
         });
     }, [Login]);
 
     return (<div id="login-popup-google-signin"></div>);
 }
-
-
-export const GoogleAuthButton = connect<StateProps, DispatchProps, OwnProps, SharedAppState>(
-    undefined,
-    { Login } as any
-)(_GoogleAuthButton);
--- src/site/shared/containers/LoginPopup/index.tsx
+++ src/site/shared/containers/LoginPopup/index.tsx
@@ -2,6 +2,7 @@ import {Fragment, useRef} from "react";
 import {connect} from "react-redux";
 
 import {NoAuthState} from "shared/api/auth/NoAuthState";
+import {GoogleAuthState} from "shared/api/auth/GoogleAuthState";
 
 import {SharedAppState} from "shared/state";
 import {CloseHeaderPopups} from "shared/state/Header/actions";
@@ -35,22 +36,30 @@ const _LoginPopup = ({curPopup, Login, CloseHeaderPopups}: Props) => {
                close={CloseHeaderPopups}>
             {(process.env.REACT_APP_AUTH_TYPES ?? "").split(" ").map((s, i) => (
                 <Fragment key={`login-popup-auth-${s}`}>
-                    {s === "google" ? (<GoogleAuthButton />) :
-                    <div>
-                        <div className="login__popup__label">NoAuth Login</div>
-                        <div><input ref={input} type="text" placeholder="username" /></div>
-                        <button onClick={() => {
-                            const val = input.current.value.trim();
-                            if (val === "") {
-                                alert("User name must not be blank!")
-                                return;
-                            }
-                            Login(new NoAuthState(val));
-                            CloseHeaderPopups();
-                        }}>
-                            Submit
-                        </button>
-                    </div>}
+                    {s === "google" ? (
+                        <GoogleAuthButton onLogin={(success) => {
+                            if (success) {
+                                Login(new GoogleAuthState());
+                                CloseHeaderPopups();
+                            } // Else don't login or close
+                        }} />
+                    ) : (
+                        <div>
+                            <div className="login__popup__label">NoAuth Login</div>
+                            <div><input ref={input} type="text" placeholder="username" /></div>
+                            <button onClick={() => {
+                                const val = input.current.value.trim();
+                                if (val === "") {
+                                    alert("User name must not be blank!")
+                                    return;
+                                }
+                                Login(new NoAuthState(val));
+                                CloseHeaderPopups();
+                            }}>
+                                Submit
+                            </button>
+                        </div>
+                    )}
                     <hr />
                 </Fragment>
             ))}
