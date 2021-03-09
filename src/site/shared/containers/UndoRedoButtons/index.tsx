cbd746fbc21aa749539fb3c813d072b6e7eadc91
Samuel Mei <Samoooomei@gmail.com>
2021-03-09 00:09:43 -0500

    Basic Button Functionality

---
+++ src/site/shared/containers/UndoRedoButtons/index.tsx
@@ -0,0 +1,41 @@
+import "./index.scss";
+import {CircuitInfo} from "core/utils/CircuitInfo";
+import {connect} from "react-redux";
+import {SharedAppState} from "shared/state";
+
+type OwnProps = {
+    info: CircuitInfo;
+}
+
+type StateProps = {
+    isLocked: boolean;
+}
+type DispatchProps = {}
+
+type Props = StateProps & DispatchProps & OwnProps;
+const _UndoRedoButtons = ({info, isLocked}: Props) => {
+    const {history} = info;
+
+    const doFunc = (func: () => void) => {
+        // Don't do anything if locked
+        func();
+    }
+
+    /* "Undo/Redo" */
+    const onUndo = async () => history.undo();
+    const onRedo = async () => history.redo();
+
+
+    return (
+        <div className="undoButton">
+            <button title="Undo" onClick={() => doFunc(onUndo)}>Undo</button>
+            <button title="Redo" onClick={() => doFunc(onRedo)}>Redo</button>
+        </div>
+    );
+}
+
+export const UndoRedoButtons = connect<StateProps, DispatchProps, OwnProps, SharedAppState>(
+    (state) => ({ isLocked: state.circuit.isLocked }),
+    { }
+)(_UndoRedoButtons);
+
