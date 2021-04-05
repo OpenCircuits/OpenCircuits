c5923156e54395ff1d6c1242cc040a29b67e2825
Samuel Mei <Samoooomei@gmail.com>
2021-04-04 23:29:51 -0400

    Compact Header when on Mobile

--- src/site/shared/containers/Header/Right/index.tsx
+++ src/site/shared/containers/Header/Right/index.tsx
@@ -6,18 +6,41 @@ import {DownloadMenuDropdown} from "./DownloadMenuDropdown";
 import {SignInOutButtons} from "./SignInOutButtons";
 
 import "./index.scss";
+import { useState } from "react";
 
 
 type Props = {
     helpers: CircuitInfoHelpers;
 }
 export const HeaderRight = ({ helpers }: Props) => {
-    return (
-        <div className="header__right">
-            <TutorialDropdown />
-            <OpenFileButton helpers={helpers} />
-            <DownloadMenuDropdown helpers={helpers} />
-            <SignInOutButtons />
-        </div>
-    );
+
+    const [isHidden, setHidden] = useState(true)
+
+    //Desktop
+    if(!window.matchMedia("(max-width: 500px)").matches) {
+        return (
+            <div className="header__right">
+                <TutorialDropdown />
+                <OpenFileButton helpers={helpers} />
+                <DownloadMenuDropdown helpers={helpers} />
+                <SignInOutButtons />
+            </div>
+        );
+    }
+    //Mobile
+    else {
+        return (
+            <div className="header__right__alt">
+                <button type="button" onClick = {() => setHidden(!isHidden)}><img className ="expand" 
+                src={isHidden ? "img/icons/expand.svg" : "img/icons/collapse.svg"} alt = ""/></button>
+                <div id={isHidden ? 'hidden' : 'notHidden' } >
+                    <li><OpenFileButton helpers={helpers} /></li>
+                    <li><TutorialDropdown /></li>
+                    <li><DownloadMenuDropdown helpers={helpers} /></li>
+                    <li><SignInOutButtons /></li>
+                </div>
+            </div>
+        );
+    }
+
 }
