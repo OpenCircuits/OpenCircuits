90fd91d883d5a5699dff69e2f58bd6b012abf0ad
Samuel Mei <Samoooomei@gmail.com>
2021-04-05 13:13:54 -0400

    changed to use css instead of eventlistener

--- src/site/shared/containers/Header/Right/index.tsx
+++ src/site/shared/containers/Header/Right/index.tsx
@@ -6,7 +6,7 @@ import {DownloadMenuDropdown} from "./DownloadMenuDropdown";
 import {SignInOutButtons} from "./SignInOutButtons";
 
 import "./index.scss";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 
 type Props = {
@@ -15,50 +15,25 @@ type Props = {
 export const HeaderRight = ({ helpers }: Props) => {
 
     const [isHidden, setHidden] = useState(true) 
-    const [isDesktop, setDesktop] = useState({
-        height: window.innerHeight,
-        width: window.innerWidth
-    })
 
-    //Rerender on Browsing Resize
-    useEffect(() => {
-        function handleResize() {
-            setDesktop({
-                height: window.innerHeight,
-                width: window.innerWidth
-            })
-        }
-        window.addEventListener('resize', handleResize)
-
-        return function clean() {
-            window.removeEventListener('resize', handleResize)
-        }
-    })
-
-    //Desktop
-    if(isDesktop.width >= 500) {
         return (
-            <div className="header__right">
-                <TutorialDropdown />
-                <OpenFileButton helpers={helpers} />
-                <DownloadMenuDropdown helpers={helpers} />
-                <SignInOutButtons />
-            </div>
-        );
-    }
-    //Mobile
-    else {
-        return (
-            <div className="header__right__alt">
-                <button type="button" onClick = {() => setHidden(!isHidden)}><img className ="expand" 
-                src={isHidden ? "img/icons/expand.svg" : "img/icons/collapse.svg"} alt = ""/></button>
-                <div id={isHidden ? 'hidden' : 'notHidden' } >
-                    <li><OpenFileButton helpers={helpers} /></li>
-                    <li><TutorialDropdown /></li>
-                    <li><DownloadMenuDropdown helpers={helpers} /></li>
-                    <li><SignInOutButtons /></li>
+            <div>  
+                <div className="header__right">
+                    <TutorialDropdown />
+                    <OpenFileButton helpers={helpers} />
+                    <DownloadMenuDropdown helpers={helpers} />
+                    <SignInOutButtons />
                 </div>
-            </div>
+                <div className="header__right__alt">
+                    <button type="button" onClick = {() => setHidden(!isHidden)}><img className ="expand" 
+                    src={isHidden ? "img/icons/expand.svg" : "img/icons/collapse.svg"} alt = ""/></button>
+                    <div id={isHidden ? 'hidden' : 'notHidden' } >
+                        <li><OpenFileButton helpers={helpers} /></li>
+                        <li><TutorialDropdown /></li>
+                        <li><DownloadMenuDropdown helpers={helpers} /></li>
+                        <li><SignInOutButtons /></li>
+                    </div>
+                </div>
+            </div>  
         );
     }
-}
