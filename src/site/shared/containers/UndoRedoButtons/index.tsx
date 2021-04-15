ba9f910fb7ad5c4ebc1bf1dc015c03bdb3dc034f
Leon Montealegre <leonm99@gmail.com>
2021-04-14 20:26:22 -0400

    Changed undo/redo buttons to gray out when disabled

--- src/site/shared/containers/UndoRedoButtons/index.tsx
+++ src/site/shared/containers/UndoRedoButtons/index.tsx
@@ -2,6 +2,7 @@ import {connect} from "react-redux";
 
 import {CircuitInfo} from "core/utils/CircuitInfo";
 
+import {useHistory} from "shared/utils/hooks/useHistory";
 import {SharedAppState} from "shared/state";
 
 import "./index.scss";
@@ -18,15 +19,26 @@ type StateProps = {
 type DispatchProps = {}
 
 type Props = StateProps & DispatchProps & OwnProps;
-const _UndoRedoButtons = ({info, undoImg, redoImg, isLocked}: Props) => (
-    <div className="undoredo__buttons">
-        <button title="Undo" onClick={() => { if (!isLocked) info.history.undo(); }}><img src={undoImg} alt="" /></button>
-        <button title="Redo" onClick={() => { if (!isLocked) info.history.redo(); }}><img src={redoImg} alt="" /></button>
-    </div>
-);
+const _UndoRedoButtons = ({info, undoImg, redoImg, isLocked}: Props) => {
+    const {undoHistory, redoHistory} = useHistory(info);
+
+    return (
+        <div className="undoredo__buttons">
+            <button title="Undo"
+                    disabled={undoHistory.length === 0}
+                    onClick={() => { if (!isLocked) info.history.undo(); }}>
+                <img src={undoImg} alt="" />
+            </button>
+            <button title="Redo"
+                    disabled={redoHistory.length === 0}
+                    onClick={() => { if (!isLocked) info.history.redo(); }}>
+                <img src={redoImg} alt="" />
+            </button>
+        </div>
+    );
+}
 
 export const UndoRedoButtons = connect<StateProps, DispatchProps, OwnProps, SharedAppState>(
     (state) => ({ isLocked: state.circuit.isLocked }),
     { }
 )(_UndoRedoButtons);
-
