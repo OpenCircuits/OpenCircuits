8de25c59bcdfc1e29d2af5e2b0daad8686fdf4c9
Leon Montealegre <leonm99@gmail.com>
2021-04-14 23:07:31 -0400

    Added settings menu

--- src/site/shared/state/Header/state.ts
+++ src/site/shared/state/Header/state.ts
@@ -1,5 +1,5 @@
 
-export type HeaderMenus = "none" | "download" | "tutorial";
+export type HeaderMenus = "none" | "download" | "settings" | "tutorial";
 export type HeaderPopups = "none" | "login" | "quick_start" | "keyboard_shortcuts";
 
 export type HeaderState = {
