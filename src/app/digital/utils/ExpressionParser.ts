8e3eda1b296330af86e0aa7fdca6ddfbc8e32713
Trevor Crystal <TGCrystal@users.noreply.github.com>
2021-04-23 11:04:11 -0400

    Revert "Set names of output and input"
    This reverts commit 1b7b48053ddad98ef76965367b26b3454414e131.

--- src/app/digital/utils/ExpressionParser.ts
+++ src/app/digital/utils/ExpressionParser.ts
@@ -269,8 +269,6 @@ function getComponentsValidate(inputs: Map<string, DigitalComponent>): IOObject[
     return components;
 }
 
-// Checks that token is a key in inputs, if so then set the corresponding component's name
-// to be token
 function validateToken(inputs: Map<string, DigitalComponent>, token: string) {
     switch(token) {
     case "(":
@@ -283,8 +281,6 @@ function validateToken(inputs: Map<string, DigitalComponent>, token: string) {
     default:
         if(!inputs.has(token))
             throw new Error("Input Not Found: " + token);
-        else
-            inputs.get(token).setName(token);
         break;
     }
 }
@@ -328,7 +324,6 @@ export function ExpressionToCircuit(inputs: Map<string, DigitalComponent>,
     if(expression == null) throw new Error("Null Parameter: expression");
     if(output == null) throw new Error("Null Parameter: output");
 
-    output.setName("Output");
     const components: IOObject[] = getComponentsValidate(inputs);
     const tokenList = getTokenListValidate(inputs, expression, output);
 
