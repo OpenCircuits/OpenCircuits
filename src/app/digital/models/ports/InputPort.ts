20f804d27b76ba7c86ffc1cf6418aaad5c845819
Leon Montealegre <leonm99@gmail.com>
2021-06-18 00:00:28 -0400

    Fixed circular import issues

--- src/app/digital/models/ports/InputPort.ts
+++ src/app/digital/models/ports/InputPort.ts
@@ -1,9 +1,11 @@
+import {serializable} from "serialeazy";
+
 import {Vector,V} from "Vector";
 
 import {Port} from "core/models/ports/Port";
-import {DigitalComponent} from "../DigitalComponent";
-import {DigitalWire} from "../DigitalWire";
-import {serializable} from "serialeazy";
+
+import {DigitalComponent, DigitalWire} from "../index";
+
 
 @serializable("DigitalInputPort")
 export class InputPort extends Port {
