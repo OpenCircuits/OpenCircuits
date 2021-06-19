e5b609eb0d62882ec4140814c81decf45c28005c
Leon Montealegre <leonm99@gmail.com>
2021-06-19 17:21:12 -0400

    Added error if starting server when it isn't built

--- scripts/start.js
+++ scripts/start.js
@@ -5,6 +5,7 @@ const startWebpack = require("./webpack");
 const getDirs = require("./utils/getDirs");
 const {existsSync} = require("fs");
 const path = require("path");
+const chalk = require("chalk");
 
 
 // Do this as the first thing so that any code reading it knows the right env.
@@ -14,6 +15,13 @@ process.env.NODE_ENV = "development";
 
 function start_server() {
     const isWin = (os.platform() === "win32");
+
+    // Check if server is built
+    if (!existsSync(path.resolve(process.cwd(), "build", (isWin ? "server.exe" : "server")))) {
+        console.log(`\n${chalk.red("Failed to start server!")}\nYou must first build the server with ${chalk.bold(chalk.cyan("yarn build"))}\n`);
+        return;
+    }
+
     spawn(`cd build && ${isWin ? "server.exe" : "./server"} -no_auth`, {
         shell: true, stdio: "inherit",
     });
