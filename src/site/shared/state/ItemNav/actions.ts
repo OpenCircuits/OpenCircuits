2bd885ee5f658e104dc2bcc534c5cbb2539ccb87
unknown <rosan2@rpi.edu>
2021-07-04 14:50:07 -0400

    Moved history box actions from side nav to item nav folder

--- src/site/shared/state/ItemNav/actions.ts
+++ src/site/shared/state/ItemNav/actions.ts
@@ -1,12 +1,30 @@
-import {TOGGLE_ITEMNAV_ID} from "./actionTypes";
+import {TOGGLE_ITEMNAV_ID, OPEN_HISTORY_BOX_ID, 
+    CLOSE_HISTORY_BOX_ID} from "./actionTypes";
 
 
 export type ToggleItemNavAction = {
     type: typeof TOGGLE_ITEMNAV_ID;
 }
 
+export type OpenHistoryBoxAction = {
+    type: typeof OPEN_HISTORY_BOX_ID;
+}
+export type CloseHistoryBoxAction = {
+    type: typeof CLOSE_HISTORY_BOX_ID;
+}
+
 
 export const ToggleItemNav = () => ({ type: TOGGLE_ITEMNAV_ID }) as ToggleItemNavAction;
 
+export const OpenHistoryBox = () => ({
+    type: OPEN_HISTORY_BOX_ID
+}) as OpenHistoryBoxAction;
+export const CloseHistoryBox = () => ({
+    type: CLOSE_HISTORY_BOX_ID
+}) as CloseHistoryBoxAction;
 
-export type ItemNavActions = ToggleItemNavAction;
+
+export type ItemNavActions = 
+    ToggleItemNavAction  |
+    OpenHistoryBoxAction |
+    CloseHistoryBoxAction;
--- src/site/shared/state/SideNav/actions.ts
+++ src/site/shared/state/SideNav/actions.ts
@@ -1,32 +1,19 @@
-import {TOGGLE_SIDENAV_ID, OPEN_HISTORY_BOX_ID, 
-        CLOSE_HISTORY_BOX_ID} from "./actionTypes";
+import {TOGGLE_SIDENAV_ID} from "./actionTypes";
 
 
 export type ToggleSideNavAction = {
     type: typeof TOGGLE_SIDENAV_ID;
 }
 
-export type OpenHistoryBoxAction = {
-    type: typeof OPEN_HISTORY_BOX_ID;
-}
-export type CloseHistoryBoxAction = {
-    type: typeof CLOSE_HISTORY_BOX_ID;
-}
+
 
 
 export const ToggleSideNav = () => ({
     type: TOGGLE_SIDENAV_ID
 }) as ToggleSideNavAction;
 
-export const OpenHistoryBox = () => ({
-    type: OPEN_HISTORY_BOX_ID
-}) as OpenHistoryBoxAction;
-export const CloseHistoryBox = () => ({
-    type: CLOSE_HISTORY_BOX_ID
-}) as CloseHistoryBoxAction;
+
 
 
 export type SideNavActions = 
-    ToggleSideNavAction  |
-    OpenHistoryBoxAction |
-    CloseHistoryBoxAction;
+    ToggleSideNavAction;
