9f3db05db66de4296224724a333ea73229ba59ed
Leon Montealegre <leonm99@gmail.com>
2021-08-01 13:47:16 -0400

    Added docusaurus site folder with initial config

---
+++ src/site/pages/docs/sidebars.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const {readdirSync} = require("fs");
+
+
+
+// Automatically generate JSDocs sidebar config
+function addSubDir(arr, dir) {
+  const files = readdirSync(path.resolve("../../../../", "docs", dir), { withFileTypes: true });
+  files.forEach((f) => {
+    const name = f.name;
+    if (name.startsWith(".")) // Ignore files like .DS_Store
+      return;
+    if (f.isFile()) {
+      arr.push(`${dir}/${name.split(".md")[0]}`);
+    } else if (f.isDirectory()) {
+      const obj = {
+        type: "category",
+        label: name,
+        items: []
+      };
+      addSubDir(obj.items, `${dir}/${name}`);
+      arr.push(obj);
+    }
+  });
+}
+
+const jsDocConfig = {};
+const files = readdirSync(path.resolve("../../../../", "docs/ts"), { withFileTypes: true });
+files.forEach((f) => {
+  if (!f.isDirectory())
+    return;
+  jsDocConfig[f.name] = [];
+  addSubDir(jsDocConfig[f.name], `ts/${f.name}`);
+});
+
+
+module.exports = {
+  mainSidebar: [
+    "introduction",
+    {
+      type: "category",
+      label: "Getting Started",
+      collapsed: false,
+      items: ["contributing", "installation", "running"],
+    },
+
+  ],
+  // mainSidebar: {
+  //   "main/intro",
+  //   "OpenCircuits": ["main/intro"],
+  //   "Getting Started": [],
+  //   "Guides": []
+  // },
+  apiSidebar: {
+    "API": ["test"]
+  },
+  jsDocSidebar: {...jsDocConfig},
+  otherSidebar: {
+    "Other": ["test"]
+  }
+};
