c6f55bc327eb269c2e9df85fcac97edb80125345
Leon Montealegre <leonm99@gmail.com>
2021-08-02 16:48:37 -0400

    Fixed port/wire collision fix and some smalling formatting fixes

--- src/app/core/tools/handlers/SelectionHandler.ts
+++ src/app/core/tools/handlers/SelectionHandler.ts
@@ -29,19 +29,19 @@ export const SelectionHandler: EventHandler = ({
         // Check if an object was clicked
         const obj = objs.find(o => o.isWithinSelectBounds(worldMousePos));
 
-        // // If we clicked the port and also hit a wire,
-        // //  we want to prioritize the port
-        // if (obj instanceof Wire && ports.some(p => p.isWithinSelectBounds(worldMousePos)))
-        //     return action;
+        // If we clicked a port and also hit a wire,
+        //  we want to prioritize the port, so skip selecting
+        if (!(obj instanceof Wire && ports.some(p => p.isWithinSelectBounds(worldMousePos)))) {
+            // Select object
+            if (obj) {
+                const deselect = (input.isShiftKeyDown() && selections.has(obj));
+                action.add(new SelectAction(selections, obj, deselect).execute());
+                action.add(new ShiftAction(designer, obj).execute());
+            }
+        }
 
-        // Select object
-        if (obj) {
-            const deselect = (input.isShiftKeyDown() && selections.has(obj));
-            action.add(new SelectAction(selections, obj, deselect).execute());
-            action.add(new ShiftAction(designer, obj).execute());
-        }
-        if (!action.isEmpty()) {
+        // https://github.com/OpenCircuits/OpenCircuits/issues/622
+        if (!action.isEmpty())
             history.add(action);
-        }
     }
 });
