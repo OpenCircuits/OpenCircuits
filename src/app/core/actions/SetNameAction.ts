fe5047b4ebe0bf220d0c9f537487873f8b500705
Kevin Mackenzie <dev.kjmack305@gmail.com>
2021-08-12 17:02:45 -0400

    Revert "Hack to serialize references"
    This reverts commit 4b3193f14afb75f9cd24ffd092333eb7f9eaf441.

--- src/app/core/actions/SetNameAction.ts
+++ src/app/core/actions/SetNameAction.ts
@@ -1,49 +1,19 @@
 import {Selectable} from "core/utils/Selectable";
 import {Action} from "core/actions/Action";
-import {CustomBehavior, serializable, serialize, Serialize} from "serialeazy";
-import {Serializer} from "serialeazy/dist/Serialization";
-import {Distinguishable} from "core/utils/Distinguishable";
 
-export function ref(): (target: any, propertyKey: string) => void {
-    return function (target, propertyKey) {
-        Reflect.defineMetadata(propertyKey, true, target);
-    };
-}
 
-// TODO: This is a hack, an annotation (@ref) would be much better
-export function serializeRefs<T>(refNames: string[]): CustomBehavior<T> {
-    return {customSerialization: (serializer: Serializer, obj: any) => {
-        const data = serializer.defaultSerialization(obj);
-        for (const o of refNames) {
-            data[o] = (obj[o] as Distinguishable).getGuid();
-        }
-        return data;
-    }};
-}
-
-@serializable("SetNameAction", serializeRefs(["obj"]))
 export class SetNameAction implements Action {
-    @serialize(false)
-    private obj?: Selectable & Distinguishable;
+    private obj: Selectable;
     private newName: string;
     private oldName: string;
 
-    public constructor(o?: Selectable & Distinguishable, newName?: string) {
-        this.setObj(o);
-        this.setName(newName);
-    }
-
-    public setObj(o?: Selectable & Distinguishable) {
+    public constructor(o: Selectable, newName: string) {
         this.obj = o;
+        this.newName = newName;
         this.oldName = o.getName();
     }
 
-    public setName(newName: string) {
-        this.newName = newName;
-    }
-
     public execute(): Action {
-        console.log(Serialize(this));
         this.obj.setName(this.newName);
         return this;
     }
