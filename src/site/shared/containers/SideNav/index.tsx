d5edc7f7c1bd140f7e3a528d8129737ef096bd02
samoo <Samoooomei@gmail.com>
2021-08-12 10:26:11 -0400

    ellipses might be better

--- src/site/shared/containers/SideNav/index.tsx
+++ src/site/shared/containers/SideNav/index.tsx
@@ -55,8 +55,8 @@ const _SideNav = ({ helpers, auth, isOpen, isLoggedIn, userCircuits, exampleCirc
 
         <div className={`sidenav ${isOpen ? "" : "sidenav__move"}`}>
             <div className="sidenav__accountinfo">
-                {auth ? `Hello, ${auth.getName()}!` : null}
-                <div className ="sidenav__accountinfo__sign"> <SignInOutButtons/> </div>
+                <div className = "sidenav__accountinfo__name">  {auth ? `Hello, ${auth.getName()}!` : null} </div>
+                <div className = "sidenav__accountinfo__sign"> <SignInOutButtons/> </div>
             </div>
             <div className="sidenav__content">
                 <h4 unselectable="on">My Circuits</h4>
