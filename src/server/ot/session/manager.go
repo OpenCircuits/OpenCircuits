972cc0aaf3037c07a3a4de84a81319722874e5c4
Kevin Mackenzie <dev.kjmack305@gmail.com>
2021-08-18 10:01:34 -0400

    Removed locks from SessionManager
    It turns out the session manager does not need to store a map of session references for anything.

--- src/server/ot/session/manager.go
+++ src/server/ot/session/manager.go
@@ -2,70 +2,40 @@ package session
 
 import (
 	"log"
-	"sync"
 
 	"github.com/OpenCircuits/OpenCircuits/site/go/core/model"
 	"github.com/OpenCircuits/OpenCircuits/site/go/ot/conn"
 	"github.com/OpenCircuits/OpenCircuits/site/go/ot/doc"
 )
 
-// SessionManager keeps track of live sessions
+// SessionManager launches sessions
 type SessionManager struct {
-	dm          *doc.DocumentManager
-	sessions    map[string]Session
-	sessionLock *sync.RWMutex
-	sessionDone chan string
+	dm          doc.DocumentManager
+	sessionDone chan model.SessionID
 }
 
-func NewSessionManager(dm *doc.DocumentManager) *SessionManager {
+func NewSessionManager(dm doc.DocumentManager) *SessionManager {
 	sm := &SessionManager{
 		dm:          dm,
-		sessions:    make(map[string]Session),
-		sessionLock: &sync.RWMutex{},
-		sessionDone: make(chan string, 20),
+		sessionDone: make(chan model.SessionID, 20),
 	}
-
-	go sm.closer()
 
 	return sm
 }
-
-//func (sm *SessionManager) updatePerms(sessionID string) {
-//	sm.sessionLock.RLock()
-//	defer sm.sessionLock.RUnlock()
-//	_, ok := sm.sessions[sessionID]
-//	if ok {
-//		// Do something to update permissions
-//	}
-//}
 
 func (sm *SessionManager) doneSender(sessionID model.SessionID) {
 	sm.sessionDone <- sessionID
 }
 
-func (sm *SessionManager) closer() {
-	for sid := range sm.sessionDone {
-		sm.sessionLock.Lock()
-		delete(sm.sessions, sid)
-		sm.sessionLock.Unlock()
-	}
-}
-
-func (sm *SessionManager) Start(circuitID string, conn conn.Connection) error {
+func (sm *SessionManager) Start(circuitID model.CircuitId, conn conn.Connection) error {
 	// Get the document
 	doc, err := sm.dm.Get(circuitID)
 	if err != nil {
-		log.Printf("%e\n", err)
+		log.Printf("%v\n", err)
 		return err
 	}
 
 	// Spawn the session
-	s := NewSession(doc, conn, sm.doneSender)
-
-	// Add the session to the map
-	sm.sessionLock.Lock()
-	sm.sessions[s.SessionID] = s
-	sm.sessionLock.Unlock()
-
+	_ = NewSession(doc, conn, sm.doneSender)
 	return nil
 }
