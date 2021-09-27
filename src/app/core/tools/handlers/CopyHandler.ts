9d90b0a4e5cd2b581a4efcf3d576f3b7a46f9352
Leon Montealegre <leonm99@gmail.com>
2021-09-26 20:22:34 -0400

    Fixed copy/paste handlers to use actual clipboard

--- src/app/core/tools/handlers/CopyHandler.ts
+++ src/app/core/tools/handlers/CopyHandler.ts
@@ -1,22 +1,34 @@
-import {C_KEY} from "core/utils/Constants";
-
-import {Event} from "core/utils/Events";
+import {CopyPasteEvent, Event} from "core/utils/Events";
 import {CircuitInfo} from "core/utils/CircuitInfo";
+import {SerializeForCopy} from "core/utils/ComponentUtils";
+
+import {GroupAction} from "core/actions/GroupAction";
+import {CreateDeselectAllAction} from "core/actions/selection/SelectAction";
+import {CreateDeleteGroupAction} from "core/actions/deletion/DeleteGroupActionFactory";
 
 import {IOObject} from "core/models";
 
 import {EventHandler} from "../EventHandler";
-import {CopyGroup} from "core/utils/ComponentUtils";
 
 
 export const CopyHandler: EventHandler = ({
-    conditions: (event: Event, {input, selections}: CircuitInfo) =>
-        (event.type === "keydown" &&
-         event.key === C_KEY &&
-         input.isModifierKeyDown() &&
+    conditions: (event: Event, {selections}: CircuitInfo) =>
+        ((event.type === "copy" || event.type === "cut") &&
          selections.amount() > 0),
 
-    getResponse: ({selections, clipboard}: CircuitInfo) => {
-        clipboard.splice(0, clipboard.length, ...CopyGroup(selections.get().filter(o => o instanceof IOObject) as IOObject[]).toList());
+    getResponse: ({selections, designer, history}: CircuitInfo, {type, ev}: CopyPasteEvent) => {
+        const objs = selections.get().filter(o => o instanceof IOObject) as IOObject[];
+
+        const str = SerializeForCopy(objs);
+        ev.clipboardData.setData("text/json", str);
+        ev.preventDefault(); // Necessary to copy correctly
+
+        if (type === "cut") {
+            // Delete selections
+            history.add(new GroupAction([
+                CreateDeselectAllAction(selections),
+                CreateDeleteGroupAction(designer, objs)
+            ]).execute());
+        }
     }
 });
--- src/app/core/tools/handlers/PasteHandler.ts
+++ src/app/core/tools/handlers/PasteHandler.ts
@@ -1,37 +1,18 @@
-import {V_KEY} from "core/utils/Constants";
-import {V} from "Vector";
-
-import {Event} from "core/utils/Events";
-import {CircuitInfo} from "core/utils/CircuitInfo";
-
-import {GroupAction} from "core/actions/GroupAction";
-import {TranslateAction} from "core/actions/transform/TranslateAction";
-import {CreateDeselectAllAction,
-        CreateGroupSelectAction} from "core/actions/selection/SelectAction";
-import {CopyGroupAction} from "core/actions/CopyGroupAction";
+import {CopyPasteEvent, Event} from "core/utils/Events";
 
 import {EventHandler} from "../EventHandler";
 
 
-export const PasteHandler: EventHandler = ({
-    conditions: (event: Event, {input, clipboard}: CircuitInfo) =>
-        (event.type === "keydown" &&
-         event.key === V_KEY &&
-         input.isModifierKeyDown() &&
-         clipboard.length > 0),
+export const PasteHandler = (paste: (text: string) => boolean): EventHandler => {
+    return ({
+        conditions: (event: Event) =>
+            (event.type === "paste" &&
+             (event.ev.clipboardData.getData("text/json").length > 0 ||
+              event.ev.clipboardData.getData("text/plain").length > 0)),
 
-    getResponse: ({history, designer, selections, clipboard}: CircuitInfo) => {
-        const copyGroupAction = new CopyGroupAction(designer, clipboard);
-        const components = copyGroupAction.getCopies().getComponents();
-
-        // Copy the group and then select them and move them over slightly
-        history.add(new GroupAction([
-            copyGroupAction.execute(),
-            CreateDeselectAllAction(selections).execute(),
-            CreateGroupSelectAction(selections, components).execute(),
-            new TranslateAction(components,
-                                components.map(o => o.getPos()),
-                                components.map(o => o.getPos().add(V(5, 5)))).execute()
-        ]));
-    }
-});
+        getResponse: (_, {ev}: CopyPasteEvent) => {
+            paste(ev.clipboardData.getData("text/json") ||
+                  ev.clipboardData.getData("text/plain"));
+        }
+    });
+}
