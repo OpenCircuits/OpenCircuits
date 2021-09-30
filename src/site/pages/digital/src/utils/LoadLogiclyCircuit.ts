06edeb39d4fbf12524af204885ea34a6fba2db62
Leon Montealegre <leonm99@gmail.com>
2021-09-30 09:48:52 -0400

    Finished basic logicly parser

--- src/site/pages/digital/src/utils/LoadLogiclyCircuit.ts
+++ src/site/pages/digital/src/utils/LoadLogiclyCircuit.ts
@@ -1,5 +1,9 @@
+import {ConnectionAction} from "core/actions/addition/ConnectionAction";
 import {DigitalCircuitDesigner, DigitalComponent} from "digital/models";
+import {Clock} from "digital/models/ioobjects";
 import {DigitalCircuitInfo} from "digital/utils/DigitalCircuitInfo";
+import {Create} from "serialeazy";
+import {V} from "Vector";
 
 
 type BaseLogiclyObject<type extends string> = {
@@ -47,6 +51,22 @@ const ObjMap: Map<LogiclyObject["type"], string> = new Map([
     ["light_bulb@logic.ly", "LED"],
 ]);
 
+function fixConnection(con: Record<string, string>): LogiclyConnection | undefined {
+    const keys = ["inputUID", "inputIndex", "outputUID", "outputIndex"];
+    if (!keys.every(key => (key in con)))
+        return;
+
+    const intKeys = ["inputIndex", "outputIndex"];
+
+    let fixedCon = {...con} as Record<string, any>;
+
+    intKeys.forEach(key => {
+        if (fixedCon[key]) fixedCon[key] = parseInt(fixedCon[key]);
+    });
+
+    return fixedCon as LogiclyConnection;
+}
+
 function fixObj(obj: Record<string, string>): LogiclyObject | undefined {
     const keys = ["type", "uid", "x", "y", "rotation"];
     if (!keys.every(key => (key in obj)))
@@ -68,12 +88,38 @@ function fixObj(obj: Record<string, string>): LogiclyObject | undefined {
 }
 
 function getObj(lObj: LogiclyObject): DigitalComponent {
-    const obj = ObjMap.get(lObj.type);
+    const uuid = ObjMap.get(lObj.type);
+    if (!uuid)
+        throw new Error(`Failed to find Object of type: ${uuid}!`);
+    const obj = Create<DigitalComponent>(uuid);
+    if (!obj)
+        throw new Error(`Failed to create digital component with ID: ${uuid}!`);
+
+    // Set base properties
+    obj.setPos(V(lObj.x, lObj.y));
+    obj.setAngle(lObj.rotation);
+    if (lObj.exportName)
+        obj.setName(lObj.exportName);
+
+    // Set specific properties
+    switch (lObj.type) {
+        case "and@logic.ly":
+        case "or@logic.ly":
+        case "not@logic.ly":
+            obj.setInputPortCount(lObj.inputs);
+            break;
+        case "label@logic.ly":
+            obj.setName(lObj.text);
+            break;
+        case "clock@logic.ly":
+            (obj as Clock).setFrequency(lObj.frequency);
+            break;
+    }
 
     return obj;
 }
 
-export function LoadLogiclyCircuit(data: string, {}: DigitalCircuitInfo) {
+export function LoadLogiclyCircuit(data: string, {designer, selections, history, renderer}: DigitalCircuitInfo) {
     console.log(data);
 
     // It is necessary to remove the `;base64,` prefix before using atob
@@ -87,16 +133,42 @@ export function LoadLogiclyCircuit(data: string, {}: DigitalCircuitInfo) {
 
     const objs = new Map<string, DigitalComponent>();
     for (const obj of xml.getElementsByTagName("object")) {
-        console.log(obj);
-        console.log(obj.getAttributeNames());
-        // console.log(obj.getAttribute())
         const data = Object.fromEntries(obj.getAttributeNames().map(n => ([n, obj.getAttribute(n)])));
         const lObj = fixObj(data);
         if (!lObj)
-            throw new Error("Invalid object for Logicly parser! " + xml);
+            throw new Error("Invalid object for Logicly parser! " + obj);
 
         const ocObj = getObj(lObj);
         objs.set(lObj.uid, ocObj);
         circuit.addObject(ocObj);
     }
+
+    for (const con of xml.getElementsByTagName("connection")) {
+        const data = Object.fromEntries(con.getAttributeNames().map(n => ([n, con.getAttribute(n)])));
+        const lConnection = fixConnection(data);
+        if (!lConnection)
+            throw new Error("Invalid connection for Logicly parser! " + con);
+
+        const inputObj = objs.get(lConnection.inputUID);
+        if (!inputObj)
+            throw new Error(`Failed to find connection input w/ UID: ${lConnection.inputUID}, from list: ${objs}`);
+        const outputObj = objs.get(lConnection.outputUID);
+        if (!outputObj)
+            throw new Error(`Failed to find connection output w/ UID: ${lConnection.outputUID}, from list: ${objs}`);
+
+        new ConnectionAction(circuit,
+            inputObj.getInputPort(lConnection.inputIndex),
+            outputObj.getOutputPort(lConnection.outputIndex)
+        ).execute();
+    }
+
+
+    // Reset selections, clear history, and replace circuit
+    selections.get().forEach(s => selections.deselect(s));
+
+    history.reset();
+
+    designer.replace(circuit);
+
+    renderer.render();
 }
