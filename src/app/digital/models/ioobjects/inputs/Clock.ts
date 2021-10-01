e6a8ee53c903d0e351dffcf0a6be2e915f50bf26
BaiTueeChuan <90936261+BaiTueeChuan@users.noreply.github.com>
2021-10-01 14:58:16 -0400

    Update Clock.ts

--- src/app/digital/models/ioobjects/inputs/Clock.ts
+++ src/app/digital/models/ioobjects/inputs/Clock.ts
@@ -4,14 +4,27 @@ import {serializable, serialize} from "serialeazy";
 
 import {DigitalComponent} from "digital/models/DigitalComponent";
 
+/** This is clock objects. When you use this object, the input you get will be changed
+    * between 1 and 0 constantly in certain amount of time.
+    */
+
 @serializable("Clock")
 export class Clock extends DigitalComponent {
+    
+    /** This is how often the clock changes input
+        */
     @serialize
     private frequency: number;
 
+    /** This is current input of the clock, isOn is True or False
+        */
     @serialize
     private isOn: boolean;
 
+    /** This is constructor of clock. This will automatically set the beginning state to
+        * false and begin to "tick", and the frequency to 1000ms.
+        */
+    
     public constructor() {
         super(new ClampedValue(0), new ClampedValue(1), V(60, 42));
         this.frequency = 1000;
@@ -25,11 +38,14 @@ export class Clock extends DigitalComponent {
         setTimeout(() => this.tick(), this.frequency);
     }
 
+    /** Changing the clock's frequency
+        */
     public setFrequency(freq: number): void {
         this.frequency = freq;
     }
 
-    // Activate changes state and image
+    /** Activate changes state and image
+        */
     public activate(bool: boolean): void {
         super.activate(bool);
         this.isOn = bool;
@@ -42,14 +58,18 @@ export class Clock extends DigitalComponent {
         return V();
     }
 
+    /** @returns return current frequency of the clock
+      */
     public getFrequency(): number {
         return this.frequency;
     }
 
+    // @returns return the name of the clock
     public getDisplayName(): string {
         return "Clock";
     }
-
+    
+    // @returns return different image of the clock accroding to current input
     public getImageName(): string {
         return (this.isOn ? "clockOn.svg" : "clock.svg");
     }
