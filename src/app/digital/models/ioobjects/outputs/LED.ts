c5cdb0a5b9bcf9235b7e7923859e145543a80657
Caitlin Callahan <caitlinc362@gmail.com>
2021-10-11 20:24:35 -0400

    added comments to LED file

--- src/app/digital/models/ioobjects/outputs/LED.ts
+++ src/app/digital/models/ioobjects/outputs/LED.ts
@@ -8,11 +8,22 @@ import {serializable, serialize} from "serialeazy";
 
 import {DigitalComponent} from "digital/models/DigitalComponent";
 
+/**
+ * The LED turns on when signal input connnected to it is 1
+ * 
+ * Outputs a bright light
+ * 
+ */
+
 @serializable("LED")
 export class LED extends DigitalComponent {
     @serialize
     private color: string;
 
+    /**
+     * initializes LED, has clamped value of 0 and 1 inputs
+     */
+    
     public constructor() {
         super(new ClampedValue(1),
               new ClampedValue(0),
@@ -24,38 +35,67 @@ export class LED extends DigitalComponent {
         this.inputs.first.setTargetPos(V(0, 2*DEFAULT_SIZE));
     }
 
-    // @Override
+    /**
+     * Turn the LED on
+     * @param signal  signal connected to the LED (on or off)
+     * @param i output port
+     */
     public activate(signal: boolean, i: number = 0): void {
         this.onTransformChange();
         super.activate(signal, i);
     }
 
-    // @Override
+    /**
+     * 
+     * @returns adds extra offset if LED is on
+     */
     public getOffset(): Vector {
         // Add extra offset if this LED is on (to account for light)
         return super.getOffset().add((this.isOn() ? (LED_LIGHT_RADIUS - LED_WIDTH/2) : (0)));
     }
 
+    /**
+     * sets the color of the LED
+     * @param color inputs a color
+     */
     public setColor(color: string): void {
         this.color = color;
     }
 
+    /**
+     * 
+     * @returns light status of LED
+     */
+
     public isOn(): boolean {
         return this.inputs.first.getIsOn();
     }
-
+    /**
+     * 
+     * @returns color of the LED
+     */
     public getColor(): string {
         return this.color;
     }
-
+    /**
+     * 
+     * @returns displayname of the LED
+     */
     public getDisplayName(): string {
         return "LED";
     }
-
+    /**
+     * 
+     * @returns image name of the LED (OFF)
+     */
     public getImageName(): string {
         return "led.svg";
     }
 
+    /**
+     * 
+     * @returns image name of the LED (ON)
+     */
     public getOnImageName(): string {
         return "ledLight.svg"
     }
