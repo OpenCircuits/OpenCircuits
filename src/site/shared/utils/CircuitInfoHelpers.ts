1a4aa86d804f77e19dead146d7dca1428831f8fc
Vansh Reddy <68335743+cutecucumber10@users.noreply.github.com>
2021-10-18 18:00:59 -0400

    Issue not completely fixed but working on it

--- src/site/shared/utils/CircuitInfoHelpers.ts
+++ src/site/shared/utils/CircuitInfoHelpers.ts
@@ -4,7 +4,7 @@ import {CircuitMetadata} from "core/models/CircuitMetadata";
 export type CircuitInfoHelpers = {
     LoadCircuit: (getData: () => Promise<string>) => Promise<void>;
     SaveCircuitRemote: () => Promise<boolean>;
-    SaveCircuitToFile: (type: "pdf" | "png" | "circuit") => Promise<void>;
+    SaveCircuitToFile: (type: "pdf" | "png" | "circuit"|"jpeg") => Promise<void>;
     DeleteCircuitRemote: (circuit: CircuitMetadata) => Promise<void>;
     GetSerializedCircuit: () => string;
 }
