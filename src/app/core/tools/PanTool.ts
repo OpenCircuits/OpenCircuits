aec03a3f38cf6cde09808fe54357fc72190163f0
RussellABH <rabh.2202@gmail.com>
2021-10-22 17:41:02 -0400

    Introduced coding best practices (functionality is the same) Move import statements Added constants for set values and used them Changed event type to be called on if not else if Added spaces to the front of comments Changed how I used variables to be more clean

--- src/app/core/tools/PanTool.ts
+++ src/app/core/tools/PanTool.ts
@@ -1,11 +1,13 @@
-import {MIDDLE_MOUSE_BUTTON,
-        OPTION_KEY, ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT}  from "core/utils/Constants";
+import {MIDDLE_MOUSE_BUTTON, 
+        OPTION_KEY,
+        ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT,
+        ARROW_PAN_DISTANCE_NORMAL, ARROW_PAN_DISTANCE_SMALL,}  from "core/utils/Constants";
+import {Vector}      from "Vector";
 
 import {Event}       from "core/utils/Events";
 import {CircuitInfo} from "core/utils/CircuitInfo";
 
 import {Tool}        from "core/tools/Tool";
-import {Vector}      from "core/utils/math/Vector";
 
 export const PanTool: Tool = (() => {
     let isDragging = false;
@@ -16,7 +18,8 @@ export const PanTool: Tool = (() => {
             //                           or the middle mouse button
             //  or if the user pressed one of of the arrow keys
             return (event.type === "keydown"   && ((event.key === OPTION_KEY) ||  
-                                                  (event.key >= ARROW_LEFT && event.key <= ARROW_DOWN)) ||
+                                                   (event.key == ARROW_LEFT || event.key == ARROW_RIGHT || 
+                                                    event.key == ARROW_UP || event.key == ARROW_DOWN )) ||
                    (event.type === "mousedrag" && (event.button === MIDDLE_MOUSE_BUTTON ||
                                                    input.getTouchCount() === 2)));
         },
@@ -26,7 +29,8 @@ export const PanTool: Tool = (() => {
             //  or if one of the arrow keys were released
             return (event.type === "mouseup") ||
                    (event.type === "keyup" && ((!isDragging && event.key === OPTION_KEY) || 
-                                               (event.key >= ARROW_LEFT && event.key <= ARROW_DOWN)))
+                                               (event.key == ARROW_LEFT || event.key == ARROW_RIGHT || 
+                                                event.key == ARROW_UP || event.key == ARROW_DOWN )))
         },
 
 
@@ -47,34 +51,28 @@ export const PanTool: Tool = (() => {
                 const dPos = input.getDeltaMousePos();
                 camera.translate(dPos.scale(-1 * camera.getZoom()));
             }
-            else if (event.type === "keydown") {
-                //We pan in relation to the zoom so it's more or less consistent 
+            if (event.type === "keydown") {
                 let moveVec = new Vector();
-                let zoom = camera.getZoom();
-                let factor = 75; //75 Chosen because it looks good
-                let x = 0;
-                let y = 0;
-
-                //If shift is held then make a smaller movement
-                if (input.isShiftKeyDown()) 
-                    factor = 5;
-
-                //No else if because it introduces bugs when 
-                //multiple arrow keys are pressed
-                if (input.isKeyDown(ARROW_LEFT))
-                    x -= 1;
-                if (input.isKeyDown(ARROW_RIGHT))
-                    x += 1;
-                if (input.isKeyDown(ARROW_UP))
-                    y -= 1;
-                if (input.isKeyDown(ARROW_DOWN))
-                    y += 1;
                 
-                camera.translate(new Vector(x * zoom * factor, y * zoom * factor));
+                // No else if because it introduces bugs when 
+                // multiple arrow keys are pressed
+                if (input.isKeyDown(ARROW_LEFT))
+                    moveVec = moveVec.add(-1, 0);
+                if (input.isKeyDown(ARROW_RIGHT))
+                    moveVec = moveVec.add(1, 0);
+                if (input.isKeyDown(ARROW_UP))
+                    moveVec = moveVec.add(0, -1);
+                if (input.isKeyDown(ARROW_DOWN))
+                    moveVec = moveVec.add(0, 1);
+
+                // Screen gets moved different amounts depending on if the shift key is held
+                const factor = (input.isShiftKeyDown() ? 5 : 75); 
+
+                camera.translate(moveVec.scale(factor * camera.getZoom()));
             }
             else {
-                //Since it wasn't one of the two event types we want we 
-                //don't need a re-render
+                // Since it wasn't one of the two event types we want we 
+                // don't need a re-render
                 return false;
             }
             // Return true since we did something
