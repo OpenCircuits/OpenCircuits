ddb429dbe6dd9c9f721f28fa6787a961b8eaad94
unknown <rosan2@rpi.edu>
2021-10-24 16:31:54 -0400

    Added group action component and style

--- src/site/shared/containers/HistoryBox/index.tsx
+++ src/site/shared/containers/HistoryBox/index.tsx
@@ -4,12 +4,40 @@ import {useSharedDispatch, useSharedSelector} from "shared/utils/hooks/useShared
 import {useHistory} from "shared/utils/hooks/useHistory";
 
 import "./index.scss";
+import {Action} from "core/actions/Action";
+import {GroupAction} from "core/actions/GroupAction";
 
 
-const thing = {
-    someProperty: "String",
-    someOtherPropetty: 5
+type HistoryEntryProps = {
+    a: Action;
+}
+const HistoryEntry = ({a}: HistoryEntryProps) => {
+    if (a instanceof GroupAction) {
+        return(<GroupActionEntry g = {a}></GroupActionEntry>)
     }
+    return(<div className="historybox__entry">{a.getName()}</div>);
+}
+
+
+type GroupActionEntryProps = {
+    g: GroupAction;
+}
+const GroupActionEntry = ({g}: GroupActionEntryProps) => {
+    if (g.isEmpty()) {
+        return null;
+    }
+    else if (g.getActions().length == 1) {
+        return(<HistoryEntry a = {g.getActions()[0]}></HistoryEntry>);
+    }
+    return(
+        <div className="historybox__groupentry">
+            <span>Group Action</span>
+            {g.getActions().map((a, i) => {
+                return(<HistoryEntry key = {`group-action-entry-${i}`} a = {a}></HistoryEntry>);
+            })}
+        </div>
+    );
+}
 
 
 type Props = {
@@ -29,7 +57,9 @@ export const HistoryBox = ({ info }: Props) => {
                 left: (isOpen ? "" : "0px"),
             }}>
             {info.history.getActions().reverse().map((a, i) => {
-                return <div key={`history-box-entry-${i}`} className="historybox__entry">{a.getName()}</div>
+                //   you could also declare a separate component for the GroupActionEntry so that it's all nice and separate
+                //    then for the HTML we need to make it special and return a list of all the sub-actions using the `HistoryEntry` components
+                return(<HistoryEntry key = {`history-box-entry-${i}`} a = {a}></HistoryEntry>)
             })}
         </div>
     );
