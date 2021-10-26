7a77c746062ad473123aba4faa30c175033749a8
Trevor Crystal <TGCrystal@users.noreply.github.com>
2021-10-26 14:58:25 -0400

    Added more negation simplification tests

--- src/app/tests/digital/utils/Simplifications.test.ts
+++ src/app/tests/digital/utils/Simplifications.test.ts
@@ -2,7 +2,7 @@ import "jest";
 
 import { IOObject } from "core/models";
 import { DigitalCircuitDesigner } from "digital/models";
-import { Switch, LED, ANDGate } from "digital/models/ioobjects";
+import { Switch, LED, ANDGate, ORGate, XORGate } from "digital/models/ioobjects";
 import { NOTGate } from "digital/models/ioobjects/gates/BUFGate";
 import { DigitalObjectSet } from "digital/utils/ComponentUtils";
 import { ConnectGate } from "digital/utils/ExpressionParser/Utils";
@@ -51,6 +51,104 @@ describe("Simplifications", () => {
                     a.activate(false);
             
                     expect(o.isOn()).toBe(true);
+                });
+                test("Inputs off", () => {
+                    b.activate(false);
+            
+                    expect(o.isOn()).toBe(true);
+                });
+            });
+        });
+        
+        describe("!(a|b)", () => {
+            const a = new Switch(), b = new Switch(), o = new LED();
+            const or = new ORGate(), not = new NOTGate();
+            const objects: IOObject[] = [
+                a,
+                b,
+                o,
+                or,
+                not
+            ];
+            objects.push(ConnectGate(a, or));
+            objects.push(ConnectGate(b, or));
+            objects.push(ConnectGate(or, not));
+            objects.push(ConnectGate(not, o));
+            const condensed = CreateNegatedGates(objects);
+    
+            test("Correct number of things", () => {
+                expect(condensed.length).toBe(7);
+            });
+    
+            const designer = new DigitalCircuitDesigner(0);
+            designer.addGroup(new DigitalObjectSet(condensed));
+            describe("Correct Circuit", () => {
+                test("Initial State", () => {
+                    expect(o.isOn()).toBe(true);
+                });
+                test("Input a on", () => {
+                    a.activate(true);
+            
+                    expect(o.isOn()).toBe(false);
+                });
+                test("Input a,b on", () => {
+                    b.activate(true);
+            
+                    expect(o.isOn()).toBe(false);
+                });
+                test("Input b on", () => {
+                    a.activate(false);
+            
+                    expect(o.isOn()).toBe(false);
+                });
+                test("Inputs off", () => {
+                    b.activate(false);
+            
+                    expect(o.isOn()).toBe(true);
+                });
+            });
+        });
+        
+        describe("!(a^b)", () => {
+            const a = new Switch(), b = new Switch(), o = new LED();
+            const xor = new XORGate(), not = new NOTGate();
+            const objects: IOObject[] = [
+                a,
+                b,
+                o,
+                xor,
+                not
+            ];
+            objects.push(ConnectGate(a, xor));
+            objects.push(ConnectGate(b, xor));
+            objects.push(ConnectGate(xor, not));
+            objects.push(ConnectGate(not, o));
+            const condensed = CreateNegatedGates(objects);
+    
+            test("Correct number of things", () => {
+                expect(condensed.length).toBe(7);
+            });
+    
+            const designer = new DigitalCircuitDesigner(0);
+            designer.addGroup(new DigitalObjectSet(condensed));
+            describe("Correct Circuit", () => {
+                test("Initial State", () => {
+                    expect(o.isOn()).toBe(true);
+                });
+                test("Input a on", () => {
+                    a.activate(true);
+            
+                    expect(o.isOn()).toBe(false);
+                });
+                test("Input a,b on", () => {
+                    b.activate(true);
+            
+                    expect(o.isOn()).toBe(true);
+                });
+                test("Input b on", () => {
+                    a.activate(false);
+            
+                    expect(o.isOn()).toBe(false);
                 });
                 test("Inputs off", () => {
                     b.activate(false);
