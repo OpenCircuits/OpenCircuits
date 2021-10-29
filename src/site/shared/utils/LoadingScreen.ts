feb3c9920f6a179266cb7827db2e9c7c63cd4821
Trevor Crystal <TGCrystal@users.noreply.github.com>
2021-10-29 00:02:11 -0400

    Added LoadingScreen jsdoc

--- src/site/shared/utils/LoadingScreen.ts
+++ src/site/shared/utils/LoadingScreen.ts
@@ -1,10 +1,22 @@
+/**
+ * This function manages a loading screen. The loading screen will iterate over the provided
+ * functions to load the site while updating text and a loading bar to indicate progress to the user.
+ * The text area that is updated must have an id equal to the supplied id with "-text" appended.
+ * The progress bar must similarly have an id with "-progress-bar" appended.
+ * 
+ * @param id the HTML id of the loading screen div
+ * @param initialPercent the percent to start the loading screen at
+ * @param segments an array with the percentage at which loading that segment ends, a text label to
+ *      indicate to the user what is currently loading, and the function itself along with an optional
+ *      function to call to update for progress that happens between segments
+ */
 export async function LoadingScreen(
     id: string,
     initialPercent: number,
     segments: [
         number,
         string,
-        (onProgress?: (partsDone: number, partsTotal: number) => Promise<void>) => Promise<void>
+        (onProgress?: (percentDone: number) => Promise<void>) => Promise<void>
     ][]
     ): Promise<void> {
 
