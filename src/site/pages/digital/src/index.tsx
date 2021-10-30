72c4ffc0a0962808d1f942d0df7bf5effe526427
Leon Montealegre <leonm99@gmail.com>
2021-10-30 19:14:03 -0400

    Refactored LoadingScreen to handle errors

--- src/site/pages/digital/src/index.tsx
+++ src/site/pages/digital/src/index.tsx
@@ -23,59 +23,34 @@ import {reducers} from "./state/reducers";
 import {App} from "./containers/App";
 
 
-async function setPercent(percentDone: number) {
-    document.getElementById("loading-screen-progress-bar").style.width = percentDone + "%";
-}
-
-function progressError(error: any, text: string) {
-    document.getElementById("loading-screen-progress-bar").style.width = "100%";
-    document.getElementById("loading-screen-progress-bar").style.backgroundColor = "#f44336";
-    const message = "<a href=\"https://github.com/OpenCircuits/OpenCircuits/issues/new/choose\">" + text + " Please refresh the page. If this error continues to occur, please click this text to submit a bug report.</a>";
-    document.getElementById("loading-screen-text").innerHTML = message;
-    console.error(error);
-}
-
 async function Init(): Promise<void> {
     const startPercent = 30;
     const imageLoadPercent = 80;
     let store: AppStore;
 
     await LoadingScreen("loading-screen", startPercent, [
-        [imageLoadPercent, "Loading Images", async () => {
-            try {
-                await Images.Load(setPercent, startPercent, imageLoadPercent);
-            } catch (e) {
-                progressError(e, "Error occurred while loading images.");
-            }
+        [imageLoadPercent, "Loading Images", async (onProgress) => {
+            await Images.Load(onProgress);
         }],
         [85, "Initializing redux", async () => {
-            try {
-                store = createStore(reducers, applyMiddleware(thunk as ThunkMiddleware<AppState, AllActions>));
-            } catch (e) {
-                progressError(e, "Error occurred while initializing redux.");
-            }
+            store = createStore(reducers, applyMiddleware(thunk as ThunkMiddleware<AppState, AllActions>));
         }],
         [95, "Initializing Authentication", async () => {
-            let AuthMethods: Record<string, () => Promise<void>>
-            try {
-                AuthMethods = {
-                    "no_auth": async () => {
-                        const username = GetCookie("no_auth_username");
-                        if (username)
-                            await store.dispatch(Login(new NoAuthState(username)));
-                    },
-                    "google": async () => {
-                        // Load auth2 from GAPI and initialize w/ metadata
-                        const clientId = process.env.OC_OAUTH2_ID;
-                        if (!clientId)
-                            throw new Error(`No client_id/OAUTH2_ID specificed for google auth!`);
-                        await new Promise((resolve) => gapi.load("auth2", resolve));
-                        await gapi.auth2.init({ client_id: clientId }).then(async (_) => {}); // Have to explicitly call .then
-                    }
-                };
-            } catch (e) {
-                progressError(e, "Error occurred while initializing authentication.");
-            }
+            const AuthMethods: Record<string, () => Promise<void>> = {
+                "no_auth": async () => {
+                    const username = GetCookie("no_auth_username");
+                    if (username)
+                        await store.dispatch(Login(new NoAuthState(username)));
+                },
+                "google": async () => {
+                    // Load auth2 from GAPI and initialize w/ metadata
+                    const clientId = process.env.OC_OAUTH2_ID;
+                    if (!clientId)
+                        throw new Error(`No client_id/OAUTH2_ID specificed for google auth!`);
+                    await new Promise((resolve) => gapi.load("auth2", resolve));
+                    await gapi.auth2.init({ client_id: clientId }).then(async (_) => {}); // Have to explicitly call .then
+                }
+            };
             try {
                 if ((process.env.OC_AUTH_TYPES ?? "").trim().length > 0)
                     await Promise.all(process.env.OC_AUTH_TYPES.split(" ").map(a => AuthMethods[a]()));
@@ -84,19 +59,15 @@ async function Init(): Promise<void> {
             }
         }],
         [100, "Rendering", async () => {
-            try {
-                const AppView = App(store);
-                ReactDOM.render(
-                    <React.StrictMode>
-                        <Provider store={store}>
-                            {AppView()}
-                        </Provider>
-                    </React.StrictMode>,
-                    document.getElementById("root")
-                );
-            } catch (e) {
-                progressError(e, "Error occurred while rendering.");
-            }
+            const AppView = App(store);
+            ReactDOM.render(
+                <React.StrictMode>
+                    <Provider store={store}>
+                        {AppView()}
+                    </Provider>
+                </React.StrictMode>,
+                document.getElementById("root")
+            );
         }]
     ]);
 }
--- src/site/shared/utils/LoadingScreen.ts
+++ src/site/shared/utils/LoadingScreen.ts
@@ -1,9 +1,12 @@
+import {getStackTrace, isError} from "./Errors";
+
+
 /**
  * This function manages a loading screen. The loading screen will iterate over the provided
  * functions to load the site while updating text and a loading bar to indicate progress to the user.
  * The text area that is updated must have an id equal to the supplied id with "-text" appended.
  * The progress bar must similarly have an id with "-progress-bar" appended.
- * 
+ *
  * @param id the HTML id of the loading screen div
  * @param initialPercent the percent to start the loading screen at
  * @param segments an array with the percentage at which loading that segment ends, a text label to
@@ -16,21 +19,62 @@ export async function LoadingScreen(
     segments: [
         number,
         string,
-        (onProgress?: (percentDone: number) => Promise<void>) => Promise<void>
+        (onProgress?: (percentDone: number) => void) => Promise<void>
     ][]
-    ): Promise<void> {
+): Promise<void> {
+    const loadingText = document.getElementById(`${id}-text`);
+    const loadingBar  = document.getElementById(`${id}-progress-bar`);
 
-    const loadingText = document.getElementById(id + "-text");
-    const loadingBar = document.getElementById(id + "-progress-bar");
-    const setText = (text: string) => loadingText.innerHTML = text;
+    const setText     = (text: string)   => loadingText.innerHTML = text;
     const setProgress = (amount: number) => loadingBar.style.width = amount + "%";
 
-    setProgress(initialPercent);
+    let errored = false;
+    let curPercent: number;
+    setProgress((curPercent = initialPercent));
 
     for (let [endPercent, label, fn] of segments) {
         setText(label);
-        await fn();
-        setProgress(endPercent);
+
+        try {
+            await fn((percentDone) => {
+                // Update "sub-percent", i.e. for Images, would show progress as images load
+                if (errored)
+                    return;
+                setProgress(curPercent + (endPercent - curPercent) * percentDone);
+                setText(`${label} (${Math.floor(percentDone*100)}%)`);
+            });
+        } catch (e) {
+            // It's assumed any errors not caught within the segment function
+            //  are critical enough to stop the site from loading
+            errored = true;
+
+            // Create URL to auto-fill a Github issue with the error
+            const issueURL = new URL("https://github.com/OpenCircuits/OpenCircuits/issues/new");
+            if (isError(e)) {
+                issueURL.searchParams.set(
+                    "body",
+                    `Auto-generated error:\nError occurred while "${label}"\n${e.name}: ${e.message}\n${getStackTrace(e).join("\n")}\n`
+                );
+            }
+            issueURL.searchParams.set(
+                "title",
+                e.toString()
+            );
+
+            // Set loading bar to red w/ Error and link to create issue for the error
+            loadingBar.style.backgroundColor = "#f44336";
+            loadingText.innerHTML = `<a href="${issueURL.toString()}" target="_blank">
+                Error occurred while "${label}". Please refresh the page.
+                <br />
+                If this error continues to occur, please click this text to submit a bug report.
+            </a>`;
+
+            console.error(e);
+
+            return;
+        }
+
+        setProgress((curPercent = endPercent));
     }
 
     document.getElementById(id).style.display = "none";
