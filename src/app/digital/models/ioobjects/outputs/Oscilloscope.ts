dc3ae5a20e37b565c6cff3af90bd4a769e4de3e8
Leon Montealegre <leonm99@gmail.com>
2021-11-03 00:18:29 -0400

    Add signal tracking for Oscilloscope

--- src/app/digital/models/ioobjects/outputs/Oscilloscope.ts
+++ src/app/digital/models/ioobjects/outputs/Oscilloscope.ts
@@ -9,23 +9,41 @@ import {serializable, serialize} from "serialeazy";
 import {DigitalComponent} from "digital/models/DigitalComponent";
 
 
+const NUM_SEGMENTS = 100;
+
+
 @serializable("Oscilloscope")
 export class Oscilloscope extends DigitalComponent {
     private signals: boolean[];
+    private frequency: number;
 
     public constructor() {
         super(new ClampedValue(1),
               new ClampedValue(0),
               V(400, 200));
+
+        this.signals = [];
+        this.frequency = 100;
+
+        this.tick();
     }
 
     public tick(): void {
+        // Add signal
+        this.signals.push(this.inputs.first.getIsOn());
+        if (this.signals.length > NUM_SEGMENTS)
+            this.signals.splice(0, 1);
+
         if (this.designer !== undefined)
             this.designer.forceUpdate();
 
         window.setTimeout(() => {
             this.tick();
         }, this.frequency);
+    }
+
+    public getSignals(): boolean[] {
+        return this.signals.slice();
     }
 
     public getDisplayName(): string {
