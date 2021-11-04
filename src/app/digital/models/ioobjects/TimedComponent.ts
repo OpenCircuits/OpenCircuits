3e85b2549ed47ebf11cbb18228e697c7ba7fcd8b
Leon Montealegre <leonm99@gmail.com>
2021-11-03 22:36:52 -0400

    Refactored clock/oscilloscope to use new TimedComponent class

---
+++ src/app/digital/models/ioobjects/TimedComponent.ts
@@ -0,0 +1,77 @@
+import {serialize} from "serialeazy";
+
+import {Vector} from "Vector";
+import {ClampedValue} from "math/ClampedValue";
+
+import {DigitalComponent} from "digital/models/DigitalComponent";
+
+
+export abstract class TimedComponent extends DigitalComponent {
+    @serialize
+    protected frequency: number;
+    @serialize
+    protected paused: boolean;
+
+    private timeout: number;
+
+    public constructor(initialFreq: number, inputPortCount: ClampedValue, outputPortCount: ClampedValue, size: Vector) {
+        super(inputPortCount, outputPortCount, size);
+
+        this.frequency = initialFreq;
+        this.paused = false;
+    }
+
+    private stopTimeout(): void {
+        // Clear the timeout if it's currently set
+        if (this.timeout !== null) {
+            window.clearTimeout(this.timeout);
+            this.timeout = null;
+        }
+    }
+
+    private tick(): void {
+        this.stopTimeout();
+
+        this.onTick();
+
+        // Send an update to the designer
+        if (this.designer !== undefined)
+            this.designer.forceUpdate();
+
+        // Recursively call `tick` to continuously update
+        this.timeout = window.setTimeout(() => {
+            this.timeout = null;
+            this.tick();
+        }, this.frequency);
+    }
+
+    protected abstract onTick(): void;
+
+    public setFrequency(freq: number): void {
+        this.frequency = freq;
+    }
+
+    public pause(): void {
+        this.paused = true;
+        this.stopTimeout();
+    }
+
+    public resume(): void {
+        this.paused = false;
+        this.tick();
+    }
+
+    public getFrequency(): number {
+        return this.frequency;
+    }
+
+    public isPaused(): boolean {
+        return this.paused;
+    }
+
+    // Restart ticking
+    public reset(): void {
+        if (!this.paused)
+            this.tick();
+    }
+}
--- src/app/digital/models/ioobjects/outputs/Oscilloscope.ts
+++ src/app/digital/models/ioobjects/outputs/Oscilloscope.ts
@@ -1,62 +1,30 @@
-import {V} from "Vector";
-import {ClampedValue} from "math/ClampedValue";
 import {serializable, serialize} from "serialeazy";
 
-import {DigitalComponent} from "digital/models/DigitalComponent";
+import {V} from "Vector";
+import {ClampedValue} from "math/ClampedValue";
+
+import {TimedComponent} from "../TimedComponent";
 
 
 @serializable("Oscilloscope")
-export class Oscilloscope extends DigitalComponent {
+export class Oscilloscope extends TimedComponent {
     @serialize
     private numSamples: number;
     @serialize
-    private frequency: number;
-    @serialize
     private signals: boolean[];
-    @serialize
-    private paused: boolean;
-
-    private timeout: number;
 
     public constructor() {
-        super(new ClampedValue(1),
-            new ClampedValue(0),
-            V(400, 200));
+        super(100, new ClampedValue(1), new ClampedValue(0), V(400, 200));
 
         this.numSamples = 100;
-        this.signals = [];
-        this.frequency = 100;
-
-        this.paused = false;
-
-        this.tick();
+        this.reset();
     }
 
-    private resetTimeout(): void {
-        // Clear the timeout if it's currently set
-        if (this.timeout !== null) {
-            window.clearTimeout(this.timeout);
-            this.timeout = null;
-        }
-    }
-
-    public tick(): void {
-        this.resetTimeout();
-
+    protected onTick(): void {
         // Add signal
         this.signals.push(this.inputs.first.getIsOn());
         if (this.signals.length > this.numSamples)
             this.signals.splice(0, 1);
-
-        // Send an update to the designer
-        if (this.designer !== undefined)
-            this.designer.forceUpdate();
-
-        // Call tick again after `frequency` time
-        this.timeout = window.setTimeout(() => {
-            this.timeout = null;
-            this.tick();
-        }, this.frequency);
     }
 
     public setNumSamples(num: number): void {
@@ -65,32 +33,9 @@ export class Oscilloscope extends DigitalComponent {
             this.signals.splice(0, (this.signals.length-this.numSamples));
     }
 
-    public setFrequency(freq: number): void {
-        this.frequency = freq;
-    }
-
     public reset(): void {
         this.signals = [];
-        if (!this.paused)
-            this.tick();
-    }
-
-    public pause(): void {
-        this.paused = true;
-        this.resetTimeout();
-    }
-
-    public resume(): void {
-        this.paused = false;
-        this.tick();
-    }
-
-    public isPaused(): boolean {
-        return this.paused;
-    }
-
-    public getFrequency(): number {
-        return this.frequency;
+        super.reset();
     }
 
     public getNumSamples(): number {
