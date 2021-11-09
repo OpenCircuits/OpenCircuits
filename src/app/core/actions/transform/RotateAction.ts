c346a0b9f694f3d11fff6c03b55dc089e23c6a10
ezier <elanorzierhut@gmail.com>
2021-11-09 16:27:22 -0500

    Update RotateAction.ts
    Documentation for RotateAction.ts

--- src/app/core/actions/transform/RotateAction.ts
+++ src/app/core/actions/transform/RotateAction.ts
@@ -3,6 +3,9 @@ import {Vector} from "Vector";
 import {Action} from "core/actions/Action";
 import {Component} from "core/models/Component";
 
+/*
+ * Implementation of Action interface for rotating components
+ */
 export class RotateAction implements Action {
     private objects: Array<Component>;
 
@@ -11,6 +14,9 @@ export class RotateAction implements Action {
     private initialAngles: Array<number>;
     private finalAngles: Array<number>;
 
+    /*
+     * Creates a rotation for a group of components
+     */
     public constructor(objects: Array<Component>, midpoint: Vector, initialAngles: Array<number>, finalAngles: Array<number>) {
         this.objects = objects;
         this.midpoint = midpoint;
@@ -18,6 +24,9 @@ export class RotateAction implements Action {
         this.finalAngles = finalAngles;
     }
 
+    /*
+     * Rotates a component or group of components.
+     */
     private setAngles(angles: Array<number>): void {
         for (let i = 0; i < this.objects.length; i++) {
             const obj = this.objects[i];
@@ -25,12 +34,21 @@ export class RotateAction implements Action {
         }
     }
 
+    /*
+     * Rotates a component or group of components from the initial angle
+     * to the final angle.
+     */
     public execute(): Action {
         this.setAngles(this.finalAngles);
 
         return this;
     }
 
+    /*
+     * Reverts the RotateAction object's angles to the original
+     * angles it was set to, putting the components back in their initial
+     * orientation.
+     */
     public undo(): Action {
         this.setAngles(this.initialAngles);
 
