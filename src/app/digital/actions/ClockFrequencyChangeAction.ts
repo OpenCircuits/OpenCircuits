7d639075e81384b94c6be2482993782ef795ed5d
Atlus.GL <guolong0512@outlook.com>
2021-11-09 13:45:23 -0500

    Add comments to 4 files.

--- src/app/digital/actions/ClockFrequencyChangeAction.ts
+++ src/app/digital/actions/ClockFrequencyChangeAction.ts
@@ -2,24 +2,46 @@ import {Action} from "core/actions/Action";
 import {Clock} from "digital/models/ioobjects/inputs/Clock";
 
 export class ClockFrequencyChangeAction implements Action {
+    /**
+     * The clock component of this Action
+     */
     private clock: Clock;
-
+    
+    /**
+     * The initial frequency of this Action
+     */
     private initialFreq: number;
+    
+    /**
+     * The target frequency of this Action
+     */
     private targetFreq: number;
-
+   
+    /**
+     * Initialize a ClockFrequencyChangeAction with the clock and 
+     * target frequency given.
+     * @param clock The clock that we need to change the frequency
+     * @param targetFreq The frequency that we want the clock change to
+     */
     public constructor(clock: Clock, targetFreq: number) {
         this.clock = clock;
 
         this.initialFreq = clock.getFrequency();
         this.targetFreq = targetFreq;
     }
-
+    /**
+     * Set the frequency of the clock to the target ferquency.
+     * @returns This action
+     */
     public execute(): Action {
         this.clock.setFrequency(this.targetFreq);
 
         return this;
     }
-
+    /**
+     * Undo the changing action, back the frequency of the clock to initialFreq
+     * @returns This action
+     */
     public undo(): Action {
         this.clock.setFrequency(this.initialFreq);
 
