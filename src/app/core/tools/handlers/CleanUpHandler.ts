5536e7d8deeceaf39463e123fbb477fb8d819449
Atlus.GL <guolong0512@outlook.com>
2021-11-11 16:27:44 -0500

    fix the format

--- src/app/core/tools/handlers/CleanUpHandler.ts
+++ src/app/core/tools/handlers/CleanUpHandler.ts
@@ -1,24 +1,24 @@
-import { K_KEY } from "core/utils/Constants";
+import {K_KEY} from "core/utils/Constants";
 
 import {Event} from "core/utils/Events";
 import {CircuitInfo} from "core/utils/CircuitInfo";
-import { Component } from "core/models";
+import {Component} from "core/models";
 
 import {EventHandler} from "../EventHandler";
 
-import { GroupAction } from "core/actions/GroupAction";
-import { RotateAction } from "core/actions/transform/RotateAction";
+import {GroupAction} from "core/actions/GroupAction";
+import {RotateAction} from "core/actions/transform/RotateAction";
 
 
 
-export const CleanUpHandler: EventHandler=({
+export const CleanUpHandler: EventHandler = ({
     conditions: (event: Event, {designer}: CircuitInfo) =>
         (event.type === "keydown" &&
          event.key === K_KEY &&
          // Don't want to select all if nothing to select or everything is already selected
          designer.getObjects().length > 0),
 
-    getResponse: ({history,designer,selections}: CircuitInfo) =>{
+    getResponse: ({history,designer,selections}: CircuitInfo) => {
         // Reset the selected units' angle to 0.
         // If nothing is selected, select all units.
         const action = new GroupAction();
@@ -31,6 +31,4 @@ export const CleanUpHandler: EventHandler=({
 
         history.add(action.execute()); 
     }
-
-
 });
