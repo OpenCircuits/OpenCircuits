7c3cb8a8915a6eb772fb1055dfe2429d7e2e4ceb
Vishal Hada <53910045+vhada7@users.noreply.github.com>
2021-11-19 00:07:43 -0500

    Update ColorModule.tsx

--- src/site/pages/digital/src/containers/SelectionPopup/modules/ColorModule.tsx
+++ src/site/pages/digital/src/containers/SelectionPopup/modules/ColorModule.tsx
@@ -4,8 +4,9 @@ import {Label, LED} from "digital/models/ioobjects";
 import {Wire} from "core/models/Wire";
 import {Node, isNode} from "core/models/Node";
 import {IOObject, Port} from "core/models";
+import {UndoHandler} from "core/tools/handlers/UndoHandler";
 import {CreateModule, ModuleConfig, PopupModule} from "shared/containers/SelectionPopup/modules/Module";
-import { UndoHandler } from "core/tools/handlers/UndoHandler";
+
 
 const Config: ModuleConfig<[LED, Label, Wire], string> = {
     types: [LED, Label, Wire],
@@ -13,8 +14,8 @@ const Config: ModuleConfig<[LED, Label, Wire], string> = {
     isActive: (selections) => {
     // Make sure that this is only active if all the types are LED/Label/Wire/Node, but not ONLY Nodes
     return (selections.all((s) => (isNode(s) || s instanceof LED || s instanceof Label || s instanceof Wire)) &&
-        !selections.all((s) => isNode(s)));
-        },
+    !selections.all((s) => isNode(s))); 
+    },
     getProps: (o) => (isNode(o) ? undefined : o.getColor()),
     getAction: (s, newCol) => new GroupAction(s.filter(o => !isNode(o)).map(o => new ColorChangeAction(o, newCol)))
 }
