80fca82794c56a1b870f4f31963bbf6aef4efbfe
Leon Montealegre <leonm99@gmail.com>
2021-11-22 16:46:52 -0500

    Small formatting fix

--- src/site/pages/digital/src/containers/SelectionPopup/modules/ColorModule.tsx
+++ src/site/pages/digital/src/containers/SelectionPopup/modules/ColorModule.tsx
@@ -12,8 +12,9 @@ const Config: ModuleConfig<[LED, Label, Wire], string> = {
     types: [LED, Label, Wire],
     valType: "string",
     isActive: (selections) => {
-    // Make sure that this is only active if all the types are LED/Label/Wire/Node, but not ONLY Nodes
-    return (selections.all((s) => (isNode(s) || s instanceof LED || s instanceof Label || s instanceof Wire)) && !selections.all((s) => isNode(s))); 
+        // Make sure that this is only active if all the types are LED/Label/Wire/Node, but not ONLY Nodes
+        return selections.all((s) => (isNode(s) || s instanceof LED || s instanceof Label || s instanceof Wire)) &&  
+               !selections.all((s) => isNode(s)); 
     },
     getProps: (o) => (isNode(o) ? undefined : o.getColor()),
     getAction: (s, newCol) => new GroupAction(s.filter(o => !isNode(o)).map(o => new ColorChangeAction(o, newCol)))
