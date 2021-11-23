896d478126f47c85c51a6a79aaa1c52394b6cdbb
Caitlin Callahan <caitlinc362@gmail.com>
2021-11-22 20:12:18 -0500

    revised comments to the LED file

--- src/app/digital/models/ioobjects/outputs/LED.ts
+++ src/app/digital/models/ioobjects/outputs/LED.ts
@@ -10,9 +10,7 @@ import {DigitalComponent} from "digital/models/DigitalComponent";
 
 /**
  * The LED turns on when signal input connnected to it is 1
- * 
- * Outputs a bright light
- * 
+ * Outputs a bright light 
  */
 
 @serializable("LED")
@@ -46,8 +44,8 @@ export class LED extends DigitalComponent {
     }
 
     /**
-     * 
-     * @returns adds extra offset if LED is on
+     * Returns offset of light radius when LED is on, if the LED is off, there is no offset to return (0).
+     * @returns Gets extra offset of light radius if LED is on
      */
     public getOffset(): Vector {
         // Add extra offset if this LED is on (to account for light)
@@ -55,46 +53,46 @@ export class LED extends DigitalComponent {
     }
 
     /**
-     * sets the color of the LED
-     * @param color inputs a color
+     * Sets the color of the LED
+     * @param color Inputs a color
      */
     public setColor(color: string): void {
         this.color = color;
     }
 
     /**
-     * 
-     * @returns light status of LED
+     * Returns true (1) if LED is on
+     * @returns Light status of LED
      */
 
     public isOn(): boolean {
         return this.inputs.first.getIsOn();
     }
     /**
-     * 
-     * @returns color of the LED
+     * Returns the color of the LED
+     * @returns Color of the LED
      */
     public getColor(): string {
         return this.color;
     }
     /**
-     * 
-     * @returns displayname of the LED
+     * Returns the display name of the LED
+     * @returns Display name of the LED
      */
     public getDisplayName(): string {
         return "LED";
     }
     /**
-     * 
-     * @returns image name of the LED (OFF)
+     * Returns the image name of the LED in the off state
+     * @returns Image name of the LED (OFF)
      */
     public getImageName(): string {
         return "led.svg";
     }
 
     /**
-     * 
-     * @returns image name of the LED (ON)
+     * Returns the image name of the LED in the on state
+     * @returns Image name of the LED (ON)
      */
     public getOnImageName(): string {
         return "ledLight.svg"
