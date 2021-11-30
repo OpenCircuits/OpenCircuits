dae6a38feb9075ce24ea82505141d945a2c7c4fe
Vishal Hada <53910045+vhada7@users.noreply.github.com>
2021-11-30 17:00:32 -0500

    Update CoderPortChangeAction.ts

--- src/app/digital/actions/ports/CoderPortChangeAction.ts
+++ src/app/digital/actions/ports/CoderPortChangeAction.ts
@@ -12,11 +12,11 @@ import {OutputPortChangeAction} from "./OutputPortChangeAction";
 
 
 /**
-* This code changes the size of the Encoder/Decoder object based on how many inputs/ouputs are entered.
-* The number of inputs (decoder) or outputs (encoder) are increased by 2 to the power of the number of inputs/outputs chosen.
-* Ex.) output count chosen  = 3, then the number of inputs changes to 2^3 or 8 for an encoder.
-* The actual size of the object is also changed accordingly.
-*/
+ * This code changes the size of the Encoder/Decoder object based on how many inputs/ouputs are entered.
+ * The number of inputs (decoder) or outputs (encoder) are increased by 2 to the power of the number of inputs/outputs chosen.
+ * Ex.) output count chosen  = 3, then the number of inputs changes to 2^3 or 8 for an encoder.
+ * The actual size of the object is also changed accordingly.
+ */
 export class CoderPortChangeAction implements Action {
     protected obj: Encoder | Decoder;
 
@@ -27,12 +27,12 @@ export class CoderPortChangeAction implements Action {
     protected outputPortAction: OutputPortChangeAction;
 
     /**
-    * The constrcutor determines whether the object is an encoder or decoder and calls on the corresponding actions based on the type.
-    *
-    * @param obj is the encoder or decoder selected
-    * @param initial the inital number of outputs/inputs
-    * @param target the target numbe of outputs/inputs
-    */
+     * The constrcutor determines whether the object is an encoder or decoder and calls on the corresponding actions based on the type.
+     *
+     * @param obj is the encoder or decoder selected
+     * @param initial the inital number of outputs/inputs
+     * @param target the target numbe of outputs/inputs
+     */
     public constructor(obj: Encoder | Decoder, initial: number, target: number) {
         this.obj = obj;
         this.targetCount = target;
@@ -49,19 +49,19 @@ export class CoderPortChangeAction implements Action {
     }
     
     /**
-    * This code changes the size of the actual object.
-    *
-    * @param val refers to the target number chosen by the user.
-    */
+     * This code changes the size of the actual object.
+     *
+     * @param val refers to the target number chosen by the user.
+     */
     protected changeSize(val: number): void {
         this.obj.setSize(V(DEFAULT_SIZE, DEFAULT_SIZE/2 * Math.pow(2, val)));
     }
     
     /**
-    * This function calls upon the change size function and the port actions to change the size of the obj and the number of inputs/output ports.
-    *
-    * @returns the changed object
-    */
+     * This function calls upon the change size function and the port actions to change the size of the obj and the number of inputs/output ports.
+     *
+     * @returns the changed object
+     */
     public execute(): Action {
         // Change size first
         this.changeSize(this.targetCount);
@@ -72,10 +72,10 @@ export class CoderPortChangeAction implements Action {
     }
     
     /**
-    * This function undoes the execute(): Action function and sets the size and number of ports back to the inital count and size.
-    *
-    * @returns the initial object
-    */
+     * This function undoes the execute(): Action function and sets the size and number of ports back to the inital count and size.
+     *
+     * @returns the initial object
+     */
     public undo(): Action {
         // Change size back first
         this.changeSize(this.initialCount);
