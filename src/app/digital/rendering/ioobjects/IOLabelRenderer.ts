d475feb033e55d22eb77ee8dc92a00ae0fdaedb1
tkoz0 <null>
2021-12-04 23:19:05 -0500

    Changed port label rendering to use the origin position

--- src/app/digital/rendering/ioobjects/IOLabelRenderer.ts
+++ src/app/digital/rendering/ioobjects/IOLabelRenderer.ts
@@ -1,10 +1,10 @@
+import {IO_LABEL_DIR_PADDING, IO_LABEL_VERTICAL_TEXT_PADDING} from "core/utils/Constants";
 import {V, Vector} from "Vector";
-import {Clamp,
-        GetNearestPointOnRect} from "math/MathUtils";
 
 import {Camera} from "math/Camera";
 import {Renderer} from "core/rendering/Renderer";
 import {Component} from "core/models/Component";
+import {Port} from "core/models";
 
 /**
  * Renders IOLabels
@@ -13,17 +13,19 @@ import {Component} from "core/models/Component";
  */
 export const IOLabelRenderer = (() => {
 
-    const drawPortText = function(renderer: Renderer, pos0: Vector, name: string, size: Vector): void {
+    const drawPortText = function(renderer: Renderer, port: Port, size: Vector): void {
         const align: CanvasTextAlign = "center";
-        const padding = 8;
-        const ww = renderer.getTextWidth(name)/2;
+        const textWidth = renderer.getTextWidth(port.getName());
+        let pos = port.getOriginPos();
 
-        let pos = GetNearestPointOnRect(V(-size.x/2, -size.y/2), V(size.x/2, size.y/2), pos0);
-        pos = pos.sub(pos0).normalize().scale(padding).add(pos);
-        pos.x = Clamp(pos.x, -size.x/2+padding+ww, size.x/2-padding-ww);
-        pos.y = Clamp(pos.y, -size.y/2+14, size.y/2-14);
+        // move a padding distance from the origin position
+        pos = pos.add(port.getDir().scale(-IO_LABEL_DIR_PADDING));
+        // move half the text width distance projected onto a horizontal vector
+        pos = pos.add(port.getDir().scale(-textWidth/2).project(V(1,0)));
+        // add in vertical direction so label is a bit farther from port
+        pos = pos.add(port.getDir().scale(-IO_LABEL_VERTICAL_TEXT_PADDING).project(V(0,1)));
 
-        renderer.text(name, pos, align);
+        renderer.text(port.getName(), pos, align);
     }
 
     return {
@@ -31,18 +33,9 @@ export const IOLabelRenderer = (() => {
             if (!camera.cull(object.getCullBox()))
                 return;
 
-            const transform = object.getTransform();
-            const size: Vector = transform.getSize();
-
-            const ports = object.getPorts();
-            for (const port of ports) {
-                const name = port.getName();
-                const pos = port.getTargetPos();
-                if (name)
-                    drawPortText(renderer, pos, name, size);
-            }
+            const size = object.getTransform().getSize();
+            object.getPorts().forEach((p) => drawPortText(renderer, p, size));
         }
     };
-
 
 }) ();
