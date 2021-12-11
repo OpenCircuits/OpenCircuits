dffa0309466cd933ee762aaf87fe0a10913dc1ea
Caitlin Callahan <caitlinc362@gmail.com>
2021-12-10 22:35:08 -0500

    Added test function for the comparator

---
+++ src/app/tests/digital/models/ioobjects/other/Comparator.test.ts
@@ -0,0 +1,47 @@
+import "jest";
+
+import {DigitalCircuitDesigner} from "digital/models/DigitalCircuitDesigner";
+import {Encoder} from "digital/models/ioobjects/other/Encoder";
+import {Decoder} from "digital/models/ioobjects/other/Decoder";
+
+import {GetHelpers} from "test/helpers/Helpers";
+import { Comparator, ConstantNumber, LED } from "digital/models/ioobjects";
+
+
+describe("Comparator", () => {
+    const designer = new DigitalCircuitDesigner(0);
+    const {Place,Connect} = GetHelpers({designer});
+
+    const [c] = Place(new Comparator());
+    const [a,b] = Place(new ConstantNumber(), new ConstantNumber());
+    const [lt,eq,gt] = Place(new LED(), new LED(), new LED());
+
+    c.setInputPortCount(4);
+
+    // Connect everything together
+    for (let i = 0; i < 4; i++) {
+        Connect(a, i, c, i);
+        Connect(b, i, c, i+4);
+    }
+    Connect(c, Comparator.LT_PORT, lt, 0);
+    Connect(c, Comparator.EQ_PORT, eq, 0);
+    Connect(c, Comparator.GT_PORT, gt, 0);
+
+    test("Numbers 0-15", () => {
+        a.setInput(5);
+        b.setInput(6);
+        expect(lt.isOn()).toBe((5 < 6));
+        expect(gt.isOn()).toBe((5 > 6));
+
+        for (let i = 0; i < 15; i++){
+            a.setInput(i);
+            for (let j = 0; j < 15; j++){
+                b.setInput(j);
+        
+                expect(lt.isOn()).toBe((i < j));
+                expect(gt.isOn()).toBe((i > j));
+                expect(eq.isOn()).toBe((i===j));                                
+            }
+        }
+    });
+});
