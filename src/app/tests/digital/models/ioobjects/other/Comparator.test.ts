39640b348d1505bf54bd9c4808fd493f136daa07
Caitlin Callahan <caitlinc362@gmail.com>
2021-12-10 23:05:27 -0500

    Added some comments to test file

--- src/app/tests/digital/models/ioobjects/other/Comparator.test.ts
+++ src/app/tests/digital/models/ioobjects/other/Comparator.test.ts
@@ -12,6 +12,7 @@ describe("Comparator", () => {
     const designer = new DigitalCircuitDesigner(0);
     const {Place,Connect} = GetHelpers({designer});
 
+    //create necessary components for testing
     const [c] = Place(new Comparator());
     const [a,b] = Place(new ConstantNumber(), new ConstantNumber());
     const [lt,eq,gt] = Place(new LED(), new LED(), new LED());
@@ -26,7 +27,8 @@ describe("Comparator", () => {
     Connect(c, Comparator.LT_PORT, lt, 0);
     Connect(c, Comparator.EQ_PORT, eq, 0);
     Connect(c, Comparator.GT_PORT, gt, 0);
-
+    
+    //testing numbers 0-15 to make sure they work
     test("Numbers 0-15", () => {
         a.setInput(5);
         b.setInput(6);
