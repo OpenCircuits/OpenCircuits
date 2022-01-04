eb2e24e40eb3ea0f6c4f357a1b4ba74cc0f8a18d
Leon Montealegre <leonm99@gmail.com>
2022-01-04 16:23:37 -0500

    Fixed ref errors in InputField

--- src/site/shared/components/InputField/index.tsx
+++ src/site/shared/components/InputField/index.tsx
@@ -1,21 +1,21 @@
-import {useEffect, useRef} from "react"
+import React, {useEffect, useRef} from "react"
 
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
     onEnter?: (e: KeyboardEvent) => void;
 }
-export const InputField = (props: Props) => {
-    const ref = useRef<HTMLInputElement>();
+export const InputField = React.forwardRef(({onEnter, ...props}: Props, ref: React.MutableRefObject<HTMLInputElement>) => {
+    ref = ref ?? useRef<HTMLInputElement>();
 
     useEffect(() => {
         ref.current.addEventListener("keyup", function(evt) {
             if (evt.key === "Escape" || evt.key === "Enter")
                 ref.current.blur();
 
-            if (evt.key === "Enter" && props.onEnter)
-                props.onEnter(evt);
+            if (evt.key === "Enter" && onEnter)
+                onEnter(evt);
         });
     }, [ref]);
 
     return <input ref={ref} {...props} />
-}
+});
