23e2f9a2beaeeb173af0da31bc9db838f87f3caf
Trevor Crystal <TGCrystal@users.noreply.github.com>
2022-01-12 09:04:49 -0500

    Fix array check

--- src/site/shared/containers/ItemNav/index.tsx
+++ src/site/shared/containers/ItemNav/index.tsx
@@ -110,7 +110,7 @@ export const ItemNav = <D,>({ info, config, additionalData, onDelete, onStart, o
 
     const {w, h} = useWindowSize();
     let sections = (w > 768 || w > h) ? config.sections : [];
-    if (sections === []) {
+    if (sections.length === 0) {
         let numPerSection = Math.floor(w / 100);
         config.sections.forEach(section => {
             let items = section.items.slice();
