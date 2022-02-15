d267b5e5137ebeba54fe34ae032931f999b0fabc
Ella Rieder <ellagracerieder@yahoo.com>
2022-02-15 17:06:46 -0500

    CopyHandler update

--- src/app/core/tools/handlers/CopyHandler.ts
+++ src/app/core/tools/handlers/CopyHandler.ts
@@ -14,7 +14,14 @@ import {EventHandler} from "../EventHandler";
 export const CopyHandler: EventHandler = ({
     conditions: (event: Event, {selections}: CircuitInfo) =>
         ((event.type === "copy" || event.type === "cut") &&
-         selections.amount() > 0),
+         selections.amount() > 0) &&
+        // If a label element is not selected, 
+        // or if an editable component is selected but it is a "Caret" (cursor) instead of highlighted text,
+        // then copy the component
+        // Otherwise there is text selected, so do default copying
+        // Necessary to fix #874
+        (document.getSelection()?.anchorNode?.nodeName !== "LABEL" || 
+         document.getSelection()?.type === "Caret"),
 
     getResponse: ({selections, designer, history}: CircuitInfo, {type, ev}: CopyPasteEvent) => {
         const objs = selections.get().filter(o => o instanceof IOObject) as IOObject[];
@@ -27,16 +34,8 @@ export const CopyHandler: EventHandler = ({
         // We don't copy the data from the json since it will cause 
         // some weird error, which will cause the issue #746
         ev.clipboardData.setData("text/plain", str);
-
-        // If an editable component (LABEL) is not selected, 
-        // or if an editable component is selected but it is a "Caret" (cursor) instead of highlighted text,
-        // then copy the component
-        // Otherwise there is text selected, so do default copying
-        // Necessary to fix #874
-        if (document.getSelection()?.anchorNode?.nodeName !== "LABEL" || document.getSelection()?.type === "Caret") {
-            ev.preventDefault(); // Necessary to copy correctly
-        }
-
+        ev.preventDefault(); // Necessary to copy correctly
+        
         if (type === "cut") {
             // Delete selections
             history.add(new GroupAction([
