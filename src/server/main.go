57d06358cc92b02a8c9aca26f6a67551c7b8bab7
brandon-red <brandonred627@gmail.com>
2022-02-21 22:56:15 -0500

    created environment variables and commented out flag variable parsing

--- src/server/main.go
+++ src/server/main.go
@@ -35,27 +35,38 @@ func getPort() string {
 func main() {
 	var err error
 
+	// First try with env 
+	os.Setenv("google_auth", "")
+	os.Setenv("no_auth", "false")
+	os.Setenv("interface", "sqlite")
+	os.Setenv("sqlitePath", "sql/sqlite")
+	os.Setenv("ds_emu_host", "")
+	os.Setenv("ds_emu_project_id", "")
+	os.Setenv("ip_address", "0.0.0.0")
+	os.Setenv("port", "8080")
+
 	// Parse flags
-	googleAuthConfig := flag.String("google_auth", "", "<path-to-config>; Enables google sign-in API login")
-	noAuthConfig := flag.Bool("no_auth", false, "Enables username-only authentication for testing and development")
-	userCsifConfig := flag.String("interface", "sqlite", "The storage interface")
-	sqlitePathConfig := flag.String("sqlitePath", "sql/sqlite", "The path to the sqlite working directory")
-	dsEmulatorHost := flag.String("ds_emu_host", "", "The emulator host address for cloud datastore")
-	dsProjectId := flag.String("ds_emu_project_id", "", "The gcp project id for the datastore emulator")
-	ipAddressConfig := flag.String("ip_address", "0.0.0.0", "IP address of server")
-	portConfig := flag.String("port", "8080", "Port to serve application, use \"auto\" to select the first available port starting at 8080")
-	flag.Parse()
+	//googleAuthConfig := flag.String("google_auth", "", "<path-to-config>; Enables google sign-in API login")
+	//noAuthConfig := flag.Bool("no_auth", false, "Enables username-only authentication for testing and development")
+	//userCsifConfig := flag.String("interface", "sqlite", "The storage interface")
+	//sqlitePathConfig := flag.String("sqlitePath", "sql/sqlite", "The path to the sqlite working directory")
+	//dsEmulatorHost := flag.String("ds_emu_host", "", "The emulator host address for cloud datastore")
+	//dsProjectId := flag.String("ds_emu_project_id", "", "The gcp project id for the datastore emulator")
+	//ipAddressConfig := flag.String("ip_address", "0.0.0.0", "IP address of server")
+	//portConfig := flag.String("port", "8080", "Port to serve application, use \"auto\" to select the first available port starting at 8080")
+	//flag.Parse()
 
 	// Bad way of registering if we're in prod and using gcp datastore and OAuth credentials
+	// Where is this env variable set?
 	if os.Getenv("DATASTORE_PROJECT_ID") != "" {
-		*googleAuthConfig = "credentials.json"
+		os.Setenv("google_auth", "credentials.json")
 		*userCsifConfig = "gcp_datastore"
 	}
 
 	// Register authentication method
 	authManager := auth.AuthenticationManager{}
 	if *googleAuthConfig != "" {
-		authManager.RegisterAuthenticationMethod(google.New(*googleAuthConfig))
+		authManager.RegisterAuthenticationMethod(google.New(os.Getenv("google_auth"))))
 	}
 	if *noAuthConfig {
 		authManager.RegisterAuthenticationMethod(auth.NewNoAuth())
