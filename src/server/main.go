2049ba8c6db3c38e1ecf67908f97c6cd2f47e05d
brandon-red <brandonred627@gmail.com>
2022-03-01 17:13:51 -0500

    adjusted rest of main.go based on new env variables

--- src/server/main.go
+++ src/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"net"
 	"os"
 	"strconv"
@@ -32,9 +31,9 @@ func getPort() string {
 	return "8080"
 }
 
-// Check if env variable exists, if not set to default value, return value of env variable 
+// Check if env variable exists, if not set to default value, return value of env variable
 func checkEnv(key string, defaultVal string) string {
-	if !LookupEnv(key){
+	if os.Getenv(key) == "" {
 		os.Setenv(key, defaultVal)
 	}
 	return os.Getenv(key)
@@ -44,42 +43,43 @@ func main() {
 	var err error
 
 	// Unsure how to store help messages in env variables, for now they are commented
-	googleAuthConfig := checkEnv("google_auth", "") // <path-to-config>; Enables google sign-in API login
-	noAuthConfig := checkEnv("no_auth", "false") // Enables username-only authentication for testing and development
-	userCsifConfig := checkEnv("interface", "sqlite") // The storage interface
-	sqlitePathConfig := checkEnv("sqlitePath", "sql/sqlite") // The path to the sqlite working directory 
-	dsEmulatorHost := checkEnv("ds_emu_host", "") // The emulator host address for cloud datastore
-	dsProjectId := checkEnv("ds_emu_project_id", "") // The gcp project id for the datastore emulator
-	ipAddressConfig := checkEnv("ip_address", "0.0.0.0") // IP address of server
-	portConfig := checkEnv("port", "8080") // Port to serve application, use \"auto\" to select the first available port starting at 8080
-	
+	googleAuthConfig := checkEnv("google_auth", "")          // <path-to-config>; Enables google sign-in API login
+	noAuthConfig := checkEnv("no_auth", "false")             // Enables username-only authentication for testing and development
+	userCsifConfig := checkEnv("interface", "sqlite")        // The storage interface
+	sqlitePathConfig := checkEnv("sqlitePath", "sql/sqlite") // The path to the sqlite working directory
+	dsEmulatorHost := checkEnv("ds_emu_host", "")            // The emulator host address for cloud datastore
+	dsProjectId := checkEnv("ds_emu_project_id", "")         // The gcp project id for the datastore emulator
+	ipAddressConfig := checkEnv("ip_address", "0.0.0.0")     // IP address of server
+	portConfig := checkEnv("port", "8080")                   // Port to serve application, use \"auto\" to select the first available port starting at 8080
+
 	// Bad way of registering if we're in prod and using gcp datastore and OAuth credentials
 	// Where is this env variable set?
 	if os.Getenv("DATASTORE_PROJECT_ID") != "" {
 		os.Setenv("google_auth", "credentials.json")
-		*userCsifConfig = "gcp_datastore"
+		os.Setenv("interface", "gcp_datastore")
 	}
 
 	// Register authentication method
 	authManager := auth.AuthenticationManager{}
-	if *googleAuthConfig != "" {
-		authManager.RegisterAuthenticationMethod(google.New(os.Getenv("google_auth"))))
+	if googleAuthConfig != "" {
+		authManager.RegisterAuthenticationMethod(google.New(googleAuthConfig))
 	}
-	if *noAuthConfig {
+	// Figure out a better way to do this (Convert to bool or decide on different indicator)
+	if noAuthConfig == "true" {
 		authManager.RegisterAuthenticationMethod(auth.NewNoAuth())
 	}
 
 	// Set up the storage interface
 	var userCsif interfaces.CircuitStorageInterfaceFactory
-	if *userCsifConfig == "mem" {
+	if userCsifConfig == "mem" {
 		userCsif = storage.NewMemStorageInterfaceFactory()
-	} else if *userCsifConfig == "sqlite" {
-		userCsif, err = sqlite.NewInterfaceFactory(*sqlitePathConfig)
+	} else if userCsifConfig == "sqlite" {
+		userCsif, err = sqlite.NewInterfaceFactory(sqlitePathConfig)
 		core.CheckErrorMessage(err, "Failed to load sqlite instance:")
-	} else if *userCsifConfig == "gcp_datastore_emu" {
-		userCsif, err = gcp_datastore.NewEmuInterfaceFactory(context.Background(), *dsProjectId, *dsEmulatorHost)
+	} else if userCsifConfig == "gcp_datastore_emu" {
+		userCsif, err = gcp_datastore.NewEmuInterfaceFactory(context.Background(), dsProjectId, dsEmulatorHost)
 		core.CheckErrorMessage(err, "Failed to load gcp datastore emulator instance:")
-	} else if *userCsifConfig == "gcp_datastore" {
+	} else if userCsifConfig == "gcp_datastore" {
 		userCsif, err = gcp_datastore.NewInterfaceFactory(context.Background())
 		core.CheckErrorMessage(err, "Failed to load gcp datastore instance: ")
 	}
@@ -102,9 +102,9 @@ func main() {
 	api.RegisterRoutes(router, authManager, userCsif)
 
 	// Check if portConfig is set to auto, if so find available port
-	if *portConfig == "auto" {
-		*portConfig = getPort()
+	if portConfig == "auto" {
+		os.Setenv("port", getPort())
 	}
 
-	router.Run(*ipAddressConfig + ":" + *portConfig)
+	router.Run(ipAddressConfig + ":" + portConfig)
 }
