e2a5e464e09c881c98e4e1d0b8f39d7ac31789ff
Vishal Hada <53910045+vhada7@users.noreply.github.com>
2022-03-15 17:01:37 -0400

    Update DeleteGroupActionFactory.ts

--- src/app/core/actions/deletion/DeleteGroupActionFactory.ts
+++ src/app/core/actions/deletion/DeleteGroupActionFactory.ts
@@ -14,20 +14,15 @@ import {isGeneratorFunction} from "util/types";
 export function CreateDeleteGroupAction(designer: CircuitDesigner, objects: IOObject[]): GroupAction {
     const action = new GroupAction();
     
-    const allDeletions = GatherGroup(objects, false);
+    const allDeletions = GatherGroup(objects,false);
     const components = allDeletions.getComponents();
     const wires = allDeletions.getWires();
 
     // Create actions for deletion of wires then objects
     //  order matters because the components need to be added
     //  (when undoing) before the wires can be connected
-    
     action.add(wires.map((wire)     => new DisconnectAction(designer, wire)));
     action.add(components.map((obj) => new DeleteAction(designer, obj)));
-    
+
     return action;
 }
-
-
-
-
