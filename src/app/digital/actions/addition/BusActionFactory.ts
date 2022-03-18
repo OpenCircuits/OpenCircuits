24d40b29fbf3f2f90cbda7c5796b81d84b187215
Leon Montealegre <leonm99@gmail.com>
2022-03-18 13:31:41 -0400

    Small formatting fixes

--- src/app/digital/actions/addition/BusActionFactory.ts
+++ src/app/digital/actions/addition/BusActionFactory.ts
@@ -10,21 +10,18 @@ export function CreateBusAction(outputPorts: OutputPort[], inputPorts: InputPort
     if (inputPorts.length !== outputPorts.length)
         throw new Error("Expected equal size input and output ports to bus!");
 
-    const action = new GroupAction();
     if (inputPorts.length === 0)
-        return action;
+        return new GroupAction();
 
     const designer = inputPorts[0].getParent().getDesigner();
     if (!designer)
         throw new Error("CreateBusAction failed: Designer not found");
 
-
     // Sort inputs/outputs by their position
     const sortByPos = (a: Port, b: Port) => {
         const p1 = a.getWorldTargetPos(), p2 = b.getWorldTargetPos();
-        if (Math.abs(p1.y - p2.y) <= .25){ // If same-ish-y, sort by x from LtR
+        if (Math.abs(p1.y - p2.y) <= .25) // If same-ish-y, sort by x from LtR
             return p1.x - p2.x;
-        }
         return p1.y - p2.y; // Sort by y-pos from Top to Bottom
     }
 
@@ -33,11 +30,7 @@ export function CreateBusAction(outputPorts: OutputPort[], inputPorts: InputPort
 
     // Connect ports by descending y-pos unless they are almost horizontal
     // in which they are connected left to right
-    for(let currPort = 0; currPort < inputPorts.length; currPort++){
-         // Create action
-         action.add(new ConnectionAction(designer, outputPorts[currPort]!, inputPorts[currPort]!));
-         // wire.setAsStraight(true); @TODO
-    }
-
-    return action;
+    return new GroupAction(inputPorts.map((inputPort, i) =>
+        new ConnectionAction(designer, inputPort, outputPorts[i])
+    ));
 }
