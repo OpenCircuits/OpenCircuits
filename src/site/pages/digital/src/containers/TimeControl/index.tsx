a195daf42d842c020842b71e368b22dbcfe0d608
Wyatt Hauben <whauben11@gmail.com>
2022-03-25 17:05:37 -0400

    very rough approx. of UI shape

--- src/site/pages/digital/src/containers/TimeControl/index.tsx
+++ src/site/pages/digital/src/containers/TimeControl/index.tsx
@@ -1,21 +1,5 @@
-// import {useLayoutEffect} from "react";
-
-// import {V} from "Vector";
-
-// import {Input} from "core/utils/Input";
-
-// import {CreateGroupPlaceAction} from "core/actions/addition/PlaceAction";
-// import {CreateDeselectAllAction} from "core/actions/selection/SelectAction";
 
 import {DigitalCircuitInfo} from "digital/utils/DigitalCircuitInfo";
-
-// import {useWindowSize} from "shared/utils/hooks/useWindowSize";
-// import {usePageVisibility} from "shared/utils/hooks/usePageVisibility";
-// import {Droppable} from "shared/components/DragDroppable/Droppable";
-
-// import {GetRenderFunc} from "site/digital/utils/Rendering";
-// import {useDigitalSelector} from "site/digital/utils/hooks/useDigital";
-// import {DigitalCreateN, SmartPlace, SmartPlaceOptions} from "site/digital/utils/DigitalCreate";
 
 import "./index.scss";
 
@@ -29,8 +13,9 @@ export const TimeControl = ({ info }: Props) => {
 
     return (
         <div className="container">
-            <button type="button" onClick={() => console.log("test")}>meow</button>
-            <p>test</p>
+            <button title="step" onClick={() => console.log("test")}>meow</button>
+            <button title="step2" onClick={() => console.log("test")}>meow</button>
+            <button title="step3" onClick={() => console.log("test")}>meow</button>
         </div>
     );
 }
