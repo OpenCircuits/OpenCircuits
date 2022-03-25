a088146e2645f362dd6c3a52396ffb27e2af5257
Leon Montealegre <leonm99@gmail.com>
2022-03-25 14:18:46 -0400

    Fixed comments and small formatting issues with PanTool

--- src/app/core/tools/PanTool.ts
+++ src/app/core/tools/PanTool.ts
@@ -12,25 +12,24 @@ import {Tool} from "core/tools/Tool";
 export const PanTool: Tool = (() => {
     let isDragging = false;
     return {
-        shouldActivate(event: Event, {input}: CircuitInfo): boolean {
+        shouldActivate(event: Event, { input }: CircuitInfo): boolean {
             // Activate if the user just pressed the "option key"
             //  or if the user began dragging with either 2 fingers
             //                           or the middle mouse button
             //  or if the user pressed one of of the arrow keys while no components are selected
-            return ((event.type === "keydown" && ((event.key === "Alt") ||  
-                                                  (event.key === "ArrowLeft" || event.key === "ArrowRight" || 
+            return ((event.type === "keydown" && ((event.key === "Alt") ||
+                                                  (event.key === "ArrowLeft" || event.key === "ArrowRight" ||
                                                    event.key === "ArrowUp" || event.key === "ArrowDown" ))) ||
                    (event.type === "mousedrag" && (event.button === MIDDLE_MOUSE_BUTTON ||
                                                    input.getTouchCount() === 2)));
         },
-        shouldDeactivate(event: Event, {input}: CircuitInfo): boolean {
-            //  Deactivate if alt key is released
-            //  or if no dragging happened and "Alt" was released
+        shouldDeactivate(event: Event, { input }: CircuitInfo): boolean {
+            //  Deactivate user stopped dragging
+            //   and the alt key isn't currently pressed
             //  or if one of the arrow keys were released
-            return  (!isDragging && !input.isAltKeyDown()) ||
-                    (event.type === "keyup" && ((!isDragging && event.key === "Alt" || 
-                                               (event.key === "ArrowLeft" || event.key === "ArrowRight" || 
-                                                event.key === "ArrowUp" || event.key === "ArrowDown"))))
+            return (!isDragging && !input.isAltKeyDown()) ||
+                   (event.type === "keyup" && (event.key === "ArrowLeft" || event.key === "ArrowRight" ||
+                                               event.key === "ArrowUp"   || event.key === "ArrowDown"));
         },
 
 
@@ -43,24 +42,25 @@ export const PanTool: Tool = (() => {
         },
 
 
-        onEvent(event: Event, {input, camera}: CircuitInfo): boolean {
+        onEvent(event: Event, { input, camera }: CircuitInfo): boolean {
             if (event.type === "mousedrag") {
                 isDragging = true;
-                
+
                 const dPos = input.getDeltaMousePos();
                 camera.translate(dPos.scale(-1 * camera.getZoom()));
-                
+
                 return true;
             }
 
             if (event.type === "mouseup") {
                 isDragging = false;
+                return true;
             }
-            
+
             if (event.type === "keydown") {
                 let dPos = new Vector();
-                
-                // No else if because it introduces bugs when 
+
+                // No else if because it introduces bugs when
                 // multiple arrow keys are pressed
                 if (input.isKeyDown("ArrowLeft"))
                     dPos = dPos.add(-1, 0);
@@ -70,18 +70,18 @@ export const PanTool: Tool = (() => {
                     dPos = dPos.add(0, -1);
                 if (input.isKeyDown("ArrowDown"))
                     dPos = dPos.add(0, 1);
-                
+
                 // Screen gets moved different amounts depending on if the shift key is held
-                const factor = (input.isShiftKeyDown() ? ARROW_PAN_DISTANCE_SMALL : ARROW_PAN_DISTANCE_NORMAL); 
-                
+                const factor = (input.isShiftKeyDown() ? ARROW_PAN_DISTANCE_SMALL : ARROW_PAN_DISTANCE_NORMAL);
+
                 camera.translate(dPos.scale(factor * camera.getZoom()));
-                
+
                 return true;
             }
-            
-            // Since it wasn't one of the two event types we want we 
+
+            // Since it wasn't one of the two event types we want we
             // don't need a re-render
             return false;
-        }
+        },
     }
 })();
