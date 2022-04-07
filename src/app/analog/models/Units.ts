f79d4201f72ce895f7f455d4eedcfa285f84e694
Leon Montealegre <leonm99@gmail.com>
2022-04-06 22:37:14 -0400

    Fixed issue with incorrect initial values and resistor display

--- src/app/analog/models/Units.ts
+++ src/app/analog/models/Units.ts
@@ -18,17 +18,17 @@ export const Unit = (sym: string, name:  string) => ({
 type UnitKey = keyof ReturnType<typeof Unit>;
 type UnitNumberInfoProps = Omit<NumberPropInfo, "type"|"unit"|"initial"> & { unit: UnitInfo };
 const UnitNumberInfo = (key: string, props: UnitNumberInfoProps,
-                        initial = 0, initialU?: UnitKey): Record<string, PropInfo> => ({
+                        initial: number, initialU: UnitKey): Record<string, PropInfo> => ({
     [key]: {
         type: "float" as const,
-        initial: (initial ?? 0),
+        initial: initial * props.unit[initialU].val,
         ...props,
     },
     [`${key}_U`]: {
         type: "string[]" as const,
         isActive: () => false, // This is a "hidden" property
         display: "",
-        initial: (initialU ?? ""),
+        initial: initialU,
         options: Object.entries(props.unit).map(([key, val]) => [val.display, key]),
     },
 });
@@ -47,7 +47,7 @@ export const AmperageInfo = (key: string, display: string, initial = 0, initialU
 
 export const ResistanceInfo = (key: string, display: string, initial = 0, initialU: UnitKey = " ") => UnitNumberInfo(
     key,
-    { display, unit: Unit("&Omega;", "Ohm"), min: 0 },
+    { display, unit: Unit("\u03A9", "Ohm"), min: 0 },
     initial, initialU,
 );
 export const CapacitanceInfo = (key: string, display: string, initial = 0, initialU: UnitKey = " ") => UnitNumberInfo(
