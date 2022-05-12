53559dd1184a71ab549439cd1912b3fd41ac3805
Leon Montealegre <leonm99@gmail.com>
2022-05-12 19:49:40 -0400

    Fixed formatting and made the code more concise

--- src/app/digital/actions/addition/BusActionFactory.ts
+++ src/app/digital/actions/addition/BusActionFactory.ts
@@ -1,11 +1,14 @@
-import { Vector } from "Vector";
-import { Transform } from "math/Transform";
+import {V, Vector} from "Vector";
+import {Transform} from "math/Transform";
 
 import {GroupAction} from "core/actions/GroupAction";
 import {ConnectionAction} from "core/actions/addition/ConnectionAction";
 
+import {Component} from "core/models";
+
 import {InputPort} from "digital/models/ports/InputPort";
 import {OutputPort} from "digital/models/ports/OutputPort";
+
 
 export function CreateBusAction(outputPorts: OutputPort[], inputPorts: InputPort[]): GroupAction {
     if (inputPorts.length !== outputPorts.length)
@@ -18,56 +21,45 @@ export function CreateBusAction(outputPorts: OutputPort[], inputPorts: InputPort
     if (!designer)
         throw new Error("CreateBusAction failed: Designer not found");
 
-    let inPosSum = new Vector();
-    let inSinSum = 0;
-    let inCosSum = 0;
-    inputPorts.forEach(((currPort) => {
-        const component = currPort.getParent();
-        inPosSum = inPosSum.add(component.getPos());
-        // Y-axis is flipped so need to convert angle for math
-        const angle = 2 * Math.PI - component.getAngle();
-        inSinSum += Math.sin(angle);
-        inCosSum += Math.cos(angle);
-    }))
+    // Basic idea is to take each input component and output component from each
+    //  port and assemble them into a sort-of single giant input component and output component
+    //  using the average positions/rotations of each input/output and then transform them
+    //  into a space where they are aligned with eachother and connect the ports from top-to-bottom
+    //  in this space.
 
-    const avgInPos = inPosSum.scale(1.0/inputPorts.length);
-    // Convert back to flipped Y-axis
-    const avgInRot = 2 * Math.PI - Math.atan2(inSinSum/inputPorts.length,inCosSum/inputPorts.length)
-    const averageInputTransform = new Transform(avgInPos, new Vector(), avgInRot);
-    const inputTargetPositions = inputPorts.map(o => averageInputTransform.toLocalSpace(o.getWorldTargetPos()));
+    const calcAvgComp = (cs: Component[]) => {
+        const avgPos = cs
+            .map(c => c.getPos())
+            .reduce((sum, cur) => sum.add(cur), V())
+            .scale(1 / cs.length);
 
-    let outPosSum = new Vector();
-    let outSinSum = 0;
-    let outCosSum = 0;
-    outputPorts.forEach(((currPort) => {
-        const component = currPort.getParent();
-        outPosSum = outPosSum.add(component.getPos());
-        // Y-axis is flipped so need to convert angle for math
-        const angle = 2 * Math.PI - component.getAngle();
-        outSinSum += Math.sin(angle);
-        outCosSum += Math.cos(angle);
-    }))
+        // Use cos/sin to store each rotation in calculation of average
+        //  because averaging an angle like -270 and 90 would lead to
+        //  -90 when they are both actually the same angle
+        const avgRot = cs
+            .map(c => c.getAngle())
+            .map(a => V(Math.cos(a), Math.sin(a)))
+            .reduce((sum, cur) => sum.add(cur), V())
+            .scale(1 / cs.length);
 
-    const avgOutPos = outPosSum.scale(1.0 / outputPorts.length);
-    // Convert back to flipped Y-axis
-    const avgOutRot = 2 * Math.PI - Math.atan2(outSinSum/outputPorts.length,outCosSum/outputPorts.length);
-    const averageOutputTransform = new Transform(avgOutPos, new Vector(), avgOutRot);
-    const outputTargetPositions = outputPorts.map(o => averageOutputTransform.toLocalSpace(o.getWorldTargetPos()));
-
-    // Associate the ports with their average component target position
-    let inputMap = new Map<Vector,InputPort>()
-    for(let currPort = 0; currPort < inputPorts.length; currPort++){
-        inputMap.set(inputTargetPositions[currPort],inputPorts[currPort])
+        return new Transform(avgPos, V(), avgRot.angle());
     }
-    let outputMap = new Map<Vector,OutputPort>()
-    for(let currPort = 0; currPort < outputPorts.length; currPort++){
-        outputMap.set(outputTargetPositions[currPort],outputPorts[currPort])
-
-    }
-
     const sortByPos = (a: Vector, b: Vector) => {
         return a.y - b.y; // Sort by y-pos from Top to Bottom
     }
+
+    // Get average components
+    const avgOutputTransform = calcAvgComp(outputPorts.map(p => p.getParent()));
+    const avgInputTransform  = calcAvgComp( inputPorts.map(p => p.getParent()));
+
+    // Get relative positions for each port in average-component-space
+    const outputTargetPositions = outputPorts.map(o => avgOutputTransform.toLocalSpace(o.getWorldTargetPos()));
+    const inputTargetPositions  =  inputPorts.map(o =>  avgInputTransform.toLocalSpace(o.getWorldTargetPos()));
+
+    // Associate the ports with their average component target position
+    // Keep track of port associated with each position
+    const inputMap  = new Map( inputTargetPositions.map((p, i) => [p,  inputPorts[i]]));
+    const outputMap = new Map(outputTargetPositions.map((p, i) => [p, outputPorts[i]]));
 
     inputTargetPositions.sort(sortByPos);
     outputTargetPositions.sort(sortByPos);
