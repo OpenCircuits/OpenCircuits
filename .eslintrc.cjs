2cad9ac7a9b9dc1ee6114b58f94940302a56d3e2
Leon Montealegre <leonm99@gmail.com>
2022-07-04 02:30:23 -0400

    Fixed isses with linting config and ESM module changes

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+const deepMerge = require("deepmerge");
+
+module.exports = deepMerge.all([{
+        "root": true,
+        "env": {
+            "browser": true,
+            "es6": true,
+        },
+        "ignorePatterns": ["*.js", "scripts/**", "*.d.ts"],
+        "parser": "@typescript-eslint/parser",
+        "parserOptions": {
+            "tsconfigRootDir": __dirname,
+            "project": "./tsconfig.base.json",
+            "ecmaFeatures": {
+                "jsx": true,
+            },
+        },
+    },
+    require("./linting/.core.cjs"),
+    require("./linting/.ts.cjs"),
+    require("./linting/.imports.cjs"),
+    require("./linting/.jsdoc.cjs"),
+    require("./linting/.react.cjs"),
+    require("./linting/.jestRules.cjs"),
+    require("./linting/.custom.cjs"),
+    require("./linting/.unicorn.cjs"),
+    // require("./linting/.sonarjs.js"),
+    // require("./linting/.jsxa11y.js"),
+]);
