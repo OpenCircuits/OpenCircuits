90ed7ce920cd0ce54fc82d2f6e9fdc603a7e9ad5
Leon Montealegre <leonm99@gmail.com>
2022-07-06 23:28:19 -0400

    Moved property module to shared and added it to digital popup

--- src/site/pages/digital/src/containers/App/index.tsx
+++ src/site/pages/digital/src/containers/App/index.tsx
@@ -17,6 +17,7 @@ import {SelectionPopup} from "shared/containers/SelectionPopup";
 import {SideNav}        from "shared/containers/SideNav";
 
 import {PositionModule} from "shared/containers/SelectionPopup/modules/PositionModule";
+import {PropertyModule} from "shared/containers/SelectionPopup/modules/PropertyModule";
 
 import {DigitalPaste} from "site/digital/utils/DigitalPaste";
 
@@ -97,6 +98,7 @@ export const App = ({ info, helpers, canvas }: Props) => {
                     <SelectionPopup info={info}
                                     docsUrlConfig={docsConfig}>
                         <PositionModule info={info} />
+                        <PropertyModule info={info} />
                         <InputCountModule info={info} />
                         <ComparatorInputCountModule info={info} />
                         <SelectPortCountModule info={info} />
