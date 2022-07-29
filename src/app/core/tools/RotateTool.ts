1e09fd5bfba05ae90745c0add0bb59f10af7d0ac
ZWilliams752 <zwilliams752@gmail.com>
2022-07-29 14:44:41 -0400

    undo bugfix

--- src/app/core/tools/RotateTool.ts
+++ src/app/core/tools/RotateTool.ts
@@ -102,7 +102,7 @@ export const RotateTool = (() => {
                 initialMidpoints = components.map(o => o.getPos())
             } else {
                 components.forEach((c, i) => c.setRotationAbout(newAngles[i], midpoint));
-                initialMidpoints = components.map(o => o.getPos());
+                initialMidpoints = components.map(o => midpoint);
             }
             prevAngle += dAngle;
 
