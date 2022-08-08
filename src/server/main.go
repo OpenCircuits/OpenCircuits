4cc343374ad445a7e5e2c4e23087174fbe84d6b7
Kevin Mackenzie <dev.kjmack305@gmail.com>
2022-08-07 19:26:41 -0700

    Switched to individual methods for parsing

--- src/server/main.go
+++ src/server/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"flag"
+	"log"
 	"net"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/OpenCircuits/OpenCircuits/site/go/api"
 	"github.com/OpenCircuits/OpenCircuits/site/go/auth"
@@ -21,53 +23,63 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func getPort() string {
+func getPort() int {
 	for port := 8080; port <= 65535; port++ {
 		ln, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 		if err == nil {
 			ln.Close()
-			return strconv.Itoa(port)
+			return port
 		}
 	}
-	return "8080"
+	return 8080
 }
 
-type flagVar struct {
-	uFlag *uint64
-	iFlag *int
-	bFlag *bool
-	sFlag *string
+func getOCEnv(name string) string {
+	return os.Getenv("OC_" + strings.ToUpper(name))
 }
 
-func createConfig(key string, message string) flagVar {
-	var config flagVar
-	val := os.Getenv(key)
-	uintVal, errU := strconv.ParseUint(val, 10, 0)
-	intVal, errI := strconv.Atoi(val)
-	boolVal, errB := strconv.ParseBool(val)
-	if errU == nil {
-		config.uFlag = flag.Uint64(key, uintVal, message)
-	} else if errI == nil {
-		config.iFlag = flag.Int(key, intVal, message)
-	} else if errB == nil {
-		config.bFlag = flag.Bool(key, boolVal, message)
-	} else {
-		config.sFlag = flag.String(key, val, message)
+// Read values from CLI flags, then env vars, then hard-coded defaults
+
+func addFlagUint64(name string, value uint64, usage string) *uint64 {
+	if env := getOCEnv(name); len(env) > 0 {
+		if val, err := strconv.ParseUint(env, 10, 64); err == nil {
+			value = val
+		} else {
+			log.Printf("Failed to parse parameter provided by env var: %s => %s.  Using fallback %d\n", name, usage, value)
+		}
 	}
-	return config
+	return flag.Uint64(name, value, usage)
+}
+
+func addFlagBool(name string, value bool, usage string) *bool {
+	if env := getOCEnv(name); len(env) > 0 {
+		if val, err := strconv.ParseBool(env); err == nil {
+			value = val
+		} else {
+			log.Printf("Failed to parse parameter provided by env var: %s => %s.  Using fallback %t\n", name, usage, value)
+		}
+	}
+	return flag.Bool(name, value, usage)
+}
+
+func addFlagString(name string, value string, usage string) *string {
+	if env := getOCEnv(name); len(env) > 0 {
+		value = env
+	}
+	return flag.String(name, value, usage)
 }
 
 func main() {
 	var err error
 
-	noAuthConfig := createConfig("no_auth", "Enables username-only authentication for testing and development").bFlag
-	googleAuthConfig := createConfig("google_auth", "<path-to-config>; Enables google sign-in API login").sFlag
-	userCsifConfig := createConfig("interface", "The storage interface").sFlag
-	sqlitePathConfig := createConfig("sqlitePath", "The path to the sqlite working directory").sFlag
-	dsEmulatorHost := createConfig("ds_emu_host", "The emulator host address for cloud datastore").sFlag
-	dsProjectId := createConfig("ds_emu_project_id", "The gcp project id for the datastore emulator").sFlag
-	ipAddressConfig := createConfig("ip_address", "IP address of server").sFlag
-	portConfig := createConfig("port", "Port to serve application, use \"auto\" to select the first available port starting at 8080").uFlag
+	noAuthConfig := addFlagBool("no_auth", false, "Enables username-only authentication for testing and development")
+	googleAuthConfig := addFlagString("google_auth", "", "<path-to-config>; Enables google sign-in API login")
+	userCsifConfig := addFlagString("interface", "sqlite", "The storage interface")
+	sqlitePathConfig := addFlagString("sqlitePath", "sql/sqlite", "The path to the sqlite working directory")
+	dsEmulatorHost := addFlagString("ds_emu_host", "", "The emulator host address for cloud datastore")
+	dsProjectId := addFlagString("ds_emu_project_id", "", "The gcp project id for the datastore emulator")
+	ipAddressConfig := addFlagString("ip_address", "0.0.0.0", "IP address of server")
+	portConfig := addFlagUint64("port", 8080, "Port to serve application, use \"0\" to select the first available port starting at 8080")
 	flag.Parse()
 
 	// Register authentication method
@@ -94,6 +106,11 @@ func main() {
 		core.CheckErrorMessage(err, "Failed to load gcp datastore instance:")
 	}
 
+	// Check if portConfig is set to auto, if so find available port
+	if *portConfig == 0 {
+		*portConfig = uint64(getPort())
+	}
+
 	// Route through Gin
 	router := gin.Default()
 	router.Use(gin.Recovery())
@@ -110,11 +127,6 @@ func main() {
 	web.RegisterPages(router, authManager)
 	authManager.RegisterHandlers(router)
 	api.RegisterRoutes(router, authManager, userCsif)
-
-	// Check if portConfig is set to auto, if so find available port
-	if strconv.FormatUint(*portConfig, 10) == "auto" {
-		os.Setenv("port", getPort())
-	}
 
 	router.Run(*ipAddressConfig + ":" + strconv.FormatUint(*portConfig, 10))
 }
