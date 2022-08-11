2219df63d5756ea5ebdff0f8bf83caa1dd9c0384
nadiachoudhury <66031422+nadiachoudhury@users.noreply.github.com>
2022-08-11 11:50:32 -0400

    Add files via upload

---
+++ src/app/tests/digital/utils/tools/handlers/FitToScreenHandler.test.ts
@@ -0,0 +1,49 @@
+import "jest";
+
+import {V} from "Vector";
+
+import {Setup} from "test/helpers/Setup";
+import {GetHelpers} from "test/helpers/Helpers";
+import {ConstantHigh, ConstantLow} from "digital/models/ioobjects";
+import {LED} from "digital/models/ioobjects/outputs/LED";
+
+describe("FitToScreenHandler", () => {
+    const {input, designer, selections, camera} = Setup();
+    const {Place} = GetHelpers(designer);
+
+    afterEach(() => {
+        designer.reset();
+        selections.get().forEach(s => selections.deselect(s));
+    });
+
+    test("Fit to Screen of a Single Object", () => {
+        const [lo] = Place(new ConstantLow());
+
+        expect(designer.getObjects()).toHaveLength(1)
+        expect(selections.amount()).toBe(0);
+
+        input.click(V(0, 0));
+
+        expect(selections.amount()).toBe(1);
+
+        input.pressKey("f")
+            .releaseKey("f");
+
+        expect(selections.amount()).toBe(1);
+        expect(camera.getPos()).toEqual(V(21.5,0));
+
+    });
+
+    test("Fit to Screen with no objects", () => {
+
+        expect(designer.getObjects()).toHaveLength(0)
+        expect(selections.amount()).toBe(0);
+
+        input.pressKey("f")
+            .releaseKey("f");
+
+        expect(selections.amount()).toBe(0);
+        expect(camera.getPos()).toEqual(V(0,0));
+
+    });
+});
