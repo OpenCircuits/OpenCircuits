95d50ca4dc15e6672900acf63ab11f0e5f251a78
Leon Montealegre <leonm99@gmail.com>
2022-08-31 18:11:09 -0400

    Change RotateAction to only apply to single components and strictly set angle

--- src/app/core/actions/transform/RotateAction.ts
+++ src/app/core/actions/transform/RotateAction.ts
@@ -1,85 +1,62 @@
-import {Vector} from "Vector";
-
 import {Action} from "core/actions/Action";
 
 import {Component} from "core/models/Component";
 
 
 /**
- * Implementation of Action interface for rotating component(s).
+ * Implementation of Action interface for rotating components.
  */
 export class RotateAction implements Action {
 
     /**
-     * An Array of the selected component(s).
+     * The selected component.
      */
-    private objects: Component[];
+    private obj: Component;
 
     /**
-     * An Array of the x, y coordinates for each component of the selected component(s) to be rotated around.
+     * The initial angle of the selected component.
      */
-    private midpoints: Vector[];
+    private initialAngle: number;
 
     /**
-     * An Array of the initial angles the selected component(s) are placed at.
+     * The final angle the selected component.
      */
-    private initialAngles: number[];
+    private finalAngle: number;
 
     /**
-     * An Array of the final angles the selected component(s) will be placed at.
-     */
-    private finalAngles: number[];
-
-    /**
-     * Creates a rotation action for a component or a group of components.
+     * Creates a rotation action for a component.
      *
-     * @param objects       Initializes the action with an Array of the selected component(s).
-     * @param midpoints     Inititalizes the action with the x, y coordinates of the points to rotate
-     *                the selected component(s) around.
-     * @param initialAngles Inititalizes the action an Array of the initial angles the selected component(s)
-     *                are placed at.
-     * @param finalAngles   Inititalizes the action an Array of the final angles the selected component(s)
-     *                will be placed at.
+     * @param obj          Initializes the action with an Array of the selected component(s).
+     * @param initialAngle Inititalizes the action an Array of the initial angles the selected component(s)
+     *               are placed at.
+     * @param finalAngle   Inititalizes the action an Array of the final angles the selected component(s)
+     *               will be placed at.
      */
-    public constructor(objects: Component[], midpoints: Vector[], initialAngles: number[], finalAngles: number[]) {
-        this.objects = objects;
-        this.midpoints = midpoints;
-        this.initialAngles = initialAngles;
-        this.finalAngles = finalAngles;
+    public constructor(obj: Component, initialAngle: number, finalAngle: number) {
+        this.obj = obj;
+        this.initialAngle = initialAngle;
+        this.finalAngle = finalAngle;
     }
 
     /**
-     * Rotates a component or group of components.
-     *
-     * @param angles The final angle(s) the component(s) will be set as.
-     */
-    private setAngles(angles: number[]): void {
-        for (let i = 0; i < this.objects.length; i++) {
-            const obj = this.objects[i];
-            obj.setRotationAbout(angles[i], this.midpoints[i]);
-        }
-    }
-
-    /**
-     * Rotates a component or group of components from the initial angle(s)
-     * to the final angle(s).
+     * Rotates a component from the initial angle o the final angle.
      *
      * @returns An Action where the rotation is executed.
      */
     public execute(): Action {
-        this.setAngles(this.finalAngles);
+        this.obj.setAngle(this.finalAngle);
 
         return this;
     }
 
     /**
-     * Reverts the RotateAction object's angles to the original angles it was
-     * set to, putting the components back in their initial orientation.
+     * Reverts the RotateAction object's angle to the original angle,
+     *  putting the component back in its initial orientation.
      *
      * @returns An Action where the rotation is undone.
      */
     public undo(): Action {
-        this.setAngles(this.initialAngles);
+        this.obj.setAngle(this.initialAngle);
 
         return this;
     }
@@ -90,10 +67,8 @@ export class RotateAction implements Action {
 
     public getCustomInfo(): string[] {
         const deg = String.fromCodePoint(176);
-        return [...this.objects].map(
-            (obj, i) =>
-                `${obj.getName()}: rotated from ${Math.round(this.initialAngles[i] * (180 / Math.PI))}${deg}
-                                            to ${Math.round(this.finalAngles[i] * (180 / Math.PI))}${deg}`
-        );
+        const a0 = Math.round(this.initialAngle * 180 / Math.PI);
+        const a1 = Math.round(this.finalAngle   * 180 / Math.PI);
+        return [`${this.obj.getName()}: rotated from ${a0}${deg} to ${a1}${deg}`];
     }
 }
