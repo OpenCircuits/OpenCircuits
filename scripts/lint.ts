73336a66ad151948e0191ff0377cc3b21a1f4811
Trevor Crystal <TGCrystal@users.noreply.github.com>
2022-09-18 00:29:25 -0400

    Add linting for diff only

---
+++ scripts/lint.ts
@@ -0,0 +1,44 @@
+import {ESLint}    from "eslint";
+import {simpleGit} from "simple-git";
+import yargs       from "yargs";
+
+
+// CLI
+(async () => {
+    const { fix, cache, all } = await yargs(process.argv.slice(2))
+        .boolean("fix")
+        .boolean("cache")
+        .boolean("all")
+        .argv;
+
+    const eslint = new ESLint({
+        fix, cache,
+        errorOnUnmatchedPattern: false, // disable because deleted files
+        extensions:              [".ts", ".tsx"],
+    });
+
+    const files: string[] = [];
+    if (!all) {
+        const git = simpleGit();
+        const diff = await git.diff(["--name-only", "master..."]);
+        diff.split("\n").filter((file) => (file.endsWith(".ts") || file.endsWith(".tsx"))).forEach((file) => {
+            if (file.endsWith(".ts") || file.endsWith(".tsx"))
+                files.push(file);
+        });
+        const status = await git.status(["--short"]);
+        status.files.forEach((summary) => {
+            const file = summary.path;
+            if (file.endsWith(".ts") || file.endsWith(".tsx"))
+                files.push(file);
+        });
+    } else {
+        files.push("scripts/", "src/");
+    }
+
+    const results = await eslint.lintFiles(files);
+    if (fix)
+        await ESLint.outputFixes(results);
+    const formatter = await eslint.loadFormatter("stylish");
+    const resultText = formatter.format(results);
+    console.log(resultText);
+})();
