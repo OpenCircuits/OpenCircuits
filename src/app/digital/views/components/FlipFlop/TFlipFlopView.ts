2c9292d4c3816746aceaf48590eed053781ffb20
Demopans <gao.jeff3@gmail.com>
2022-10-17 02:34:06 +0000

    Can display. Currently a AND gate due to reasons Also added AI docs as I come across poor docs Signed-off-by: Demopans <gao.jeff3@gmail.com>
    Signed-off-by: Demopans <gao.jeff3@gmail.com>

--- src/app/digital/views/components/FlipFlop/TFlipFlopView.ts
+++ src/app/digital/views/components/FlipFlop/TFlipFlopView.ts
@@ -1,47 +1,11 @@
-import {DEFAULT_BORDER_COLOR, DEFAULT_BORDER_WIDTH, DEFAULT_CURVE_BORDER_WIDTH, SELECTED_BORDER_COLOR} from "core/utils/Constants";
+import {TFlipFlop} from "core/models/types/digital";
 
-import {V} from "Vector";
-
-import {Style} from "core/utils/rendering/Style";
-
-import {Line} from "core/utils/rendering/shapes/Line";
-
-import {DigitalPortGroup, TFlipFlop} from "core/models/types/digital";
-
-import {RenderInfo}      from "core/views/BaseView";
+import {FlipFlopView}    from "digital/views/components/FlipFlopView";
 import {DigitalViewInfo} from "digital/views/DigitalViewInfo";
-import {FlipFlopView} from "digital/views/components/FlipFlopView";
 
 // flipflops share much of the same model. Need to get in contact with the other flipflop people, or I do all of them.
 export class TFlipFlopView extends FlipFlopView<TFlipFlop, DigitalViewInfo>{
     public constructor(info: DigitalViewInfo, obj: TFlipFlop) {
         super(info, obj);
     }
-
-    protected override renderComponent({ renderer, selections }: RenderInfo): void {
-        const selected = selections.has(this.obj.id);
-
-        const borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
-
-        const style = new Style(undefined, borderCol, DEFAULT_CURVE_BORDER_WIDTH);
-
-        // Get size of model
-        const size = this.transform.get().getSize();
-
-        // Get current number of inputs
-        const inputs = this.circuit.getPortsFor(this.obj)
-            .filter((p) => p.group === DigitalPortGroup.Input).length;
-
-        // Draw line to visually match input ports
-        const l1 = -(inputs-1)/2*(0.5 - DEFAULT_BORDER_WIDTH/2) - DEFAULT_BORDER_WIDTH/2;
-        const l2 =  (inputs-1)/2*(0.5 - DEFAULT_BORDER_WIDTH/2) + DEFAULT_BORDER_WIDTH/2;
-
-        const s = (size.x-DEFAULT_BORDER_WIDTH)/2;
-        const p1 = V(-s, l1);
-        const p2 = V(-s, l2);
-
-        renderer.draw(new Line(p1, p2), style);
-    }
-
-
 }
--- src/app/digital/views/components/FlipFlopView.ts
+++ src/app/digital/views/components/FlipFlopView.ts
@@ -1,4 +1,9 @@
-import {DEFAULT_BORDER_WIDTH} from "core/utils/Constants";
+import {
+    DEFAULT_BORDER_COLOR,
+    DEFAULT_BORDER_WIDTH,
+    DEFAULT_CURVE_BORDER_WIDTH,
+    SELECTED_BORDER_COLOR
+} from "core/utils/Constants";
 
 import {V} from "Vector";
 
@@ -9,8 +14,10 @@ import {AnyComponent} from "core/models/types";
 import {DigitalPortGroup} from "core/models/types/digital";
 
 import {CircuitController} from "core/controllers/CircuitController";
-import {ViewCircuitInfo}   from "core/views/BaseView";
+import {RenderInfo, ViewCircuitInfo} from "core/views/BaseView";
 import {ComponentView}     from "core/views/ComponentView";
+import {Style} from "core/utils/rendering/Style";
+import {Line} from "core/utils/rendering/shapes/Line";
 
 
 export class FlipFlopView<
@@ -31,6 +38,31 @@ export class FlipFlopView<
      */
     public constructor(info: Info, obj: Obj) {
         super(info, obj, V(1, 1), "and.svg"); // file location: src/site/pages/digital/public/img/items. Also temp placeholder as I figure the positioning.
+    }
+
+    protected override renderComponent({ renderer, selections }: RenderInfo): void {
+        const selected = selections.has(this.obj.id);
+
+        const borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
+
+        const style = new Style(undefined, borderCol, DEFAULT_CURVE_BORDER_WIDTH);
+
+        // Get size of model
+        const size = this.transform.get().getSize();
+
+        // Get current number of inputs
+        const inputs = this.circuit.getPortsFor(this.obj)
+            .filter((p) => p.group === DigitalPortGroup.Input).length;
+
+        // Draw line to visually match input ports
+        const l1 = -(inputs-1)/2*(0.5 - DEFAULT_BORDER_WIDTH/2) - DEFAULT_BORDER_WIDTH/2;
+        const l2 =  (inputs-1)/2*(0.5 - DEFAULT_BORDER_WIDTH/2) + DEFAULT_BORDER_WIDTH/2;
+
+        const s = (size.x-DEFAULT_BORDER_WIDTH)/2;
+        const p1 = V(-s, l1);
+        const p2 = V(-s, l2);
+
+        renderer.draw(new Line(p1, p2), style);
     }
 
     protected override getBounds(): Rect {
