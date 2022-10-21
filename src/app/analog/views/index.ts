cd32155b6c322be013e29e0003c2399fcd4ddca7
simonchan002 <schan2234@gmail.com>
2022-10-21 17:46:58 -0400

    changes  to views
    added oscilliscope to views

--- src/app/analog/views/index.ts
+++ src/app/analog/views/index.ts
@@ -30,6 +30,8 @@ export const Views: ViewRecord<AnalogObj, AnalogViewInfo> = {
     "AnalogPort": (c, o) => new AnalogPortView(c, o),
     "AnalogNode": (c, o) => new AnalogNodeView(c, o),
 
+    "Oscilliscope":   (c, o) => new ComponentView(c, o, V(1.2, 0.6), "oscilliscope.svg"),
+
     "Ground":   (c, o) => new ComponentView(c, o, V(1.2, 0.6), "ground.svg"),
     "Resistor": (c, o) => new ComponentView(c, o, V(1.2, 1), "resistor.svg"),
 };
