45046416506f4a5eed6484a193aaebc2f87cd185
Leon Montealegre <leonm99@gmail.com>
2022-10-26 19:46:56 -0400

    Fixed analog with new port group type and also wiring colors for digital and an issue with wiring directions

--- src/app/core/views/portinfo/analog/index.ts
+++ src/app/core/views/portinfo/analog/index.ts
@@ -5,12 +5,15 @@ import {AnalogComponent, DefaultAnalogPort} from "core/models/types/analog";
 import {PortInfoRecord} from "../types";
 
 
+const DefaultAnalogPortInfo = {
+    Default:       DefaultAnalogPort,
+    InitialConfig: 0,
+    AllowChanges:  false,
+} as const;
+
 export const AnalogPortInfo: PortInfoRecord<AnalogComponent> = {
     "AnalogNode": {
-        Default:       DefaultAnalogPort,
-        InitialConfig: 0,
-        AllowChanges:  false,
-
+        ...DefaultAnalogPortInfo,
         PositionConfigs: [{
             "ports": [
                 { origin: V(0, 0), target: V(0, 0), dir: V(-1, 0) },
@@ -19,19 +22,13 @@ export const AnalogPortInfo: PortInfoRecord<AnalogComponent> = {
         }],
     },
     "Ground": {
-        Default:       DefaultAnalogPort,
-        InitialConfig: 0,
-        AllowChanges:  false,
-
+        ...DefaultAnalogPortInfo,
         PositionConfigs: [{
             "ports": [{ origin: V(0, +0.3), target: V(0, +1), dir: V(0, +1) }],
         }],
     },
     "Resistor": {
-        Default:       DefaultAnalogPort,
-        InitialConfig: 0,
-        AllowChanges:  false,
-
+        ...DefaultAnalogPortInfo,
         PositionConfigs: [{
             "ports": [
                 { origin: V(-0.6, 0), target: V(-1.3, 0), dir: V(-1, 0) },
