5002ecbcea75a216af0b778e5e10d844669152cb
InahL <inah0331@gmail.com>
2022-11-04 16:33:12 -0400

    import

--- src/app/digital/views/components/SRLatchView.ts
+++ src/app/digital/views/components/SRLatchView.ts
@@ -1,12 +1,1 @@
-import {serializable} from "serialeazy";
-
-
-
-@serializable("SRLatch")
-export class SRLatch extends Latch {
-    public static readonly SET_PORT;
-    public static readonly;
-
-    public constructor() {
-    }
-}
+import {SRLatch} from "core/models/types/digital";
