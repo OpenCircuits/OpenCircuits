9a712ffc032cd12976d18a9d01ee537b05920b47
InahL <inah0331@gmail.com>
2022-11-04 16:36:25 -0400

    componentview

--- src/app/digital/views/components/SRLatchView.ts
+++ src/app/digital/views/components/SRLatchView.ts
@@ -1,1 +1,7 @@
 import {SRLatch} from "core/models/types/digital";
+import {ComponentView} from "core/views/ComponentView";
+
+export class ANDGateView extends ComponentView<SRLatch> {
+    public constructor( obj: SRLatch) {
+        super(obj);
+    }
