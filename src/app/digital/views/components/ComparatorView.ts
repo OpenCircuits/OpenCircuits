b571c94d756b83cda3663ea3d8f7d5503c1ae2d9
ragar3 <ria.agarwal@gmail.com>
2022-11-14 23:54:06 -0500

    Update ComparatorView.ts

--- src/app/digital/views/components/ComparatorView.ts
+++ src/app/digital/views/components/ComparatorView.ts
@@ -2,6 +2,12 @@ import {DEFAULT_BORDER_COLOR, DEFAULT_BORDER_WIDTH, DEFAULT_CURVE_BORDER_WIDTH, 
 
 import {V} from "Vector";
 
+import {Rect} from "math/Rect";
+
+import {Style} from "core/utils/rendering/Style";
+
+import {Line} from "core/utils/rendering/shapes/Line";
+
 import {Comparator} from "core/models/types/digital";
 
 import {RenderInfo}    from "core/views/BaseView";
@@ -15,7 +21,14 @@ export class ComparatorView extends ComponentView<Comparator, DigitalViewInfo> {
         super(info, obj, V(1, 1));
     }
 
-    protected override renderComponent(_: RenderInfo): void {
-        //
+    protected override renderComponent({ renderer, selections }: RenderInfo): void {
+        const selected = selections.has(this.obj.id);
+
+        const borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
+
+        const style = new Style(undefined, borderCol, DEFAULT_CURVE_BORDER_WIDTH);
+
+        const t = this.transform.get();
+        renderer.draw(new Rect(t.getPos(), t.getSize()), style);
     }
 }
