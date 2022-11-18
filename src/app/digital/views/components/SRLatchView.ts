414788b327fb586686c11651b04e94f0ad455122
InahL <inah0331@gmail.com>
2022-11-18 16:39:35 -0500

    rendercomponent and getbounds

--- src/app/digital/views/components/SRLatchView.ts
+++ src/app/digital/views/components/SRLatchView.ts
@@ -7,12 +7,41 @@ import {DigitalViewInfo} from "../DigitalViewInfo";
 
 export class SRLatchView extends ComponentView<SRLatch, DigitalViewInfo> {
     public constructor(info: DigitalViewInfo, obj: SRLatch) {
-        super(info, obj);
+        super(info, obj, V(1, 1));
     }
 
-    const drawBox = function(renderer: , transform: , selected: boolean, fillcol = "#ffffff"): void {
+    
+    protected override renderComponent({ renderer, selections }: RenderInfo): void {
+        const selected = selections.has(this.obj.id);
+
         const borderCol = (selected ? SELECTED_BORDER_COLOR : DEFAULT_BORDER_COLOR);
-        const style = new Style(borderCol, DEFAULT_BORDER_WIDTH);
-        renderer.draw(new Shape(V(), transform.getSize()), style);
+
+        const style = new Style(undefined, borderCol);
+
+        // Get size of model
+        const size = this.transform.get().getSize();
+
+        // Get current number of inputs
+        const inputs = this.circuit.getPortsFor(this.obj)
+            .filter((p) => p.group === "inputs").length;
+
+        // Draw line to visually match input ports
+        const l1 = -(inputs-1)/2*(0.5 - DEFAULT_BORDER_WIDTH/2) - DEFAULT_BORDER_WIDTH/2;
+        const l2 =  (inputs-1)/2*(0.5 - DEFAULT_BORDER_WIDTH/2) + DEFAULT_BORDER_WIDTH/2;
+
+        const s = (size.x-DEFAULT_BORDER_WIDTH)/2;
+        const p1 = V(-s, l1);
+        const p2 = V(-s, l2);
+
+        renderer.draw();
+    }
+
+    public override getBounds(): Rect {
+        // Get current number of inputs
+        const inputs = this.circuit.getPortsFor(this.obj)
+            .filter((p) => p.group === "inputs").length;
+        return super.getBounds().expand(V(0, ((inputs-1)/2*(0.5 - DEFAULT_BORDER_WIDTH/2) + DEFAULT_BORDER_WIDTH/2)));
     }
 }
+
+
