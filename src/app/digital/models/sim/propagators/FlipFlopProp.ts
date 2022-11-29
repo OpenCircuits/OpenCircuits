5504a7d6fe482ff8eb556cdef223800e4d7fae2b
Tevetron <friedrich.jonathan4@gmail.com>
2022-11-29 16:55:55 -0500

    Update props

--- src/app/digital/models/sim/propagators/FlipFlopProp.ts
+++ src/app/digital/models/sim/propagators/FlipFlopProp.ts
@@ -4,13 +4,19 @@ import {Signal} from "digital/models/sim/Signal";
 
 import {Propagator} from "../Propagators";
 
+function up(clock: Signal, lastClock: Signal) {
+    return (clock && !lastClock) as boolean;
+}
+
 // ToDo: implement an 'intial' state to have 'Q' port start as active
 // ToDo2: possibly implement the use of clock and lastclock if needed
 export const DFF: Propagator<DigitalComponent> = ({ signals, state }) => {
     const input = signals["inputs"];
     const sel = signals["selects"];
 
-    // Set state variable
+    const lastClock = state[1];
+    const clock = input[1];
+
     state = (() => {
         // If PRE or CLR are set, then don't care about data or clock since asynchronous
         // Return same state as before
@@ -69,10 +75,38 @@ export const JKFF: Propagator<DigitalComponent> = ({ signals,state }) => {
     console.log(signals) // keep here for now
     const input = signals["inputs"];
     const sel = signals["selects"];
-    // split input into specific gates
-    // run boolean logic
 
-    return [signals]
+    const lastClock = state[1];
+    const clock = input[1];
+
+    state = (() => {
+        // If PRE or CLR are set, then don't care about data or clock since asynchronous
+        if (sel[0] && sel[1]) {
+            // undefined, just keep same state
+            return [state[0], clock];
+        } else if (sel[0]) {
+            return [Signal.On, clock];
+        } else if (sel[1]) {
+            return [Signal.Off, clock];
+        }
+
+        if(up(clock, lastClock)) 
+        {
+            const set   = input[0];
+            const reset = input[2];
+
+            if (set && reset) {
+                return [state[0], clock];
+            } else if (set) {
+                return [Signal.On, clock];
+            } else if (reset) {
+                return [Signal.Off, clock];
+            }
+        }
+        return state;
+    })();
+
+    return [{ "outputs": state }, state]
 }
 export const SRFF: Propagator<DigitalComponent> = ({ signals,state }) => {
     console.log(signals) // keep here for now
