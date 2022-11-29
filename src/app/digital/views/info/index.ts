8da28d973cefb8fc37d9fcff3de193b795cdd66a
InahL <inah0331@gmail.com>
2022-11-29 16:13:03 -0500

    solving errors

--- src/app/digital/views/info/index.ts
+++ src/app/digital/views/info/index.ts
@@ -13,4 +13,5 @@ export const DigitalPropInfo: PropInfoRecord<DigitalObj> = {
     "Switch":  DefaultComponentPropInfo,
     "LED":     DefaultComponentPropInfo,
     "ANDGate": DefaultComponentPropInfo,
+    "SRLatch": DefaultComponentPropInfo,
 } as const;
