46fa73fea315390a7f525057a5a08f40643b4549
Tevetron <friedrich.jonathan4@gmail.com>
2022-12-01 11:37:52 -0500

    comments about functionality

--- src/app/digital/models/sim/propagators/FlipFlopProp.ts
+++ src/app/digital/models/sim/propagators/FlipFlopProp.ts
@@ -47,8 +47,10 @@ export const TFF: Propagator<DigitalComponent> = ({ signals, state = [Signal.Off
     state[1] = input[1];
 
     if(Signal.isOff(sel[0]) && Signal.isOff(sel[1])) {
-        if(up(state[1],lastClock) && Signal.isOn(input[0]))
+        if(up(state[1],lastClock) && Signal.isOn(input[0])) {
+            //if we update we will flip the current state
             state[0] = Signal.isOn(state[0]) ? Signal.Off : Signal.On;
+        }
     } else {
         if (Signal.isOn(sel[0]) && Signal.isOff(sel[1]))
             state[0] = Signal.On;
@@ -69,10 +71,11 @@ export const JKFF: Propagator<DigitalComponent> = ({ signals, state = [Signal.Of
 
     if(Signal.isOff(sel[0]) && Signal.isOff(sel[1])) {
         if(up(state[1],lastClock)) {
+            //if both inputs are active we flip the signal
             if(Signal.isOn(input[0]) && Signal.isOn(input[2])) {
                 state[0] = Signal.isOn(state[0]) ? Signal.Off : Signal.On;
-
             }
+            //if only one port is active we set it to that one
             else if (Signal.isOn(input[0]) && Signal.isOff(input[2]))
                 state[0] = Signal.On;
             else if (Signal.isOff(input[0]) && Signal.isOn(input[2]))
@@ -98,6 +101,7 @@ export const SRFF: Propagator<DigitalComponent> = ({ signals, state = [Signal.Of
 
     if(Signal.isOff(sel[0]) && Signal.isOff(sel[1])) {
         if(up(state[1],lastClock)) {
+            //if only one input is active we set it to that one
             if(Signal.isOn(input[0]) && Signal.isOff(input[2]))
                 state[0] = Signal.On;
             else if (Signal.isOff(input[0]) && Signal.isOn(input[2]))
