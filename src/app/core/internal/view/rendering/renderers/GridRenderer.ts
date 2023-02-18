7cf25bee53ea1ae0d184b72f8a2834f2aecf77bf
Leon Montealegre <leonm99@gmail.com>
2023-02-18 02:43:34 -0500

    Fixed grid renderer

--- src/app/core/internal/view/rendering/renderers/GridRenderer.ts
+++ src/app/core/internal/view/rendering/renderers/GridRenderer.ts
@@ -1,23 +1,23 @@
-import {V} from "Vector";
+import {V, Vector} from "Vector";
 import {RenderState} from "../RenderState";
 
 
 export function RenderGrid({ camera, renderer, options }: RenderState) {
     const step = options.gridSize;
 
-    const size = renderer.size.scale(camera.zoom);
+    const size = renderer.size.scale(camera.zoom/2);
 
-    const cpx = size.scale(0.5).x;
-    const cpy = size.scale(0.5).y;
+    const cpx = Math.ceil(size.x)*step;
+    const cpy = Math.ceil(size.y)*step;
 
     // Batch-render the lines = uglier code + way better performance
     renderer.save();
     renderer.toWorldSpace();
     renderer.setStyle(options.gridStyle);
     renderer.beginPath();
-    for (let x = -cpx; x <= cpx + step; x += step)
+    for (let x = -cpx; x <= cpx; x += step)
         renderer.pathLine(V(x, -cpy), V(x, cpy));
-    for (let y = -cpy; y <= cpy + step; y += step)
+    for (let y = -cpy; y <= cpy; y += step)
         renderer.pathLine(V(-cpx, y), V(cpx, y));
     renderer.closePath();
     renderer.stroke();
