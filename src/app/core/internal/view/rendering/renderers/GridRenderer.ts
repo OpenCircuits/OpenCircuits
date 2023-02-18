3bfb25b146263fe5831490826449443b38ca3943
Leon Montealegre <leonm99@gmail.com>
2023-02-18 03:29:20 -0500

    Fixed grid renderer fully

--- src/app/core/internal/view/rendering/renderers/GridRenderer.ts
+++ src/app/core/internal/view/rendering/renderers/GridRenderer.ts
@@ -1,24 +1,30 @@
-import {V} from "Vector";
+import {V, Vector} from "Vector";
 import {RenderState} from "../RenderState";
 
 
 export function RenderGrid({ camera, renderer, options }: RenderState) {
     const step = options.gridSize;
 
-    const size = renderer.size.scale(camera.zoom/2);
-
-    const cpx = Math.ceil(size.x)*step;
-    const cpy = Math.ceil(size.y)*step;
+    const size = Vector.Ceil(renderer.size.scale(camera.zoom * step / 2)).add(1);
+    const offset = Vector.Floor(camera.pos.scale(1 / step)).scale(step);
 
     // Batch-render the lines = uglier code + way better performance
     renderer.save();
     renderer.toWorldSpace();
     renderer.setStyle(options.gridStyle);
     renderer.beginPath();
-    for (let x = -cpx; x <= cpx; x += step)
-        renderer.pathLine(V(x, -cpy), V(x, cpy));
-    for (let y = -cpy; y <= cpy; y += step)
-        renderer.pathLine(V(-cpx, y), V(cpx, y));
+    for (let x = -size.x; x <= size.x; x += step) {
+        renderer.pathLine(
+            V(x, -size.y).add(offset),
+            V(x,  size.y).add(offset)
+        );
+    }
+    for (let y = -size.y; y <= size.y; y += step) {
+        renderer.pathLine(
+            V(-size.x, y).add(offset),
+            V( size.x, y).add(offset)
+        );
+    }
     renderer.closePath();
     renderer.stroke();
     renderer.restore();
