e180f79d4f109de043c92d84d60209b7f3a10d87
rren8957 <ruonren@gmail.com>
2023-03-01 18:23:23 -0500

    Redid firstAvailable() and started on test cases
    Misunderstood what an existing method was returning and had to redo parts of it

--- src/app/digital/public/api/impl/DigitalComponent.ts
+++ src/app/digital/public/api/impl/DigitalComponent.ts
@@ -20,23 +20,38 @@ export class DigitalComponentImpl extends ComponentImpl<
     }
 
     public override firstAvailable(portGroup: string): DigitalPort | undefined {
-        let Ports;
+        const Ports = this.internal.getPortsForComponent(this.id);
 
         // Get all ports of the specified port group
         if (portGroup === "input") {
-            Ports = this.info.inputPortGroups;
-        } else {
-            Ports = this.info.outputPortGroups;
-            // output ports are always available
-            return new DigitalPortImpl(this.circuit, Ports[0]);
-        }
-            
-        // Find first open input port
-        for (var portNum of Ports) {
-            if (new DigitalPortImpl(this.circuit, portNum).isAvailable())
-                return new DigitalPortImpl(this.circuit, portNum);   
+            const portTypes = this.info.inputPortGroups;
+            if (!portTypes.includes('input')) {
+                return undefined;
+            } else {
+                for (var p in Ports) {
+                    const portObject = this.internal.getPortByID(p);
+                    const portWire = this.internal.getWiresForPort(p); // no wires = available
+                    if (portObject !== undefined && portObject.group === 'input' && portWire === undefined) 
+                        return new DigitalPortImpl(this.circuit, p);   
+                }
+            }
         }
 
+        if (portGroup === "output") {
+            const portTypes = this.info.outputPortGroups;
+            if (!portTypes.includes('output')) {
+                return undefined;
+            } else {
+                for (var p in Ports) {
+                    const portObject = this.internal.getPortByID(p);
+                    // output ports are always available
+                    if (portObject !== undefined && portObject.group === 'input') 
+                        return new DigitalPortImpl(this.circuit, p);   
+                }
+            }
+            
+        }
+        
         return undefined;
     }
 }
--- src/app/tests/FirstAvailable.test.ts
+++ src/app/tests/FirstAvailable.test.ts
@@ -4,28 +4,30 @@ import {CreateCircuit} from "digital/public";
 
 import "./Extensions";
 
-
 describe("FirstAvailable", () => {
     test("All ports available", () => {
         const circuit = CreateCircuit();
 
-        const c = circuit.placeComponentAt(V(0, 0), "ANDGate");
-
-        expect(circuit.getObjs()).toHaveLength(4); // 2 inputs, 1 output, 1 component
-        expect(c.pos).toEqual(V(0, 0));
+        const c = circuit.placeComponentAt(V(0, 0), "Multiplexer");
+        
+        // let v  = c.ports["output"][0].id
+        // Case: 'output' port group
+        expect(c.firstAvailable('output')).toEqual(undefined);
+        // Case: 'input' port group
+        expect(c.firstAvailable('input')).toEqual(undefined);
     });
-    test("Only selected ports are available", () => {
-        const circuit = CreateCircuit();
+    test("Only some/no ports are available", () => {
+        // const circuit = CreateCircuit();
 
-        const s1 = circuit.placeComponentAt(V(-5,  5), "Switch");
-        const s2 = circuit.placeComponentAt(V(-5, -5), "Switch");
-        const c1 = circuit.placeComponentAt(V(0,   0), "ANDGate");
-        const l1 = circuit.placeComponentAt(V(5,   0), "LED");
+        // const s1 = circuit.placeComponentAt(V(-5,  5), "Switch");
+        // const c1 = circuit.placeComponentAt(V(0,   0), "ANDGate");
 
-        expect(circuit.getObjs()).toHaveLength(2 + 2 + 4 + 2);
-        expect(s1.pos).toEqual(V(-5,  5));
-        expect(s2.pos).toEqual(V(-5, -5));
-        expect(c1.pos).toEqual(V(0,   0));
-        expect(l1.pos).toEqual(V(5,   0));
+        // circuit.connectWire(s1.ports["output"][0], c1.ports["input"][0])
+
+        // // There should be no output ports for s1
+        // expect(s1.firstAvailable('output')).toEqual(undefined);
+        // // There should still be 1 input and output port available for c1
+        // expect(c1.firstAvailable('output')).toEqual(new DigitalPortImpl(circuit., c1.ports["input"][1].id));
+        // expect(c1.firstAvailable('output')).toEqual(new DigitalPortImpl(circuit., c1.ports["output"][0].id));
     });
 });
