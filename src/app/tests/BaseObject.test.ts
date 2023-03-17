483af67234dca2e00eb80045ff63183646df12cd
Tevetron <friedrich.jonathan4@gmail.com>
2023-03-17 17:13:26 -0400

    BaseObject api testing

--- src/app/tests/BaseObject.test.ts
+++ src/app/tests/BaseObject.test.ts
@@ -20,6 +20,10 @@ describe("Acessors", () => {
         const circuit = CreateCircuit();
 
         const a = circuit.placeComponentAt(V(0, 0), "ANDGate");
-        
+        expect(a.getProp('x')).toEqual(0);
+        expect(a.getProp('y')).toEqual(0);
+
+        expect(a.getProps()['x']).toEqual(0);
+        expect(a.getProps()['y']).toEqual(0);
     });
 });
