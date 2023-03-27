e698b475114c9567ded0fcc99bda8cde74a63754
Trevor Crystal <TGCrystal@users.noreply.github.com>
2023-03-27 07:57:49 -0400

    vscode issues

--- src/site/pages/digital/src/utils/ExpressionParser/TreeToCircuit.ts
+++ src/site/pages/digital/src/utils/ExpressionParser/TreeToCircuit.ts
@@ -1,17 +1,17 @@
-import {Create} from "serialeazy";
+// import {Create} from "serialeazy";
 
 import {InputTree, InputTreeBinOpType, InputTreeOpType} from "./Constants/DataStructures";
 
-import {IOObject} from "core/models";
+// import {IOObject} from "core/models";
 
-import {LazyConnect} from "digital/utils/ComponentUtils";
+// import {LazyConnect} from "digital/utils/ComponentUtils";
 
-import {DigitalComponent} from "digital/models";
+// import {DigitalComponent} from "digital/models";
 
-import {Gate} from "digital/models/ioobjects/gates/Gate";
+// import {Gate} from "digital/models/ioobjects/gates/Gate";
 import {CreateCircuit, DigitalCircuit} from "digital/public";
 import {V} from "Vector";
-import {DigitalCircuitImpl} from "digital/public/api/impl/DigitalCircuit";
+// import {DigitalCircuitImpl} from "digital/public/api/impl/DigitalCircuit";
 
 
 /**
@@ -47,39 +47,39 @@ export const NegatedTypeToGate = {
  * @throws When one of the leaf nodes of the InputTree references an input that is not inputs.
  * @see TreeToCircuit
  */
-function treeToCircuitCore(node: InputTree, inputs: Map<string, DigitalComponent>, circuit: DigitalCircuit):
-    DigitalCircuit {
-    if (node.kind === "leaf") { // Rearranges array so thge relevant input is at the end
-        if (!inputs.has(node.ident))
-            throw new Error("Input Not Found: \"" + node.ident + "\"");
-        const index = circuit.indexOf(inputs.get(node.ident)!);
-        circuit.splice(index, 1);
-        circuit.push(inputs.get(node.ident)!);
-        return circuit;
-    }
+// function treeToCircuitCore(node: InputTree, inputs: Map<string, DigitalComponent>, circuit: DigitalCircuit):
+//     DigitalCircuit {
+//     if (node.kind === "leaf") { // Rearranges array so thge relevant input is at the end
+//         if (!inputs.has(node.ident))
+//             throw new Error("Input Not Found: \"" + node.ident + "\"");
+//         const index = circuit.indexOf(inputs.get(node.ident)!);
+//         circuit.splice(index, 1);
+//         circuit.push(inputs.get(node.ident)!);
+//         return circuit;
+//     }
 
-    const ret = circuit;
-    const newGate = ((node.kind === "binop" && node.isNot)
-                                 ? NegatedTypeToGate[node.type]
-                                 : TypeToGate[node.type]);
-    circuit.placeComponentAt(V(0, 0), newGate)
-    if (node.kind === "unop") {
-        const prevNode = treeToCircuitCore(node.child, inputs, ret).at(-1) as DigitalComponent;
-        const wire = LazyConnect(prevNode, newGate);
-        ret.push(wire);
-    } else if (node.kind === "binop") {
-        newGate.setInputPortCount(node.children.length);
-        node.children.forEach((child) => {
-            if (!child)
-                throw new Error("treeToCircuitCore failed: child was undefined");
-            const prevNode = treeToCircuitCore(child, inputs, ret).at(-1) as DigitalComponent;
-            const wire = LazyConnect(prevNode, newGate);
-            ret.push(wire);
-        });
-    }
-    ret.push(newGate);
-    return ret;
-}
+//     const ret = circuit;
+//     const newGate = ((node.kind === "binop" && node.isNot)
+//                                  ? NegatedTypeToGate[node.type]
+//                                  : TypeToGate[node.type]);
+//     circuit.placeComponentAt(V(0, 0), newGate)
+//     if (node.kind === "unop") {
+//         const prevNode = treeToCircuitCore(node.child, inputs, ret).at(-1) as DigitalComponent;
+//         const wire = LazyConnect(prevNode, newGate);
+//         ret.push(wire);
+//     } else if (node.kind === "binop") {
+//         newGate.setInputPortCount(node.children.length);
+//         node.children.forEach((child) => {
+//             if (!child)
+//                 throw new Error("treeToCircuitCore failed: child was undefined");
+//             const prevNode = treeToCircuitCore(child, inputs, ret).at(-1) as DigitalComponent;
+//             const wire = LazyConnect(prevNode, newGate);
+//             ret.push(wire);
+//         });
+//     }
+//     ret.push(newGate);
+//     return ret;
+// }
 
 /**
  * Converts a given InputTree to an array of connected components (and the wires used to connect them).
@@ -90,15 +90,16 @@ function treeToCircuitCore(node: InputTree, inputs: Map<string, DigitalComponent
  * @returns      The components and wires converted from the tree.
  * @throws When one of the leaf nodes of the InputTree references an input that is not inputs.
  */
-export function TreeToCircuit(tree: InputTree | undefined, inputs: Map<string, DigitalComponent>,
-                              output: DigitalComponent): IOObject[] {
+export function TreeToCircuit(tree: InputTree | undefined, inputs: Map<string, string>,){
+                            //   output: string): IOObject[] {
     if (!tree)
         return [];
 
-    let ret = CreateCircuit();
+    const circuit = CreateCircuit();
+    const c = circuit.placeComponentAt(V(0, 0), "Multiplexer");
 
-    ret = treeToCircuitCore(tree, inputs, ret);
-    const wire = LazyConnect(ret.at(-1) as DigitalComponent, output);
-    ret.push(wire, output);
-    return ret;
+    // ret = treeToCircuitCore(tree, inputs, ret);
+    // const wire = LazyConnect(ret.at(-1) as DigitalComponent, output);
+    // ret.push(wire, output);
+    // return ret;
 }
