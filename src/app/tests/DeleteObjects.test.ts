ae54719084d6c8302d6caca4722eee4c9f7288c6
Tevetron <friedrich.jonathan4@gmail.com>
2023-04-07 16:34:07 -0400

    Update DeleteObjects.test.ts

--- src/app/tests/DeleteObjects.test.ts
+++ src/app/tests/DeleteObjects.test.ts
@@ -30,6 +30,24 @@ describe("DeleteObjects", () => {
         expect(l1.exists()).toEqual(false);
     });
     test("Delete Circuit", () => {
+        const circuit = CreateCircuit();
 
+        const s1 = circuit.placeComponentAt(V(-4,2), "Switch");
+        const s2 = circuit.placeComponentAt(V(-4,-2), "Switch");
+        const g  = circuit.placeComponentAt(V(0,0), "ANDGate");
+        const l  = circuit.placeComponentAt(V(4,2), "LED");
+      
+        const w1 = s1.ports["outputs"][0].connectTo(g.ports["inputs"][0]),
+        w2 = s2.ports["outputs"][0].connectTo(g.ports["inputs"][1]),
+        w3 =  g.ports["outputs"][0].connectTo(l.ports["inputs"][0]); 
+        
+        circuit.deleteObjs([s1, s2, g, l]);
+        expect(s1.exists()).toEqual(false);
+        expect(s2.exists()).toEqual(false);
+        expect(g.exists()).toEqual(false);
+        expect(l.exists()).toEqual(false);
+        //expect(w1?.exists()).toEqual(false);
+        //TODO test for wires existing
+        //TODO ensure deletion working properly for full circuit
     });
 });
