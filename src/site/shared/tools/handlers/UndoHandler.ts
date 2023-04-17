ae546034a76ab7bdf45acf17d7e275274849b402
Leon Montealegre <leonm99@gmail.com>
2023-04-16 20:44:30 -0400

    Re-added undo/redo handlers

---
+++ src/site/shared/tools/handlers/UndoHandler.ts
@@ -0,0 +1,15 @@
+import {ToolHandler, ToolHandlerResponse} from "./ToolHandler";
+
+
+export const UndoHandler: ToolHandler = {
+    onEvent: (ev, { circuit }) => {
+        // Activate when pressing CTRL/CMD + Z
+        if (!(ev.type === "keydown" && ev.key === "z" && ev.input.isModifierKeyDown))
+            return ToolHandlerResponse.PASS;
+
+        circuit.undo();
+
+        // This should be the only handler to execute
+        return ToolHandlerResponse.HALT;
+    },
+}
