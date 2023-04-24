8273fc6537a09a5c002f35fe2e3fedd6ed3b92db
Leon Montealegre <leonm99@gmail.com>
2023-04-24 17:22:05 -0400

    Fixed firstAvailable and associated test

--- src/app/tests/FirstAvailable.test.ts
+++ src/app/tests/FirstAvailable.test.ts
@@ -25,8 +25,9 @@ describe("FirstAvailable", () => {
 
         circuit.connectWire(s1.ports["outputs"][0], c1.ports["inputs"][0]);
 
-        // There should be no available output ports for s1
-        expect(s1.firstAvailable("outputs")?.id).toBeUndefined();
+        // Switch should still have an available output port
+        expect(s1.firstAvailable("outputs")?.id).toEqual(s1.ports["outputs"][0].id);
+
         // There should still be 1 input and output port available for c1
         expect(c1.firstAvailable("inputs")?.id).toEqual(c1.ports["inputs"][1].id);
         expect(c1.firstAvailable("outputs")?.id).toEqual(c1.ports["outputs"][0].id);
