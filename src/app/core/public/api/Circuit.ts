ada1f58b298a93cf1b6e81fecfb82e514cadfa04
Leon Montealegre <leonm99@gmail.com>
2023-05-07 01:34:14 -0400

    Added initial declaration for Circuit.find/findAll

--- src/app/core/public/api/Circuit.ts
+++ src/app/core/public/api/Circuit.ts
@@ -4,6 +4,8 @@ import {Rect} from "math/Rect";
 
 import {GUID} from "core/schema/GUID";
 
+import {CleanupFunc} from "core/utils/types"
+
 import {DebugOptions} from "core/internal/impl/DebugOptions";
 
 import {RenderHelper}  from "core/internal/view/rendering/RenderHelper";
@@ -16,8 +18,30 @@ import {Obj}           from "./Obj";
 import {Port}          from "./Port";
 import {Wire}          from "./Wire";
 import {Selections}    from "./Selections";
-import {CleanupFunc}   from "core/utils/types";
 
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace Circuit {
+    export type ObjQueryTypes = {
+        "Component": Component;
+        "Wire": Wire;
+        "Port": Port;
+        "Component | Wire": Component | Wire;
+        "Component | Port": Component | Port;
+        "Wire | Port": Wire | Port;
+        "Obj": Obj;
+    }
+    export interface BaseObjQuery {
+        with(props: { id: GUID }): this;
+        at(pt: Vector, space?: Vector.Spaces): this;
+    }
+    export interface ObjQuery<K extends keyof ObjQueryTypes> extends BaseObjQuery {
+        readonly result: ObjQueryTypes[K] | undefined;
+    }
+    export interface MultiObjQuery<K extends keyof ObjQueryTypes> extends BaseObjQuery {
+        readonly result: Array<ObjQueryTypes[K]>;
+    }
+}
 
 export type {CircuitMetadata} from "core/schema/CircuitMetadata";
 
@@ -41,18 +65,20 @@ export interface Circuit {
     readonly selections: Selections;
 
     // Queries
-    pickObjAt(pt: Vector, space?: Vector.Spaces): Obj | undefined;
-    pickComponentAt(pt: Vector, space?: Vector.Spaces): Component | undefined;
-    pickWireAt(pt: Vector, space?: Vector.Spaces): Wire | undefined;
-    pickPortAt(pt: Vector, space?: Vector.Spaces): Port | undefined;
-    pickObjRange(bounds: Rect): Obj[];
+    find<K extends keyof Circuit.ObjQueryTypes>(kind: K): Circuit.ObjQuery<K>;
+    findAll<K extends keyof Circuit.ObjQueryTypes>(kind: K): Circuit.MultiObjQuery<K>;
+    // pickObjAt(pt: Vector, space?: Vector.Spaces): Obj | undefined;
+    // pickComponentAt(pt: Vector, space?: Vector.Spaces): Component | undefined;
+    // pickWireAt(pt: Vector, space?: Vector.Spaces): Wire | undefined;
+    // pickPortAt(pt: Vector, space?: Vector.Spaces): Port | undefined;
+    // pickObjRange(bounds: Rect): Obj[];
 
-    getComponent(id: GUID): Component | undefined;
-    getWire(id: GUID): Wire | undefined;
-    getPort(id: GUID): Port | undefined;
-    getObj(id: GUID): Obj | undefined;
-    getObjs(): Obj[];
-    getComponents(): Component[];
+    // getComponent(id: GUID): Component | undefined;
+    // getWire(id: GUID): Wire | undefined;
+    // getPort(id: GUID): Port | undefined;
+    // getObj(id: GUID): Obj | undefined;
+    // getObjs(): Obj[];
+    // getComponents(): Component[];
     getComponentInfo(kind: string): ComponentInfo | undefined;
 
     // Object manipulation
