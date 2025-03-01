548c8bbdaa9537b0f8eb5ba5bd3356008e984fd2
Trevor Crystal <TGCrystal@users.noreply.github.com>
2025-02-28 22:13:43 -0500

    Component, Port, and Selection unit tests

--- src/projects/shared/api/circuit/tests/public/Component.test.ts
+++ src/projects/shared/api/circuit/tests/public/Component.test.ts
@@ -11,66 +11,295 @@ import {CreateTestRootCircuit} from "tests/helpers/CreateTestCircuit";
 
 describe("Component", () => {
     describe("Bounds", () => {
-        // Test ideas:
-        // Bounds for the TestComp are 1x1 I think
+        test("1x1 default", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.bounds.center).toStrictEqual(V(0, 0));
+            expect(c.bounds.size).toStrictEqual(V(1, 1));
+        });
+
+        test("1x1 with rotation", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            c.angle = Math.PI / 2;
+            expect(c.bounds.center).toStrictEqual(V(0, 0));
+            expect(c.bounds.size).toStrictEqual(V(1, 1));
+        });
+
+        test("1x1 default offset start", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(-1, -1));
+            expect(c.bounds.center).toStrictEqual(V(-1, -1));
+            expect(c.bounds.size).toStrictEqual(V(1, 1));
+        });
     });
 
     describe("Name", () => {
-        // Test ideas:
-        // Make sure default is undefined
-        // Setting name and undoing brings it back to undefined
+        test("set and undo/redo", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.name).toBeUndefined();
+            c.name = "Test Component";
+            expect(c.name).toBe("Test Component");
+            circuit.undo();
+            expect(c.name).toBeUndefined();
+            circuit.redo();
+            expect(c.name).toBe("Test Component");
+        });
     });
 
     describe("isSelected", () => {
-        // Test ideas:
-        // setting directly and using select/deselect all work
-        // Undoing should undo it
+        test(".isSelected", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.isSelected).toBeFalsy();
+
+            c.isSelected = true;
+            expect(c.isSelected).toBeTruthy();
+            circuit.undo();
+            expect(c.isSelected).toBeFalsy();
+            circuit.redo();
+            expect(c.isSelected).toBeTruthy();
+
+            c.isSelected = false;
+            expect(c.isSelected).toBeFalsy();
+            circuit.undo();
+            expect(c.isSelected).toBeTruthy();
+            circuit.redo();
+            expect(c.isSelected).toBeFalsy();
+        });
+
+        test(".select/.deselect", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.isSelected).toBeFalsy();
+
+            c.select();
+            expect(c.isSelected).toBeTruthy();
+            circuit.undo();
+            expect(c.isSelected).toBeFalsy();
+            circuit.redo();
+            expect(c.isSelected).toBeTruthy();
+
+            c.deselect();
+            expect(c.isSelected).toBeFalsy();
+            circuit.undo();
+            expect(c.isSelected).toBeTruthy();
+            circuit.redo();
+            expect(c.isSelected).toBeFalsy();
+        });
     });
 
-    describe("zIndex", () => {
-        // Idk yet, not sure we even want to expose this
-    });
+    // TODO: Decide if we want to expose this.
+    // describe("zIndex", () => {
+    //     // Idk yet, not sure we even want to expose this
+    // });
 
     describe("Exists", () => {
-        // Test ideas:
-        // Returns true when added
-        // Returns false when undoing an add
-        // Returns false when deleted
+        test("add/delete and undo/redo", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.exists()).toBeTruthy();
+            circuit.undo();
+            expect(c.exists()).toBeFalsy();
+            circuit.redo();
+            expect(c.exists()).toBeTruthy();
+
+            c.delete();
+            expect(c.exists()).toBeFalsy();
+            circuit.undo();
+            expect(c.exists()).toBeTruthy();
+            circuit.redo();
+            expect(c.exists()).toBeFalsy();
+        });
+        test("circuit.deleteObjs", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+
+            circuit.deleteObjs([c]);
+            expect(c.exists()).toBeFalsy();
+            circuit.undo();
+            expect(c.exists()).toBeTruthy();
+            circuit.redo();
+            expect(c.exists()).toBeFalsy();
+        });
     });
 
     describe("Props", () => {
-        // Test ideas:
-        // For comp should all have name/zIndex/x/y/angle
-        // Setting an invalid prop should error
-        // Getting an invalid prop should return undefined
+        test("getProps", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            c.x = 1;
+            c.y = 2;
+            c.angle = 3;
+            c.name = "Test Component";
+            const props = c.getProps();
+            expect("x" in props).toBeTruthy();
+            expect("y" in props).toBeTruthy();
+            expect("pos" in props).toBeFalsy();
+            expect("angle" in props).toBeTruthy();
+            expect("name" in props).toBeTruthy();
+            expect("isSelected" in props).toBeTruthy();
+            expect("exists" in props).toBeTruthy();
+            expect("zIndex" in props).toBeTruthy();
+        });
+
+        test("setProp", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            c.setProp("name", "Test Component");
+            expect(c.name).toBe("Test Component");
+        });
+
+        test("getProp", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.getProp("name")).toBeUndefined();
+            c.name = "Test Component";
+            expect(c.getProp("name")).toBe("Test Component");
+        });
+
+        test("getProp with invalid prop", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.getProp("fakeProp")).toBeUndefined();
+        });
+
+        test("Set invalid prop", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(() => {c.setProp("fakeProp", true)}).toThrow();
+        });
     });
 
     describe("Transform", () => {
-        // Test ideas:
-        // Making sure default x/y/pos/angle are 0s
-        // Setting positions => getting position
-        // Undo/redo settings positions
+        test("Basic movement", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.x).toBe(0);
+            expect(c.y).toBe(0);
+            expect(c.pos.x).toBe(0);
+            expect(c.pos.y).toBe(0);
+            expect(c.angle).toBe(0);
+
+            c.x = 3;
+            expect(c.x).toBe(3);
+            expect(c.y).toBe(0);
+            expect(c.pos.x).toBe(3);
+            expect(c.pos.y).toBe(0);
+            expect(c.angle).toBe(0);
+            c.y = 5;
+            expect(c.x).toBe(3);
+            expect(c.y).toBe(5);
+            expect(c.pos.x).toBe(3);
+            expect(c.pos.y).toBe(5);
+            expect(c.angle).toBe(0);
+
+            circuit.undo();
+            expect(c.x).toBe(3);
+            expect(c.y).toBe(0);
+            expect(c.pos.x).toBe(3);
+            expect(c.pos.y).toBe(0);
+            expect(c.angle).toBe(0);
+            circuit.redo();
+            expect(c.x).toBe(3);
+            expect(c.y).toBe(5);
+            expect(c.pos.x).toBe(3);
+            expect(c.pos.y).toBe(5);
+            expect(c.angle).toBe(0);
+        });
+
+        test("Movement with .pos", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.x).toBe(0);
+            expect(c.y).toBe(0);
+            expect(c.pos.x).toBe(0);
+            expect(c.pos.y).toBe(0);
+            expect(c.angle).toBe(0);
+
+            c.pos = V(9, -17);
+            expect(c.x).toBe(9);
+            expect(c.y).toBe(-17);
+            expect(c.pos.x).toBe(9);
+            expect(c.pos.y).toBe(-17);
+            expect(c.angle).toBe(0);
+
+            // undo/redo
+            circuit.undo();
+            expect(c.x).toBe(0);
+            expect(c.y).toBe(0);
+            expect(c.pos.x).toBe(0);
+            expect(c.pos.y).toBe(0);
+            expect(c.angle).toBe(0);
+            circuit.redo();
+            expect(c.x).toBe(9);
+            expect(c.y).toBe(-17);
+            expect(c.pos.x).toBe(9);
+            expect(c.pos.y).toBe(-17);
+            expect(c.angle).toBe(0);
+        });
     });
 
     describe("isNode", () => {
-        // Idk
+        test("Test component is not a node", () => {
+            const [circuit, _, {PlaceAt}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.isNode()).toBeFalsy();
+        });
     });
 
     describe("Ports", () => {
-        // Test ideas:
-        // Maybe make a new helper to create a circuit with a specific set of port groups
-        // make sure `.ports` has the correct groups and number of ports in each group
-        // make sure allPorts has the correct number of ports
-        // in each of these you can `setNumPorts` as well maybe?
-        // Also test firstAvailable in here
+        test(".ports", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            const {ports} = c
+            expect(Object.keys(ports)).toHaveLength(1);
+            expect("" in ports).toBeTruthy();
+            expect(ports[""]).toHaveLength(1);
+            c.setNumPorts("", 3);
+            expect(ports[""]).toHaveLength(3);
+        });
+
+        test(".allPorts", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c] = PlaceAt(V(0, 0));
+            expect(c.allPorts).toHaveLength(1);
+            c.setNumPorts("", 6);
+            expect(c.allPorts).toHaveLength(6);
+        });
+
+        test("firstAvailable", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c1, c2] = PlaceAt(V(0, 0), V(1, 1));
+            expect(c1.firstAvailable("")).toBeDefined();
+            c1.setNumPorts("", 2);
+            c1.firstAvailable("")!.connectTo(c2.firstAvailable("")!)
+            expect(c1.firstAvailable("")).toBeDefined();
+            expect(c2.firstAvailable("")).toBeUndefined();
+            expect(c1.ports[""][0].connections).toHaveLength(1);
+            expect(c1.ports[""][1].connections).toHaveLength(0);
+        });
+
+        test("Use setNumPorts to remove connection", () => {
+            const [circuit, _, {PlaceAt, Connect, GetPort}] = CreateTestRootCircuit();
+            const [c1, c2, c3] = PlaceAt(V(0, 0), V(1, 1), V(2, 2));
+            expect(c1.firstAvailable("")).toBeDefined();
+            c1.setNumPorts("", 2);
+            c1.ports[""][0].connectTo(c2.ports[""][0])
+            c1.ports[""][1].connectTo(c3.ports[""][0])
+            c1.setNumPorts("", 1);
+            expect(c1.ports[""]).toHaveLength(1);
+            expect(c1.ports[""][0].connections).toHaveLength(1);
+            expect(c2.ports[""]).toHaveLength(1);
+            expect(c2.ports[""][0].connections).toHaveLength(1);
+            expect(c3.ports[""]).toHaveLength(1);
+            expect(c3.ports[""][0]).toHaveLength(0);
+        });
+
     });
 
     // TODO: Do we even want this in the API?
     // describe("Connected Components", () => {
     // });
-
-    describe("Delete", () => {
-        // Test ideas:
-        // Deleting components and undoing it
-    });
 });
