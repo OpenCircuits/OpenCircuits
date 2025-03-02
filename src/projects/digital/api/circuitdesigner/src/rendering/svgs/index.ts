2e6f5f62ea65c40f22db32567fc15488ea854807
Trevor Crystal <TGCrystal@users.noreply.github.com>
2025-03-02 11:11:41 -0500

    Uncomment try/catch block

--- src/projects/digital/api/circuitdesigner/src/rendering/svgs/index.ts
+++ src/projects/digital/api/circuitdesigner/src/rendering/svgs/index.ts
@@ -14,16 +14,16 @@ import constLowSVG from "./constLow.svg";
 
 
 function ToSVGDrawing(name: string, svgStr: string): SVGDrawing {
-    // try {
+    try {
         const svgS = window.atob(svgStr.replace("data:image/svg+xml;base64,", ""));
         const svg = new DOMParser().parseFromString(svgS, "text/xml");
         const drawing = CreateDrawingFromSVG(svg);
         if (!drawing)
             throw new Error("Failed to create drawing from SVG!");
         return drawing;
-    // } catch (e) {
-    //     throw new Error(`Failed to turn svg ${name}.svg (${JSON.stringify(svgStr)}) into SVG! ${e}`);
-    // }
+    } catch (e) {
+        throw new Error(`Failed to turn svg ${name}.svg (${JSON.stringify(svgStr)}) into SVG! ${e}`);
+    }
 }
 
 export const SVGs: Map<string, SVGDrawing> = new Map([
