66f09928a5e2f92168ba551e99ffe972ba4f615c
Trevor Crystal <TGCrystal@users.noreply.github.com>
2025-03-27 08:00:58 -0400

    progress

--- src/projects/digital/api/circuit/src/internal/assembly/components/SegmentDisplayAssembler.ts
+++ src/projects/digital/api/circuit/src/internal/assembly/components/SegmentDisplayAssembler.ts
@@ -15,6 +15,11 @@ import {Style} from "shared/api/circuit/internal/assembly/Style";
 const horizontal = [V(-0.27,0.08), V(0.27,0.08), V(0.35,0), V(0.27,-0.08), V(-0.27,-0.08), V(-0.35,0)] as const;
 const vertical = horizontal.map((v) => v.negativeReciprocal());
 const horizontalHalf = [V(-0.095,0.08), V(0.095,0.08), V(0.175,0), V(0.095,-0.08), V(-0.095,-0.08), V(-0.175,0)] as const;
+const topLeft = [V(.095,-0.18), V(-0.045,0.27), V(-0.155,0.27), V(0.015,-0.27), V(.095,-0.27)]
+const topRight = topLeft.map((v) => v.scale(V(-1, 1)));
+const bottomLeft = topLeft.map((v) => v.scale(V(1, -1)));
+const bottomRight = topLeft.map((v) => v.scale(V(-1, -1)));
+const horizontalTopSegment = horizontal.map((v) => v.add(V(0, 1)))
 // bl - -47.5,40 47.5,40 87.5,0 47.5,-40 -47.5,-40 -87.5,0
 // br - 47.5,90 -22.5,-135 -77.5,-135 7.5,135 47.5,135
 // tl - -47.5,-90 22.5,135 77.5,135 -7.5,-135 -47.5,-135
@@ -25,7 +30,9 @@ export class SegmentDisplayAssembler extends ComponentAssembler {
 
     protected info: DigitalComponentConfigurationInfo;
 
-    public constructor(params: AssemblerParams, sim: DigitalSim) {
+    protected readonly kind: "SegmentDisplay" | "BCDDisplay" | "ASCIIDisplay";
+
+    public constructor(params: AssemblerParams, sim: DigitalSim, kind: "SegmentDisplay" | "BCDDisplay" | "ASCIIDisplay") {
         super(params, V(1.4, 2), {
             "inputs": (index, total) => {
                 const y = this.size.y * index / (total + 2);
@@ -50,14 +57,15 @@ export class SegmentDisplayAssembler extends ComponentAssembler {
                 kind: "BaseShape",
 
                 dependencies: new Set([AssemblyReason.TransformChanged]),
-                assemble: (comp) => this.assembleSegments(comp),
+                assemble:     (comp) => this.assembleSegments(comp),
 
                 styleChangesWhenSelected: true,
-                getStyle: (comp) => this.getSegmentStyle(comp),
+                getStyle:                 (comp) => this.getSegmentStyle(comp),
             },
         ]);
         this.sim = sim;
         this.info = this.circuit.getComponentInfo("SegmentDisplay").unwrap() as DigitalComponentConfigurationInfo;
+        this.kind = kind;
     }
 
     private assembleRectangle(comp: Schema.Component) {
@@ -70,23 +78,45 @@ export class SegmentDisplayAssembler extends ComponentAssembler {
     }
 
     private getSegmentStyle(comp: Schema.Component): Style {
-        // If on, #1E5679 regardless of 
+        // If on, #1E5679 regardless of isSelected
+        // These values come from the result of using a tint when these segments were SVGs
         const color = this.isSelected(comp.id) ? "#0E801F" : "#808080";
         return {
             stroke: { color, size: 0.01, lineCap: "round", lineJoin: "round" },
         }
     }
 
+    private onPorts(comp: Schema.Component) {
+        const [...inputPorts] = this.circuit.getPortsForComponent(comp.id).unwrap();
+        return inputPorts.map((inputPort) => Signal.isOn(this.sim.getSignal(inputPort)));
+    }
+
     private assembleSegments(comp: Schema.Component) {
         // Border is subtracted from size so that size matches constant high/low
         // const transform = new Transform(this.getPos(comp), this.size.sub(V(this.options.defaultBorderWidth)), this.getAngle(comp));
-        const prims: Array<Omit<PolygonPrim, "style">> = [{
+        if (this.kind === "SegmentDisplay") {
+            
+        }
+        const prims: Array<Omit<PolygonPrim, "style">> = [
+        {
             kind:   "Polygon",
             points: [...horizontal],
         },
+        // {
+        //     kind:   "Polygon",
+        //     points: [...vertical],
+        // },
         {
             kind:   "Polygon",
-            points: [...vertical],
+            points: [...topLeft],
+        },
+        {
+            kind:   "Polygon",
+            points: [...bottomLeft],
+        },
+        {
+            kind:   "Polygon",
+            points: [...bottomRight],
         }];
         // const segmentCount = this.circuit.getCompByID(comp.id).unwrap().props["segmentCount"] ?? 7;
         // if (segmentCount === 7) {
