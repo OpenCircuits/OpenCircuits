6a3c26b4dbaf895b029af4a78e29e124681e3e21
Trevor Crystal <TGCrystal@users.noreply.github.com>
2025-04-16 00:05:57 -0400

    Add some tests for .toSchema() simState behaviors on deleted components

---
+++ src/projects/digital/api/circuit/tests/DigitalCircuit.test.ts
@@ -0,0 +1,56 @@
+import "shared/tests/helpers/Extensions";
+
+import {CreateTestCircuit} from "./helpers/CreateTestCircuit";
+import {V} from "Vector";
+
+
+describe("DigitalCircuit", () => {
+    describe(".toSchema()", () => {
+        test("Deleted off Switch should not be in simState", () => {
+            const [circuit, _, { Place }] = CreateTestCircuit();
+            const [sw] = Place("Switch");
+
+            sw.delete();
+            const schema = circuit.toSchema();
+            expect(Object.keys(schema.simState.signals)).toHaveLength(0);
+            expect(Object.keys(schema.simState.states)).toHaveLength(0);
+            expect(Object.keys(schema.simState.icStates)).toHaveLength(0);
+        });
+        test("Deleted on Switch should not be in simState", () => {
+            const [circuit, _, { Place, TurnOn }] = CreateTestCircuit();
+            const [sw] = Place("Switch");
+            TurnOn(sw);
+
+            sw.delete();
+            const schema = circuit.toSchema();
+            expect(Object.keys(schema.simState.signals)).toHaveLength(0);
+            expect(Object.keys(schema.simState.states)).toHaveLength(0);
+            expect(Object.keys(schema.simState.icStates)).toHaveLength(0);
+        });
+        test("Deleted IC should not be in simState", () => {
+            const [circuit, _, { Place }] = CreateTestCircuit();
+            const [icCircuit, {}, { PlaceAndConnect: ICPlaceAndConnect }] = CreateTestCircuit();
+            const ic = (() => {
+                const [sw, { outputs: [led] }] = ICPlaceAndConnect("Switch");
+                icCircuit.name = "To be deleted IC";
+
+                return circuit.createIC({
+                    circuit: icCircuit,
+                    display: {
+                        size: V(4, 2),
+                        pins: [
+                            { id: sw.outputs[0].id, group: "inputs", name: "In 1", pos: V(-1, -0.5), dir: V(-1, 0) },
+                            { id: led.inputs[0].id, group: "outputs", name: "Out",  pos: V(+1,   0), dir: V(+1, 0) },
+                        ],
+                    },
+                });
+            })();
+            const [icInstance] = Place(ic.id);
+            icInstance.delete();
+            const schema = circuit.toSchema();
+            expect(Object.keys(schema.simState.signals)).toHaveLength(0);
+            expect(Object.keys(schema.simState.states)).toHaveLength(0);
+            expect(Object.keys(schema.simState.icStates)).toHaveLength(0);
+        });
+    });
+});
