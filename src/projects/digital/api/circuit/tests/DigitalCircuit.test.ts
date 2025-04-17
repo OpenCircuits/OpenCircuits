97d164ec055aa83a9beae87d946400af710de3eb
Trevor Crystal <TGCrystal@users.noreply.github.com>
2025-04-16 23:29:33 -0400

    Improve IC schema save/load test

--- src/projects/digital/api/circuit/tests/DigitalCircuit.test.ts
+++ src/projects/digital/api/circuit/tests/DigitalCircuit.test.ts
@@ -56,16 +56,17 @@ describe("DigitalCircuit", () => {
             const [circuit, _, { Place }] = CreateTestCircuit();
             const [icCircuit, {}, { PlaceAndConnect: ICPlaceAndConnect }] = CreateTestCircuit();
             const ic = (() => {
-                const [sw, { outputs: [led] }] = ICPlaceAndConnect("Switch");
-                icCircuit.name = "To be deleted IC";
+                const [gate, { inputs: [sw1, sw2], outputs: [out] }] = ICPlaceAndConnect("ANDGate");
+                icCircuit.name = "To be serialized IC";
 
                 return circuit.createIC({
                     circuit: icCircuit,
                     display: {
                         size: V(4, 2),
                         pins: [
-                            { id: sw.outputs[0].id, group: "inputs", name: "In 1", pos: V(-1, -0.5), dir: V(-1, 0) },
-                            { id: led.inputs[0].id, group: "outputs", name: "Out",  pos: V(+1,   0), dir: V(+1, 0) },
+                            { id: sw1.outputs[0].id, group: "inputs", name: "In 1", pos: V(-1, +0.5), dir: V(-1, 0) },
+                            { id: sw2.outputs[0].id, group: "inputs", name: "In 2", pos: V(-1, -0.5), dir: V(-1, 0) },
+                            { id: out.inputs[0].id, group: "outputs", name: "Out",  pos: V(+1,    0), dir: V(+1, 0) },
                         ],
                     },
                 });
@@ -73,12 +74,20 @@ describe("DigitalCircuit", () => {
             Place(ic.id);
             const schema = circuit.toSchema();
 
-            const [newCircuit] = CreateTestCircuit();
-            // The schema can appear to load while logging errors to the console.
-            // The only other way to notice these errors are to connect switches/leds to the IC and test it.
-            const errorLogSpy = jest.spyOn(global.console, "error");
+            const [newCircuit, _state, { Place: PlaceNew, Connect: ConnectNew, TurnOn: TurnOnNew }] = CreateTestCircuit();
             newCircuit.loadSchema(schema);
-            expect(errorLogSpy).not.toHaveBeenCalled();
+            const newComps = newCircuit.getComponents();
+            expect(newComps).toHaveLength(1);
+            const icInstance = newComps[0];
+
+            const [sw1, sw2, led] = PlaceNew("Switch", "Switch", "LED");
+            ConnectNew(sw1, icInstance.inputs[0]);
+            ConnectNew(sw2, icInstance.inputs[1]);
+            ConnectNew(icInstance, led);
+            expect(led).toBeOff();
+            TurnOnNew(sw1);
+            TurnOnNew(sw2);
+            expect(led).toBeOn();
         });
     });
 });
