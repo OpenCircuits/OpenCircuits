83c3122bc2ae752eb096a84f037c372c54b755e4
Trevor Crystal <TGCrystal@users.noreply.github.com>
2025-06-14 21:50:42 -0400

    Improve smart place by sorting inputs/outputs

--- src/projects/digital/site/src/containers/DigitalMainDesigner/index.tsx
+++ src/projects/digital/site/src/containers/DigitalMainDesigner/index.tsx
@@ -4,76 +4,15 @@ import {usePageVisibility} from "shared/site/utils/hooks/usePageVisibility";
 
 import {MainDesigner} from "shared/site/containers/MainDesigner";
 
-// import {SmartPlaceOptions} from "digital/site/utils/DigitalCreate";
+import {SmartPlace} from "digital/site/utils/SmartPlace";
 import {useCurDigitalDesigner} from "digital/site/utils/hooks/useDigitalDesigner";
 
 import "./index.scss";
 import {DigitalCircuit} from "digital/api/circuit/public";
 import {SmartPlaceOptions} from "digital/site/utils/DigitalCreate";
-import {V, Vector} from "Vector";
-import {Rect} from "math/Rect";
 
 // import {useMainCircuit} from "shared/site/utils/hooks/useCircuit";
 
-
-const AUTO_PLACE_SWITCH_SPACE = 2.5;
-const AUTO_PLACE_LED_SPACE = 1;
-/**
- * Utility function that, given a DigitalComponent id, will create the component N times vertically
- *  (with behavior matches DigitalCreateN) but also create Switches for each input and LEDs for each
- *  output and automatically connect them together. Starts placing at position `pos`.
- * This function is directly used for implementation of issue #689.
- *
- * @param pos     The position of the first component.
- * @param itemId  The ID of the item, if an IC then it has the form: `ic/INDEX`, where INDEX
- *                corresponds to the index of the IC relative to the list of ICs in `designer`.
- * @param circuit The cirucit the items. Needed to place items.
- * @param N       The number of items to create.
- * @param options The options used to indicate what connected components to create.
- * @throws If the itemId is an invalid item or IC.
- */
-export function SmartPlace(pos: Vector, itemId: string, circuit: DigitalCircuit,
-    N: number, options: SmartPlaceOptions) {
-    circuit.beginTransaction();
-    for (let i = 0; i < N; i++) {
-        const comp = circuit.placeComponentAt(itemId, pos);
-
-        const inputPorts = (options & SmartPlaceOptions.Inputs) ? comp.inputs : [];
-
-        const outputPorts = (options & SmartPlaceOptions.Outputs) ? comp.outputs : [];
-
-        const inputs = inputPorts.map((port) => {
-            const sw = circuit.placeComponentAt("Switch", V(0, 0));
-            port.connectTo(sw.outputs[0]);
-            return sw;
-        });
-        const outputs = outputPorts.map((port) => {
-            const led = circuit.placeComponentAt("LED", V(0, 0));
-            port.connectTo(led.inputs[0]);
-            return led;
-        });
-
-        // Using the bounding box of the component with ports gives more spacing betwen it and the inputs/outputs
-        const compBoundsWithPorts = Rect.Bounding([comp.bounds, ...comp.allPorts.map((port) => port.bounds)]);
-        inputs.forEach((s, i) => {
-            s.pos = V(-compBoundsWithPorts.size.x / 2 - AUTO_PLACE_SWITCH_SPACE,
-                ((inputPorts.length - 1) / 2 - i) * s.bounds.size.y).add(comp.pos);
-        });
-        outputs.forEach((l, i) => {
-            l.pos = V(compBoundsWithPorts.size.x / 2 + AUTO_PLACE_LED_SPACE * (i + 1),
-                // This centers the LED around the port of the LED
-                (outputPorts[i].targetPos.y - comp.y) - (l.inputs[0].targetPos.y - l.y)).add(comp.pos)
-        });
-
-        pos = pos.sub(V(0, Rect.Bounding([
-            compBoundsWithPorts,
-            ...inputs.flatMap(({ allPorts, bounds }) => [bounds, ...allPorts.map(({ bounds }) => bounds)]),
-            ...outputs.flatMap(({ allPorts, bounds }) => [bounds, ...allPorts.map(({ bounds }) => bounds)]),
-        ]).height));
-    }
-
-    circuit.commitTransaction("Placed Group");
-}
 
 export const DigitalMainDesigner = ({ circuit }: {circuit: DigitalCircuit}) => {
     const designer = useCurDigitalDesigner();
