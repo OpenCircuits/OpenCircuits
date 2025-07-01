6e7a7dc81525a0b30bde77c55d2df00936a39d9e
Trevor Crystal <TGCrystal@users.noreply.github.com>
2025-07-01 00:37:40 -0400

    Add basic desktop/mobile test for Playwright

---
+++ playwright/digital/desktop/shared/basicCircuit.spec.ts
@@ -0,0 +1,61 @@
+import {expect, test} from "@playwright/test";
+
+import {pageActions} from "shared/helpers/actions";
+
+
+test("Basic Switch/LED Test", async ({ page }) => {
+    const { openItemnav, closeItemnav, togglePropagationMenu, togglePausePropagation, stepPropagation } = pageActions(page, false);
+
+    await page.goto("/");
+
+    const main = page.locator("main");
+    await expect(main).toBeVisible();
+
+    // Open itemnav
+    await openItemnav();
+
+    await page.getByRole("button", { name: "Switch", exact: true }).dragTo(main, {
+        targetPosition: { x: 400, y: 200 },
+    });
+
+    expect(await page.evaluate(() => window.Circuit.getComponents())).toHaveLength(1);
+
+    await page.getByRole("button", { name: "LED", exact: true }).dragTo(main, {
+        targetPosition: { x: 600, y: 200 },
+    });
+    expect(await page.evaluate(() => window.Circuit.getComponents())).toHaveLength(2);
+
+    // Close itemnav
+    await closeItemnav();
+
+    // Connect components
+    await main.dragTo(main, {
+        sourcePosition: { x: 465, y: 200 },
+        targetPosition: { x: 600, y: 300 },
+    });
+
+    // We will pause the simulation and manually advance it to test the state propagation
+    await togglePropagationMenu();
+    await togglePausePropagation();
+
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(0);
+    // Toggle on and off
+    await main.click({
+        position: {
+            x: 400,
+            y: 200,
+        },
+    });
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(0);
+    await stepPropagation();
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(1);
+    await main.click({
+        position: {
+            x: 400,
+            y: 200,
+        },
+    });
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(1);
+    await stepPropagation();
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(0);
+});
---
+++ playwright/digital/mobile/shared/basicCircuit.spec.ts
@@ -0,0 +1,81 @@
+import {expect, test} from "@playwright/test";
+
+import {pageActions} from "shared/helpers/actions";
+
+
+test("Basic Switch/LED Test", async ({ page }) => {
+    const { openItemnav, closeItemnav, togglePropagationMenu, togglePausePropagation, stepPropagation } = pageActions(page, true);
+
+    await page.goto("/");
+
+    const main = page.locator("main");
+    await expect(main).toBeVisible();
+
+    // Open itemnav
+    await openItemnav();
+
+    await page.waitForTimeout(1000);
+    await page.getByRole("button", { name: "Switch", exact: true }).tap();
+    await page.waitForTimeout(1000);
+    await main.tap({
+        position: {
+            x: 100,
+            y: 200,
+        },
+    });
+
+    expect(await page.evaluate(() => window.Circuit.getComponents())).toHaveLength(1);
+
+    await page.waitForTimeout(1000);
+    await page.getByRole("button", { name: "LED", exact: true }).tap();
+    await page.waitForTimeout(1000);
+    await main.tap({
+        position: {
+            x: 300,
+            y: 200,
+        },
+    });
+    expect(await page.evaluate(() => window.Circuit.getComponents())).toHaveLength(2);
+
+    // Close itemnav
+    await closeItemnav();
+
+    // Connect components
+    await main.tap({
+        position: {
+            x: 165,
+            y: 200,
+        },
+    });
+    await main.tap({
+        position: {
+            x: 300,
+            y: 300,
+        },
+    });
+
+    // We will pause the simulation and manually advance it to test the state propagation
+    await togglePropagationMenu();
+    await togglePausePropagation();
+
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(0);
+    // Toggle on and off
+    await main.tap({
+        position: {
+            x: 100,
+            y: 200,
+        },
+    });
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(0);
+    await stepPropagation();
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(1);
+    await main.tap({
+        position: {
+            x: 100,
+            y: 200,
+        },
+    });
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(1);
+    await stepPropagation();
+    expect(await page.evaluate(() => window.Circuit.getComponents().find(({ kind }) => kind === "LED").allPorts[0].signal)).toBe(0);
+});
