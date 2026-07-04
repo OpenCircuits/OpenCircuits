0451a092e90a9f4c802a58e5c2557378d3003d05
Trevor Crystal <TGCrystal@users.noreply.github.com>
2026-07-03 20:20:27 -0400

    Add oxfmt, remove more obsolete eslint stuff

---
+++ oxfmt.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+    // This endOfLine behavior matches the auto behavior we have in .gitignore
+    endOfLine: process.platform === "win32" ? "crlf" : "lf",
+    ignorePatterns: ["*.md"],
+    printWidth: 120,
+    quoteProps: "preserve",
+    tabWidth: 4,
+});
